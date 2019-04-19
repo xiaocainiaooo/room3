aosp-mirror/platform_frameworks_support

Adding Support for Burst Capture.
Bug Id :130192229
Test: ./gradlew camera:camera*:test

Change-Id: I2c473156ae41864af3d854dad45142f5c9075fe1
Signed-off-by: wasim.ansari <wasim.ansari@samsung.com>

camera/camera2/src/androidTest/java/androidx/camera/camera2/PreviewTest.java
@@ -485,7 +485,8 @@ public final class PreviewTest {
                     }
 
                     @Override
-                    public void onCameraControlSingleRequest(CaptureConfig captureConfig) {
+                    public void onCameraControlCaptureRequest(List<CaptureConfig> captureConfigs) {
+
                     }
                 },
                 new Handler());

camera/camera2/src/main/java/androidx/camera/camera2/Camera2CameraControl.java
@@ -39,7 +39,10 @@ import androidx.camera.core.FlashMode;
 import androidx.camera.core.OnFocusListener;
 import androidx.camera.core.SessionConfig;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 /**
@@ -241,7 +244,7 @@ public final class Camera2CameraControl implements CameraControl {
         configBuilder.setCaptureRequestOption(CaptureRequest.CONTROL_AF_TRIGGER,
                 CaptureRequest.CONTROL_AF_TRIGGER_CANCEL);
         singleRequestBuilder.addImplementationOptions(configBuilder.build());
-        notifySingleRequest(singleRequestBuilder.build());
+        notifyCaptureRequest(Collections.singletonList(singleRequestBuilder.build()));
 
         mIsFocusLocked = false;
         updateSessionConfig();
@@ -288,7 +291,7 @@ public final class Camera2CameraControl implements CameraControl {
             configBuilder.setCaptureRequestOption(CaptureRequest.CONTROL_AE_MODE,
                     CaptureRequest.CONTROL_AE_MODE_ON);
             singleRequestBuilder.addImplementationOptions(configBuilder.build());
-            notifySingleRequest(singleRequestBuilder.build());
+            notifyCaptureRequest(Collections.singletonList(singleRequestBuilder.build()));
         }
         updateSessionConfig();
     }
@@ -326,7 +329,7 @@ public final class Camera2CameraControl implements CameraControl {
         configBuilder.setCaptureRequestOption(CaptureRequest.CONTROL_AF_TRIGGER,
                 CaptureRequest.CONTROL_AF_TRIGGER_START);
         builder.addImplementationOptions(configBuilder.build());
-        notifySingleRequest(builder.build());
+        notifyCaptureRequest(Collections.singletonList(builder.build()));
     }
 
     /**
@@ -352,7 +355,7 @@ public final class Camera2CameraControl implements CameraControl {
         configBuilder.setCaptureRequestOption(CaptureRequest.CONTROL_AE_PRECAPTURE_TRIGGER,
                 CaptureRequest.CONTROL_AE_PRECAPTURE_TRIGGER_START);
         builder.addImplementationOptions(configBuilder.build());
-        notifySingleRequest(builder.build());
+        notifyCaptureRequest(Collections.singletonList(builder.build()));
     }
 
     /**
@@ -387,24 +390,24 @@ public final class Camera2CameraControl implements CameraControl {
                     CaptureRequest.CONTROL_AE_PRECAPTURE_TRIGGER_CANCEL);
         }
         builder.addImplementationOptions(configBuilder.build());
-        notifySingleRequest(builder.build());
+        notifyCaptureRequest(Collections.singletonList(builder.build()));
     }
 
     private int getDefaultTemplate() {
         return CameraDevice.TEMPLATE_PREVIEW;
     }
 
-    void notifySingleRequest(final CaptureConfig captureConfig) {
+    void notifyCaptureRequest(final List<CaptureConfig> captureConfigs) {
         if (Looper.myLooper() != mHandler.getLooper()) {
             mHandler.post(new Runnable() {
                 @Override
                 public void run() {
-                    Camera2CameraControl.this.notifySingleRequest(captureConfig);
+                    Camera2CameraControl.this.notifyCaptureRequest(captureConfigs);
                 }
             });
             return;
         }
-        mControlUpdateListener.onCameraControlSingleRequest(captureConfig);
+        mControlUpdateListener.onCameraControlCaptureRequest(captureConfigs);
     }
 
     void updateSessionConfig() {
@@ -423,21 +426,25 @@ public final class Camera2CameraControl implements CameraControl {
 
     /** {@inheritDoc} */
     @Override
-    public void submitSingleRequest(final CaptureConfig captureConfig) {
+    public void submitCaptureRequest(final List<CaptureConfig> captureConfigs) {
         if (Looper.myLooper() != mHandler.getLooper()) {
             mHandler.post(new Runnable() {
                 @Override
                 public void run() {
-                    Camera2CameraControl.this.submitSingleRequest(captureConfig);
+                    Camera2CameraControl.this.submitCaptureRequest(captureConfigs);
                 }
             });
             return;
         }
 
-        CaptureConfig.Builder builder = CaptureConfig.Builder.from(captureConfig);
-        // Always override options by shared options for the single request from outside.
-        builder.addImplementationOptions(getSharedOptions());
-        notifySingleRequest(builder.build());
+        List<CaptureConfig> captureConfigsWithImpl = new ArrayList<>();
+        for (CaptureConfig captureConfig : captureConfigs) {
+            CaptureConfig.Builder builder = CaptureConfig.Builder.from(captureConfig);
+            // Always override options by shared options for the single request from outside.
+            builder.addImplementationOptions(getSharedOptions());
+            captureConfigsWithImpl.add(builder.build());
+        }
+        notifyCaptureRequest(captureConfigsWithImpl);
     }
 
     /**

camera/camera2/src/main/java/androidx/camera/camera2/CameraBurstCaptureCallback.java
@@ -1,0 +1,80 @@
+/*
+ * Copyright (C) 2019 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package androidx.camera.camera2;
+
+import android.hardware.camera2.CameraCaptureSession;
+import android.hardware.camera2.CaptureFailure;
+import android.hardware.camera2.CaptureRequest;
+import android.hardware.camera2.TotalCaptureResult;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.RestrictTo;
+import androidx.annotation.RestrictTo.Scope;
+import androidx.camera.core.CameraCaptureCallback;
+import androidx.camera.core.CameraCaptureFailure;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * A callback object for tracking the progress of a capture request submitted to the camera device.
+ *
+ * @hide
+ */
+@RestrictTo(Scope.LIBRARY_GROUP)
+public class CameraBurstCaptureCallback extends CameraCaptureSession.CaptureCallback {
+
+    Map<CaptureRequest, List<CameraCaptureCallback>> mCallbackMap;
+
+    CameraBurstCaptureCallback() {
+        mCallbackMap = new HashMap<>();
+    }
+
+    @Override
+    public void onCaptureCompleted(@NonNull CameraCaptureSession session,
+            @NonNull CaptureRequest request, @NonNull TotalCaptureResult result) {
+        List<CameraCaptureCallback> cameraCaptureCallback = mCallbackMap.get(request);
+
+        for (CameraCaptureCallback cb : cameraCaptureCallback) {
+            cb.onCaptureCompleted(
+                    new Camera2CameraCaptureResult(request.getTag(), result));
+        }
+
+    }
+
+    @Override
+    public void onCaptureFailed(@NonNull CameraCaptureSession session,
+            @NonNull CaptureRequest request, @NonNull CaptureFailure failure) {
+
+        List<CameraCaptureCallback> cameraCaptureCallback = mCallbackMap.get(request);
+
+
+        CameraCaptureFailure cameraFailure =
+                new CameraCaptureFailure(CameraCaptureFailure.Reason.ERROR);
+        for (CameraCaptureCallback cb : cameraCaptureCallback) {
+            cb.onCaptureFailed(cameraFailure);
+        }
+    }
+
+    void addCallback(CaptureRequest captureRequest,
+            List<CameraCaptureCallback> cameraCaptureCallbacks) {
+
+        mCallbackMap.put(captureRequest, cameraCaptureCallbacks);
+
+    }
+}

camera/core/src/main/java/androidx/camera/core/ImageCapture.java
@@ -820,6 +820,7 @@ public class ImageCapture extends UseCase {
     /** Issues a take picture request. */
     ListenableFuture<Void> issueTakePicture() {
         final List<ListenableFuture<Void>> futureList = new ArrayList<>();
+        final List<CaptureConfig> captureConfigs = new ArrayList<>();
 
         for (final CaptureStage captureStage : mCaptureBundle.getCaptureStages()) {
             final CaptureConfig.Builder builder = new CaptureConfig.Builder();
@@ -834,7 +835,6 @@ public class ImageCapture extends UseCase {
             builder.setTag(captureStage.getCaptureConfig().getTag());
             builder.addCameraCaptureCallback(mMetadataMatchingCaptureCallback);
 
-            final CameraControl cameraControl = getCurrentCameraControl();
             ListenableFuture<Void> future = CallbackToFutureAdapter.getFuture(
                     new CallbackToFutureAdapter.Resolver<Void>() {
                         @Override
@@ -858,7 +858,7 @@ public class ImageCapture extends UseCase {
                             };
                             builder.addCameraCaptureCallback(completerCallback);
 
-                            cameraControl.submitSingleRequest(builder.build());
+                            captureConfigs.add(builder.build());
                             return "issueTakePicture[stage=" + captureStage.getId() + "]";
                         }
                     });
@@ -866,6 +866,7 @@ public class ImageCapture extends UseCase {
 
         }
 
+        getCurrentCameraControl().submitCaptureRequest(captureConfigs);
         return CallbackToFutureAdapter.getFuture(new CallbackToFutureAdapter.Resolver<Void>() {
             @Override
             public Object attachCompleter(

camera/testing/src/main/java/androidx/camera/testing/fakes/FakeCamera.java
@@ -24,6 +24,7 @@ import androidx.camera.core.SessionConfig;
 import androidx.camera.core.UseCase;
 
 import java.util.Collection;
+import java.util.List;
 
 /** A fake camera which will not produce any data. */
 public class FakeCamera implements BaseCamera {
@@ -92,6 +93,6 @@ public class FakeCamera implements BaseCamera {
     }
 
     @Override
-    public void onCameraControlSingleRequest(CaptureConfig captureConfig) {
+    public void onCameraControlCaptureRequest(List<CaptureConfig> captureConfigs) {
     }
 }

xiaocainiaooo/room3

camera/camera2/src/androidTest/java/androidx/camera/camera2/PreviewTest.java
@@ -485,7 +485,8 @@ public final class PreviewTest {
                     }
 
                     @Override
-                    public void onCameraControlSingleRequest(CaptureConfig captureConfig) {
+                    public void onCameraControlCaptureRequests(List<CaptureConfig> captureConfigs) {
+
                     }
                 },
                 new Handler());

camera/camera2/src/main/java/androidx/camera/camera2/Camera2CameraControl.java
@@ -39,7 +39,10 @@ import androidx.camera.core.FlashMode;
 import androidx.camera.core.OnFocusListener;
 import androidx.camera.core.SessionConfig;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 /**
@@ -241,7 +244,7 @@ public final class Camera2CameraControl implements CameraControl {
         configBuilder.setCaptureRequestOption(CaptureRequest.CONTROL_AF_TRIGGER,
                 CaptureRequest.CONTROL_AF_TRIGGER_CANCEL);
         singleRequestBuilder.addImplementationOptions(configBuilder.build());
-        notifySingleRequest(singleRequestBuilder.build());
+        notifyCaptureRequests(Collections.singletonList(singleRequestBuilder.build()));
 
         mIsFocusLocked = false;
         updateSessionConfig();
@@ -288,7 +291,7 @@ public final class Camera2CameraControl implements CameraControl {
             configBuilder.setCaptureRequestOption(CaptureRequest.CONTROL_AE_MODE,
                     CaptureRequest.CONTROL_AE_MODE_ON);
             singleRequestBuilder.addImplementationOptions(configBuilder.build());
-            notifySingleRequest(singleRequestBuilder.build());
+            notifyCaptureRequests(Collections.singletonList(singleRequestBuilder.build()));
         }
         updateSessionConfig();
     }
@@ -326,7 +329,7 @@ public final class Camera2CameraControl implements CameraControl {
         configBuilder.setCaptureRequestOption(CaptureRequest.CONTROL_AF_TRIGGER,
                 CaptureRequest.CONTROL_AF_TRIGGER_START);
         builder.addImplementationOptions(configBuilder.build());
-        notifySingleRequest(builder.build());
+        notifyCaptureRequests(Collections.singletonList(builder.build()));
     }
 
     /**
@@ -352,7 +355,7 @@ public final class Camera2CameraControl implements CameraControl {
         configBuilder.setCaptureRequestOption(CaptureRequest.CONTROL_AE_PRECAPTURE_TRIGGER,
                 CaptureRequest.CONTROL_AE_PRECAPTURE_TRIGGER_START);
         builder.addImplementationOptions(configBuilder.build());
-        notifySingleRequest(builder.build());
+        notifyCaptureRequests(Collections.singletonList(builder.build()));
     }
 
     /**
@@ -387,24 +390,24 @@ public final class Camera2CameraControl implements CameraControl {
                     CaptureRequest.CONTROL_AE_PRECAPTURE_TRIGGER_CANCEL);
         }
         builder.addImplementationOptions(configBuilder.build());
-        notifySingleRequest(builder.build());
+        notifyCaptureRequests(Collections.singletonList(builder.build()));
     }
 
     private int getDefaultTemplate() {
         return CameraDevice.TEMPLATE_PREVIEW;
     }
 
-    void notifySingleRequest(final CaptureConfig captureConfig) {
+    void notifyCaptureRequests(final List<CaptureConfig> captureConfigs) {
         if (Looper.myLooper() != mHandler.getLooper()) {
             mHandler.post(new Runnable() {
                 @Override
                 public void run() {
-                    Camera2CameraControl.this.notifySingleRequest(captureConfig);
+                    Camera2CameraControl.this.notifyCaptureRequests(captureConfigs);
                 }
             });
             return;
         }
-        mControlUpdateListener.onCameraControlSingleRequest(captureConfig);
+        mControlUpdateListener.onCameraControlCaptureRequests(captureConfigs);
     }
 
     void updateSessionConfig() {
@@ -423,21 +426,25 @@ public final class Camera2CameraControl implements CameraControl {
 
     /** {@inheritDoc} */
     @Override
-    public void submitSingleRequest(final CaptureConfig captureConfig) {
+    public void submitCaptureRequests(final List<CaptureConfig> captureConfigs) {
         if (Looper.myLooper() != mHandler.getLooper()) {
             mHandler.post(new Runnable() {
                 @Override
                 public void run() {
-                    Camera2CameraControl.this.submitSingleRequest(captureConfig);
+                    Camera2CameraControl.this.submitCaptureRequests(captureConfigs);
                 }
             });
             return;
         }
 
-        CaptureConfig.Builder builder = CaptureConfig.Builder.from(captureConfig);
-        // Always override options by shared options for the single request from outside.
-        builder.addImplementationOptions(getSharedOptions());
-        notifySingleRequest(builder.build());
+        List<CaptureConfig> captureConfigsWithImpl = new ArrayList<>();
+        for (CaptureConfig captureConfig : captureConfigs) {
+            CaptureConfig.Builder builder = CaptureConfig.Builder.from(captureConfig);
+            // Always override options by shared options for the capture request from outside.
+            builder.addImplementationOptions(getSharedOptions());
+            captureConfigsWithImpl.add(builder.build());
+        }
+        notifyCaptureRequests(captureConfigsWithImpl);
     }
 
     /**

camera/camera2/src/main/java/androidx/camera/camera2/CameraBurstCaptureCallback.java
@@ -1,0 +1,78 @@
+/*
+ * Copyright (C) 2019 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package androidx.camera.camera2;
+
+import android.hardware.camera2.CameraCaptureSession;
+import android.hardware.camera2.CaptureFailure;
+import android.hardware.camera2.CaptureRequest;
+import android.hardware.camera2.TotalCaptureResult;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.RestrictTo;
+import androidx.annotation.RestrictTo.Scope;
+import androidx.camera.core.CameraCaptureCallback;
+import androidx.camera.core.CameraCaptureFailure;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * A callback object for tracking the progress of a capture request submitted to the camera device.
+ *
+ * @hide
+ */
+@RestrictTo(Scope.LIBRARY_GROUP)
+public class CameraBurstCaptureCallback extends CameraCaptureSession.CaptureCallback {
+
+    final Map<CaptureRequest, List<CameraCaptureCallback>> mCallbackMap;
+
+    CameraBurstCaptureCallback() {
+        mCallbackMap = new HashMap<>();
+    }
+
+    @Override
+    public void onCaptureCompleted(@NonNull CameraCaptureSession session,
+            @NonNull CaptureRequest request, @NonNull TotalCaptureResult result) {
+        List<CameraCaptureCallback> cameraCaptureCallbacks = mCallbackMap.get(request);
+
+        for (CameraCaptureCallback cb : cameraCaptureCallbacks) {
+            cb.onCaptureCompleted(
+                    new Camera2CameraCaptureResult(request.getTag(), result));
+        }
+    }
+
+    @Override
+    public void onCaptureFailed(@NonNull CameraCaptureSession session,
+            @NonNull CaptureRequest request, @NonNull CaptureFailure failure) {
+
+        List<CameraCaptureCallback> cameraCaptureCallbacks = mCallbackMap.get(request);
+
+        CameraCaptureFailure cameraFailure =
+                new CameraCaptureFailure(CameraCaptureFailure.Reason.ERROR);
+        for (CameraCaptureCallback cb : cameraCaptureCallbacks) {
+            cb.onCaptureFailed(cameraFailure);
+        }
+    }
+
+    void addCallback(CaptureRequest captureRequest,
+            List<CameraCaptureCallback> cameraCaptureCallbacks) {
+
+        mCallbackMap.put(captureRequest, cameraCaptureCallbacks);
+
+    }
+}

camera/core/src/main/java/androidx/camera/core/ImageCapture.java
@@ -820,6 +820,7 @@ public class ImageCapture extends UseCase {
     /** Issues a take picture request. */
     ListenableFuture<Void> issueTakePicture() {
         final List<ListenableFuture<Void>> futureList = new ArrayList<>();
+        final List<CaptureConfig> captureConfigs = new ArrayList<>();
 
         for (final CaptureStage captureStage : mCaptureBundle.getCaptureStages()) {
             final CaptureConfig.Builder builder = new CaptureConfig.Builder();
@@ -834,7 +835,6 @@ public class ImageCapture extends UseCase {
             builder.setTag(captureStage.getCaptureConfig().getTag());
             builder.addCameraCaptureCallback(mMetadataMatchingCaptureCallback);
 
-            final CameraControl cameraControl = getCurrentCameraControl();
             ListenableFuture<Void> future = CallbackToFutureAdapter.getFuture(
                     new CallbackToFutureAdapter.Resolver<Void>() {
                         @Override
@@ -858,7 +858,7 @@ public class ImageCapture extends UseCase {
                             };
                             builder.addCameraCaptureCallback(completerCallback);
 
-                            cameraControl.submitSingleRequest(builder.build());
+                            captureConfigs.add(builder.build());
                             return "issueTakePicture[stage=" + captureStage.getId() + "]";
                         }
                     });
@@ -866,6 +866,7 @@ public class ImageCapture extends UseCase {
 
         }
 
+        getCurrentCameraControl().submitCaptureRequests(captureConfigs);
         return CallbackToFutureAdapter.getFuture(new CallbackToFutureAdapter.Resolver<Void>() {
             @Override
             public Object attachCompleter(

camera/testing/src/main/java/androidx/camera/testing/fakes/FakeCamera.java
@@ -24,6 +24,7 @@ import androidx.camera.core.SessionConfig;
 import androidx.camera.core.UseCase;
 
 import java.util.Collection;
+import java.util.List;
 
 /** A fake camera which will not produce any data. */
 public class FakeCamera implements BaseCamera {
@@ -92,6 +93,6 @@ public class FakeCamera implements BaseCamera {
     }
 
     @Override
-    public void onCameraControlSingleRequest(CaptureConfig captureConfig) {
+    public void onCameraControlCaptureRequests(List<CaptureConfig> captureConfigs) {
     }
 }