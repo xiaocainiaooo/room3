aosp-mirror/platform_frameworks_support

Allow developers to customize WorkManager's task executor thread.
* Use the same bounded executor for all Room async reads.
* Wrap the underlying Executor with a SerialExecutor for ordered writes.

Test: All unit tests pass.
Ran integration tests.

Change-Id: I8be04671d7ee1fbb37312fb90bed70cf39f32ec1

work/integration-tests/testapp/src/main/java/androidx/work/integration/testapp/TestApplication.java
@@ -22,6 +22,8 @@ import android.util.Log;
 import androidx.annotation.NonNull;
 import androidx.work.Configuration;
 
+import java.util.concurrent.Executors;
+
 /**
  * An Application class that initializes WorkManager.
  */
@@ -30,6 +32,8 @@ public class TestApplication extends Application implements Configuration.Provid
     @NonNull
     @Override
     public Configuration getWorkManagerConfiguration() {
-        return new Configuration.Builder().setMinimumLoggingLevel(Log.VERBOSE).build();
+        return new Configuration.Builder()
+                .setTaskExecutorDelegate(Executors.newCachedThreadPool())
+                .setMinimumLoggingLevel(Log.VERBOSE).build();
     }
 }

work/workmanager/src/main/java/androidx/work/impl/WorkManagerImpl.java
@@ -179,7 +179,7 @@ public class WorkManagerImpl extends WorkManager {
                     sDefaultInstance = new WorkManagerImpl(
                             context,
                             configuration,
-                            new WorkManagerTaskExecutor());
+                            new WorkManagerTaskExecutor(configuration.getTaskExecutorDelegate()));
                 }
                 sDelegatedInstance = sDefaultInstance;
             }
@@ -224,7 +224,8 @@ public class WorkManagerImpl extends WorkManager {
             boolean useTestDatabase) {
 
         Context applicationContext = context.getApplicationContext();
-        WorkDatabase database = WorkDatabase.create(applicationContext, useTestDatabase);
+        WorkDatabase database = WorkDatabase.create(
+                applicationContext, configuration.getTaskExecutorDelegate(), useTestDatabase);
         Logger.setLogger(new Logger.LogcatLogger(configuration.getMinimumLoggingLevel()));
         List<Scheduler> schedulers = createSchedulers(applicationContext);
         Processor processor = new Processor(

xiaocainiaooo/room3

work/integration-tests/testapp/src/main/java/androidx/work/integration/testapp/TestApplication.java
@@ -22,6 +22,8 @@ import android.util.Log;
 import androidx.annotation.NonNull;
 import androidx.work.Configuration;
 
+import java.util.concurrent.Executors;
+
 /**
  * An Application class that initializes WorkManager.
  */
@@ -30,6 +32,8 @@ public class TestApplication extends Application implements Configuration.Provid
     @NonNull
     @Override
     public Configuration getWorkManagerConfiguration() {
-        return new Configuration.Builder().setMinimumLoggingLevel(Log.VERBOSE).build();
+        return new Configuration.Builder()
+                .setTaskExecutor(Executors.newCachedThreadPool())
+                .setMinimumLoggingLevel(Log.VERBOSE).build();
     }
 }

work/workmanager/src/main/java/androidx/work/impl/WorkManagerImpl.java
@@ -179,7 +179,7 @@ public class WorkManagerImpl extends WorkManager {
                     sDefaultInstance = new WorkManagerImpl(
                             context,
                             configuration,
-                            new WorkManagerTaskExecutor());
+                            new WorkManagerTaskExecutor(configuration.getTaskExecutor()));
                 }
                 sDelegatedInstance = sDefaultInstance;
             }
@@ -224,7 +224,8 @@ public class WorkManagerImpl extends WorkManager {
             boolean useTestDatabase) {
 
         Context applicationContext = context.getApplicationContext();
-        WorkDatabase database = WorkDatabase.create(applicationContext, useTestDatabase);
+        WorkDatabase database = WorkDatabase.create(
+                applicationContext, configuration.getTaskExecutor(), useTestDatabase);
         Logger.setLogger(new Logger.LogcatLogger(configuration.getMinimumLoggingLevel()));
         List<Scheduler> schedulers = createSchedulers(applicationContext);
         Processor processor = new Processor(