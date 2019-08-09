aosp-mirror/platform_frameworks_support

Add a tooling API for collecting information needed by developer tools
Test: new tests
Change-Id: Ief01ad5f07b074b6af10e0efb775bf9f68b814a5

ui/tooling/src/androidTest/java/androidx/ui/tooling/InspectableTests.kt
@@ -1,0 +1,101 @@
+/*
+ * Copyright 2019 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package androidx.ui.tooling
+
+import androidx.compose.SlotTable
+import androidx.test.filters.SmallTest
+import org.junit.Test
+
+import androidx.ui.core.dp
+import androidx.ui.core.px
+import androidx.ui.graphics.Color
+import androidx.compose.composer
+import androidx.ui.baseui.ColoredRect
+import androidx.ui.layout.Column
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertFalse
+import org.junit.Assert.assertNotEquals
+import org.junit.Assert.assertNotNull
+import org.junit.Assert.assertTrue
+import org.junit.runner.RunWith
+import org.junit.runners.JUnit4
+
+@SmallTest
+@RunWith(JUnit4::class)
+class InspectableTests : ToolingTest() {
+    @Test
+    fun simpleInspection() {
+        show {
+            Inspectable {
+                Column {
+                    ColoredRect(color = Color(0xFF), width = 100.dp, height = 100.dp)
+                }
+            }
+        }
+
+        // Should be able to find the group for this test
+        val group = tables.findGroupForFile("InspectableTests")
+        assertNotNull(group)
+
+        // The group should have a non-empty bounding box
+        assertEquals(0.px, group!!.box.top)
+        assertEquals(0.px, group.box.left)
+        assertNotEquals(0.px, group.box.right)
+        assertNotEquals(0.px, group.box.bottom)
+    }
+
+    @Test
+    fun inInspectionMode() {
+        var displayed = false
+        show {
+            Inspectable {
+                Column {
+                    InInspectionModeOnly {
+                        ColoredRect(color = Color(0xFF), width = 100.dp, height = 100.dp)
+                        displayed = true
+                    }
+                }
+            }
+        }
+
+        assertTrue(displayed)
+    }
+
+    @Test
+    fun notInInspectionMode() {
+        var displayed = false
+        show {
+            Column {
+                InInspectionModeOnly {
+                    ColoredRect(color = Color(0xFF), width = 100.dp, height = 100.dp)
+                    displayed = true
+                }
+            }
+        }
+
+        assertFalse(displayed)
+    }
+}
+
+fun Iterable<SlotTable>.findGroupForFile(fileName: String) =
+    map { it.findGroupForFile(fileName) }.filterNotNull().firstOrNull()
+fun SlotTable.findGroupForFile(fileName: String) = asTree().findGroupForFile(fileName)
+fun Group.findGroupForFile(fileName: String): Group? {
+    val position = position
+    if (position != null && position.contains(fileName)) return this
+    return children.map { it.findGroupForFile(fileName) }.filterNotNull().firstOrNull()
+}

ui/tooling/src/androidTest/java/androidx/ui/tooling/ToolingTest.kt
@@ -1,0 +1,82 @@
+/*
+ * Copyright 2019 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package androidx.ui.tooling
+
+import android.os.Handler
+import androidx.compose.Children
+import androidx.compose.Composable
+import androidx.compose.setContent
+import androidx.compose.composer
+import androidx.test.rule.ActivityTestRule
+import androidx.ui.core.CraneWrapper
+import androidx.ui.core.OnPositioned
+import org.junit.Before
+import org.junit.Rule
+import java.util.concurrent.CountDownLatch
+import java.util.concurrent.TimeUnit
+
+open class ToolingTest {
+    @get:Rule
+    val activityTestRule = ActivityTestRule<TestActivity>(
+        TestActivity::class.java
+    )
+    lateinit var activity: TestActivity
+    lateinit var handler: Handler
+    lateinit var positionedLatch: CountDownLatch
+
+    @Before
+    fun setup() {
+        activity = activityTestRule.activity
+        activity.hasFocusLatch.await(5, TimeUnit.SECONDS)
+
+        activityTestRule.onUiThread { handler = Handler() }
+    }
+
+    @Composable
+    internal fun Positioned() {
+        OnPositioned(onPositioned = { positionedLatch.countDown() })
+    }
+
+    internal fun show(@Children composable: @Composable() () -> Unit) {
+        positionedLatch = CountDownLatch(1)
+        activityTestRule.onUiThread {
+            activity.setContent {
+                CraneWrapper {
+                    Positioned()
+                    composable()
+                }
+            }
+        }
+
+        // Wait for the layout to be performed
+        positionedLatch.await(1, TimeUnit.SECONDS)
+
+        // Wait for the UI thread to complete its current work so we know that layout is done.
+        activityTestRule.onUiThread { }
+    }
+}
+
+// Kotlin IR compiler doesn't seem too happy with auto-conversion from
+// lambda to Runnable, so separate it here
+fun ActivityTestRule<TestActivity>.onUiThread(block: () -> Unit) {
+    val runnable: Runnable = object : Runnable {
+        override fun run() {
+            block()
+        }
+    }
+    runOnUiThread(runnable)
+}

xiaocainiaooo/room3

ui/ui-tooling/src/androidTest/java/androidx/ui/tooling/InspectableTests.kt
@@ -1,0 +1,100 @@
+/*
+ * Copyright 2019 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package androidx.ui.tooling
+
+import androidx.compose.SlotTable
+import androidx.compose.composer
+import androidx.test.filters.SmallTest
+import androidx.ui.core.dp
+import androidx.ui.core.px
+import androidx.ui.foundation.ColoredRect
+import androidx.ui.graphics.Color
+import androidx.ui.layout.Column
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertFalse
+import org.junit.Assert.assertNotEquals
+import org.junit.Assert.assertNotNull
+import org.junit.Assert.assertTrue
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.JUnit4
+
+@SmallTest
+@RunWith(JUnit4::class)
+class InspectableTests : ToolingTest() {
+    @Test
+    fun simpleInspection() {
+        show {
+            Inspectable {
+                Column {
+                    ColoredRect(color = Color(0xFF), width = 100.dp, height = 100.dp)
+                }
+            }
+        }
+
+        // Should be able to find the group for this test
+        val group = tables.findGroupForFile("InspectableTests")
+        assertNotNull(group)
+
+        // The group should have a non-empty bounding box
+        assertEquals(0.px, group!!.box.top)
+        assertEquals(0.px, group.box.left)
+        assertNotEquals(0.px, group.box.right)
+        assertNotEquals(0.px, group.box.bottom)
+    }
+
+    @Test
+    fun inInspectionMode() {
+        var displayed = false
+        show {
+            Inspectable {
+                Column {
+                    InInspectionModeOnly {
+                        ColoredRect(color = Color(0xFF), width = 100.dp, height = 100.dp)
+                        displayed = true
+                    }
+                }
+            }
+        }
+
+        assertTrue(displayed)
+    }
+
+    @Test
+    fun notInInspectionMode() {
+        var displayed = false
+        show {
+            Column {
+                InInspectionModeOnly {
+                    ColoredRect(color = Color(0xFF), width = 100.dp, height = 100.dp)
+                    displayed = true
+                }
+            }
+        }
+
+        assertFalse(displayed)
+    }
+}
+
+fun Iterable<SlotTable>.findGroupForFile(fileName: String) =
+    map { it.findGroupForFile(fileName) }.filterNotNull().firstOrNull()
+fun SlotTable.findGroupForFile(fileName: String) = asTree().findGroupForFile(fileName)
+fun Group.findGroupForFile(fileName: String): Group? {
+    val position = position
+    if (position != null && position.contains(fileName)) return this
+    return children.map { it.findGroupForFile(fileName) }.filterNotNull().firstOrNull()
+}

ui/ui-tooling/src/androidTest/java/androidx/ui/tooling/ToolingTest.kt
@@ -1,0 +1,79 @@
+/*
+ * Copyright 2019 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package androidx.ui.tooling
+
+import android.os.Handler
+import androidx.compose.Children
+import androidx.compose.Composable
+import androidx.compose.composer
+import androidx.test.rule.ActivityTestRule
+import androidx.ui.core.OnPositioned
+import androidx.ui.core.setContent
+import org.junit.Before
+import org.junit.Rule
+import java.util.concurrent.CountDownLatch
+import java.util.concurrent.TimeUnit
+
+open class ToolingTest {
+    @get:Rule
+    val activityTestRule = ActivityTestRule<TestActivity>(
+        TestActivity::class.java
+    )
+    lateinit var activity: TestActivity
+    lateinit var handler: Handler
+    lateinit var positionedLatch: CountDownLatch
+
+    @Before
+    fun setup() {
+        activity = activityTestRule.activity
+        activity.hasFocusLatch.await(5, TimeUnit.SECONDS)
+
+        activityTestRule.onUiThread { handler = Handler() }
+    }
+
+    @Composable
+    internal fun Positioned() {
+        OnPositioned(onPositioned = { positionedLatch.countDown() })
+    }
+
+    internal fun show(@Children composable: @Composable() () -> Unit) {
+        positionedLatch = CountDownLatch(1)
+        activityTestRule.onUiThread {
+            activity.setContent {
+                Positioned()
+                composable()
+            }
+        }
+
+        // Wait for the layout to be performed
+        positionedLatch.await(1, TimeUnit.SECONDS)
+
+        // Wait for the UI thread to complete its current work so we know that layout is done.
+        activityTestRule.onUiThread { }
+    }
+}
+
+// Kotlin IR compiler doesn't seem too happy with auto-conversion from
+// lambda to Runnable, so separate it here
+fun ActivityTestRule<TestActivity>.onUiThread(block: () -> Unit) {
+    val runnable: Runnable = object : Runnable {
+        override fun run() {
+            block()
+        }
+    }
+    runOnUiThread(runnable)
+}