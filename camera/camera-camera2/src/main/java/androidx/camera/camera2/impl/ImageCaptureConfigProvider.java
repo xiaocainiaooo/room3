aosp-mirror/platform_frameworks_support

Update the behavior of setTargetAspectRatio() and setTargetResolution()
1. Throws an error at runtime for using both setTargetAspectRatio() and setTargetResolution() on the same config.
2. Sets the enum as a parameter to setTargetAspectRatio(CameraX.AspectRatio ratio).
3. Sets both the target resolution and target aspect ratio if the developer call the setTargetResolution().
4. Adds new API setTargetAspectRatioRational() which used to crop the image and set the default of use case.

Bug: b/130753788
Test: ./gradlew camera:camera-core:build && ./gradlew camera:camera-core:connectedAndroidTest && ./gradlew camera:camera-camera2:build && ./gradlew camera:camera-camera2:connectedAndroidTes
Change-Id: I544c8ab19aebff4f14cfce672a57b3cf245530f7

camera/camera2/src/main/java/androidx/camera/camera2/impl/ImageCaptureConfigProvider.java
@@ -99,7 +99,7 @@ public final class ImageCaptureConfigProvider implements ConfigProvider<ImageCap
                     targetRotation);
             boolean isRotateNeeded = (rotationDegrees == 90 || rotationDegrees == 270);
             builder.setTargetRotation(targetRotation);
-            builder.setTargetAspectRatio(
+            builder.setTargetAspectRatioRational(
                     isRotateNeeded ? DEFAULT_ASPECT_RATIO_3_4 : DEFAULT_ASPECT_RATIO_4_3);
         } catch (Exception e) {
             Log.w(TAG, "Unable to determine default lens facing for ImageCapture.", e);

xiaocainiaooo/room3

camera/camera-camera2/src/main/java/androidx/camera/camera2/impl/ImageCaptureConfigProvider.java
@@ -85,7 +85,7 @@ public final class ImageCaptureConfigProvider implements ConfigProvider<ImageCap
                     targetRotation);
             boolean isRotateNeeded = (rotationDegrees == 90 || rotationDegrees == 270);
             builder.setTargetRotation(targetRotation);
-            builder.setTargetAspectRatio(
+            builder.setTargetAspectRatioCustom(
                     isRotateNeeded ? DEFAULT_ASPECT_RATIO_3_4 : DEFAULT_ASPECT_RATIO_4_3);
         } catch (Exception e) {
             Log.w(TAG, "Unable to determine default lens facing for ImageCapture.", e);