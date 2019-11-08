xiaocainiaooo/room3

commit 6379fa9b8695331d8b264d9df2b915845e481a50

IMPROVEMENT: Make GestureDetector listen directly to RecyclerView.
This detangles the event handling a bit. Was using convoluted logic to determine when to delegate events to GestureDetector. After experimentation it didn't appear that convoluted arrangement was necessary.
Added new test for ToolHandlerRegistry.
Rename TouchEventHandler > EventHandler to avoid confusion. Can't convince git this is a rename.  Lower similarity threshold works `git log -M40 -p --stat`, but I'm not sure how to convince gerrit to use the same threshold.

Test: Added new test for ToolHandlerRegistry.

Change-Id: I1d8cc5beb1d3e0eaeec7ab6dfd74809903af5411

recyclerview/recyclerview-selection/src/main/java/androidx/recyclerview/selection/EventRouter.java
@@ -1,0 +1,71 @@
+/*
+ * Copyright 2017 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package androidx.recyclerview.selection;
+
+import static androidx.core.util.Preconditions.checkArgument;
+
+import android.view.MotionEvent;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.recyclerview.widget.RecyclerView.OnItemTouchListener;
+
+/**
+ * A class responsible for routing MotionEvents to tool-type specific handlers.
+ * Individual tool-type specific handlers are added after the class is constructed.
+ *
+ * <p>
+ * EventRouter takes its name from
+ * {@link RecyclerView#addOnItemTouchListener(OnItemTouchListener)}. Despite "Touch"
+ * being in the name, it receives MotionEvents for all types of tools.
+ */
+final class EventRouter implements OnItemTouchListener {
+
+    private static final String TAG = "EventRouter";
+
+    private final ToolHandlerRegistry<OnItemTouchListener> mDelegates;
+
+    EventRouter() {
+        mDelegates = new ToolHandlerRegistry<OnItemTouchListener>(new DummyOnItemTouchListener());
+    }
+
+    /**
+     * @param toolType See MotionEvent for details on available types.
+     * @param delegate An {@link OnItemTouchListener} to receive events
+     *     of {@code toolType}.
+     */
+    void set(int toolType, @NonNull OnItemTouchListener delegate) {
+        checkArgument(delegate != null);
+
+        mDelegates.set(toolType, delegate);
+    }
+
+    @Override
+    public boolean onInterceptTouchEvent(@NonNull RecyclerView rv, @NonNull MotionEvent e) {
+        return mDelegates.get(e).onInterceptTouchEvent(rv, e);
+    }
+
+    @Override
+    public void onTouchEvent(@NonNull RecyclerView rv, @NonNull MotionEvent e) {
+        mDelegates.get(e).onTouchEvent(rv, e);
+    }
+
+    @Override
+    public void onRequestDisallowInterceptTouchEvent(boolean disallowIntercept) {
+        // TODO(b/139141511): Handle onRequestDisallowInterceptTouchEvent.
+    }
+}

recyclerview/recyclerview-selection/src/main/java/androidx/recyclerview/selection/GestureDetectorOnItemTouchListenerAdapter.java
@@ -1,0 +1,52 @@
+/*
+ * Copyright 2019 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package androidx.recyclerview.selection;
+
+import static androidx.core.util.Preconditions.checkArgument;
+
+import android.view.GestureDetector;
+import android.view.MotionEvent;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+/**
+ * Class allowing GestureDetector to listen directly to RecyclerView touch events.
+ */
+final class GestureDetectorOnItemTouchListenerAdapter implements RecyclerView.OnItemTouchListener {
+
+    private final GestureDetector mDetector;
+    GestureDetectorOnItemTouchListenerAdapter(@NonNull GestureDetector detector) {
+        checkArgument(detector != null);
+
+        mDetector = detector;
+    }
+
+    @Override
+    public boolean onInterceptTouchEvent(@NonNull RecyclerView rv, @NonNull MotionEvent e) {
+        // Regardless of whether or not gesture detector detects something, don't
+        // consume the event stream. The concept of intercepting is alien to GestureDetector.
+        mDetector.onTouchEvent(e);
+        return false;
+    }
+
+    @Override
+    public void onTouchEvent(@NonNull RecyclerView rv, @NonNull MotionEvent e) {}
+
+    @Override
+    public void onRequestDisallowInterceptTouchEvent(boolean disallowIntercept) {}
+}

recyclerview/recyclerview-selection/src/main/java/androidx/recyclerview/selection/SelectionTracker.java
@@ -490,6 +490,7 @@ public abstract class SelectionTracker<K> {
         private BandPredicate mBandPredicate;
         private int mBandOverlayId = R.drawable.selection_band_overlay;
 
+        // TODO(b/138958244): Support resetting state in response to unknown > cancel events.
         private int[] mGestureToolTypes = new int[] {
                 MotionEvent.TOOL_TYPE_FINGER,
                 MotionEvent.TOOL_TYPE_UNKNOWN
@@ -696,16 +697,14 @@ public abstract class SelectionTracker<K> {
             // of events. If mouse handling is configured as well, the mouse input
             // related handlers will intercept mouse input events.
 
+            // EventRouter receives events for RecyclerView, dispatching to handlers
+            // registered by tool-type.
+            EventRouter eventRouter = new EventRouter();
+
             // GestureRouter is responsible for routing GestureDetector events
             // to tool-type specific handlers.
             GestureRouter<MotionInputHandler> gestureRouter = new GestureRouter<>();
             GestureDetector gestureDetector = new GestureDetector(mContext, gestureRouter);
-
-            // TouchEventRouter takes its name from RecyclerView#OnItemTouchListener.
-            // Despite "Touch" being in the name, it receives events for all types of tools.
-            // This class is responsible for routing events to tool-type specific handlers,
-            // and if not handled by a handler, on to a GestureDetector for analysis.
-            TouchEventRouter eventRouter = new TouchEventRouter(gestureDetector);
 
             // GestureSelectionHelper provides logic that interprets a combination
             // of motions and gestures in order to provide gesture driven selection support
@@ -715,6 +714,8 @@ public abstract class SelectionTracker<K> {
 
             // Finally hook the framework up to listening to recycle view events.
             mRecyclerView.addOnItemTouchListener(eventRouter);
+            mRecyclerView.addOnItemTouchListener(
+                    new GestureDetectorOnItemTouchListenerAdapter(gestureDetector));
 
             // But before you move on, there's more work to do. Event plumbing has been
             // installed, but we haven't registered any of our helpers or callbacks.
@@ -782,7 +783,7 @@ public abstract class SelectionTracker<K> {
 
             for (int toolType : mGestureToolTypes) {
                 gestureRouter.register(toolType, touchHandler);
-                eventRouter.register(toolType, gestureHelper);
+                eventRouter.set(toolType, gestureHelper);
             }
 
             // Provides high level glue for binding mouse events and gestures
@@ -826,7 +827,7 @@ public abstract class SelectionTracker<K> {
                     mDetailsLookup, mOnDragInitiatedListener, bandHelper);
 
             for (int toolType : mPointerToolTypes) {
-                eventRouter.register(toolType, pointerEventHandler);
+                eventRouter.set(toolType, pointerEventHandler);
             }
 
             return tracker;

commit 44925c8e1bdbf5ecdbc68ed21dadc0a9aeed73c6

Fix for DocumentsUI context menu regression resulting from patch 6379fa9b8695331d8b264d9df2b915845e481a50
It is still unclear to me why the context menu was not showing.
It is possible RecyclerView does something when none of the
OnItemTouchEventListeners handles an event that was causing
the context menu to be hidden.

Bug: 143494310
Test: Added new test coverage.
Change-Id: I87ca1675a350ff0a821d3147618005b4f545e36c

recyclerview/recyclerview-selection/src/main/java/androidx/recyclerview/selection/EventRouter.java
@@ -35,8 +35,6 @@ import androidx.recyclerview.widget.RecyclerView.OnItemTouchListener;
  */
 final class EventRouter implements OnItemTouchListener {
 
-    private static final String TAG = "EventRouter";
-
     private final ToolHandlerRegistry<OnItemTouchListener> mDelegates;
 
     EventRouter() {

recyclerview/recyclerview-selection/src/main/java/androidx/recyclerview/selection/GestureDetectorOnItemTouchListenerAdapter.java
@@ -30,6 +30,7 @@ import androidx.recyclerview.widget.RecyclerView;
 final class GestureDetectorOnItemTouchListenerAdapter implements RecyclerView.OnItemTouchListener {
 
     private final GestureDetector mDetector;
+
     GestureDetectorOnItemTouchListenerAdapter(@NonNull GestureDetector detector) {
         checkArgument(detector != null);
 
@@ -38,15 +39,19 @@ final class GestureDetectorOnItemTouchListenerAdapter implements RecyclerView.On
 
     @Override
     public boolean onInterceptTouchEvent(@NonNull RecyclerView rv, @NonNull MotionEvent e) {
-        // Regardless of whether or not gesture detector detects something, don't
-        // consume the event stream. The concept of intercepting is alien to GestureDetector.
-        mDetector.onTouchEvent(e);
-        return false;
+        // While the idea of "intercepting" an event stream isn't consistent
+        // with the world-view of GestureDetector, failure to return true here
+        // resulted in a bug where a context menu shown on an item view was not
+        // visible...despite returning reporting that the menu was shown.
+        // See b/143494310 for further details.
+        return mDetector.onTouchEvent(e);
     }
 
     @Override
-    public void onTouchEvent(@NonNull RecyclerView rv, @NonNull MotionEvent e) {}
+    public void onTouchEvent(@NonNull RecyclerView rv, @NonNull MotionEvent e) {
+    }
 
     @Override
-    public void onRequestDisallowInterceptTouchEvent(boolean disallowIntercept) {}
+    public void onRequestDisallowInterceptTouchEvent(boolean disallowIntercept) {
+    }
 }

recyclerview/recyclerview-selection/src/main/java/androidx/recyclerview/selection/SelectionTracker.java
@@ -94,7 +94,7 @@ public abstract class SelectionTracker<K> {
      * of changes to selection. Look for this value in the {@code payload}
      * Object argument supplied to
      * {@link RecyclerView.Adapter#onBindViewHolder
-     *     Adapter#onBindViewHolder}.
+     * Adapter#onBindViewHolder}.
      * If present the call is occurring in response to a selection state change.
      * This would be a good opportunity to animate changes between unselected and selected state.
      * When state is being restored, this argument will not be present.
@@ -144,9 +144,8 @@ public abstract class SelectionTracker<K> {
      * This affords clients the ability to restore selection from selection saved
      * in Activity state.
      *
-     * @see StorageStrategy details on selection state support.
-     *
      * @param selection selection being restored.
+     * @see StorageStrategy details on selection state support.
      */
     protected abstract void restoreSelection(@NonNull Selection<K> selection);
 
@@ -203,9 +202,9 @@ public abstract class SelectionTracker<K> {
      * (see {@link #isRangeActive()}. Items in the range [anchor, end] will be
      * selected after consulting SelectionPredicate.
      *
-     * @param position  The new end position for the selection range.
+     * @param position The new end position for the selection range.
      * @throws IllegalStateException if a range selection is not active. Range selection
-     *         must have been started by a call to {@link #startRange(int)}.
+     *                               must have been started by a call to {@link #startRange(int)}.
      * @hide
      */
     @RestrictTo(LIBRARY)
@@ -215,6 +214,7 @@ public abstract class SelectionTracker<K> {
      * Clears an in-progress range selection. Provisional range selection established
      * using {@link #extendProvisionalRange(int)} will be cleared (unless
      * {@link #mergeProvisionalSelection()} is called first.)
+     *
      * @hide
      */
     @RestrictTo(LIBRARY)
@@ -251,7 +251,7 @@ public abstract class SelectionTracker<K> {
 
     /**
      * Sets the provisional selection, replacing any existing selection.
-     * @param newSelection
+     *
      * @hide
      */
     @RestrictTo(LIBRARY)
@@ -259,6 +259,7 @@ public abstract class SelectionTracker<K> {
 
     /**
      * Clears any existing provisional selection
+     *
      * @hide
      */
     @RestrictTo(LIBRARY)
@@ -267,6 +268,7 @@ public abstract class SelectionTracker<K> {
     /**
      * Converts the provisional selection into primary selection, then clears
      * provisional selection.
+     *
      * @hide
      */
     @RestrictTo(LIBRARY)
@@ -338,7 +340,7 @@ public abstract class SelectionTracker<K> {
         /**
          * Validates a change to selection for a specific key.
          *
-         * @param key the item key
+         * @param key       the item key
          * @param nextState the next potential selected/unselected state
          * @return true if the item at {@code id} can be set to {@code nextState}.
          */
@@ -348,7 +350,7 @@ public abstract class SelectionTracker<K> {
          * Validates a change to selection for a specific position. If necessary
          * use {@link ItemKeyProvider} to identy associated key.
          *
-         * @param position the item position
+         * @param position  the item position
          * @param nextState the next potential selected/unselected state
          * @return true if the item at {@code id} can be set to {@code nextState}.
          */
@@ -384,7 +386,7 @@ public abstract class SelectionTracker<K> {
      *        new MyDetailsLookup(recyclerView),
      *        StorageStrategy.createParcelableStorage(Uri.class))
      *        .build();
-     *</pre>
+     * </pre>
      *
      * <p>
      * <b>Restricting which items can be selected and limiting selection size</b>
@@ -399,14 +401,14 @@ public abstract class SelectionTracker<K> {
      * by supplying {@link SelectionPredicates#createSelectSingleAnything()}.
      *
      * SelectionTracker<String> tracker = new SelectionTracker.Builder<>(
-     *        "my-string-selection",
-     *        recyclerView,
-     *        new DemoStableIdProvider(recyclerView.getAdapter()),
-     *        new MyDetailsLookup(recyclerView),
-     *        StorageStrategy.createStringStorage())
-     *        .withSelectionPredicate(SelectionPredicates#createSelectSingleAnything())
-     *        .build();
-     *</pre>
+     * "my-string-selection",
+     * recyclerView,
+     * new DemoStableIdProvider(recyclerView.getAdapter()),
+     * new MyDetailsLookup(recyclerView),
+     * StorageStrategy.createStringStorage())
+     * .withSelectionPredicate(SelectionPredicates#createSelectSingleAnything())
+     * .build();
+     * </pre>
      * <p>
      * <b>Retaining state across Android lifecycle events</b>
      *
@@ -447,25 +449,25 @@ public abstract class SelectionTracker<K> {
      * private SelectionTracker<Uri> mTracker;
      *
      * public void onCreate(Bundle savedInstanceState) {
-     *   // See above for details on constructing a SelectionTracker instance.
+     * // See above for details on constructing a SelectionTracker instance.
      *
-     *   if (savedInstanceState != null) {
-     *      mTracker.onRestoreInstanceState(savedInstanceState);
-     *   }
+     * if (savedInstanceState != null) {
+     * mTracker.onRestoreInstanceState(savedInstanceState);
+     * }
      * }
      *
      * protected void onSaveInstanceState(Bundle outState) {
-     *     super.onSaveInstanceState(outState);
-     *     mTracker.onSaveInstanceState(outState);
+     * super.onSaveInstanceState(outState);
+     * mTracker.onSaveInstanceState(outState);
      * }
      * </pre>
      *
      * @param <K> Selection key type. Built in support is provided for {@link String},
-     *           {@link Long}, and {@link Parcelable}. {@link StorageStrategy}
-     *           provides factory methods for each type:
-     *           {@link StorageStrategy#createStringStorage()},
-     *           {@link StorageStrategy#createParcelableStorage(Class)},
-     *           {@link StorageStrategy#createLongStorage()}
+     *            {@link Long}, and {@link Parcelable}. {@link StorageStrategy}
+     *            provides factory methods for each type:
+     *            {@link StorageStrategy#createStringStorage()},
+     *            {@link StorageStrategy#createParcelableStorage(Class)},
+     *            {@link StorageStrategy#createLongStorage()}
      */
     public static final class Builder<K> {
 
@@ -491,12 +493,12 @@ public abstract class SelectionTracker<K> {
         private int mBandOverlayId = R.drawable.selection_band_overlay;
 
         // TODO(b/138958244): Support resetting state in response to unknown > cancel events.
-        private int[] mGestureToolTypes = new int[] {
+        private int[] mGestureToolTypes = new int[]{
                 MotionEvent.TOOL_TYPE_FINGER,
                 MotionEvent.TOOL_TYPE_UNKNOWN
         };
 
-        private int[] mPointerToolTypes = new int[] {
+        private int[] mPointerToolTypes = new int[]{
                 MotionEvent.TOOL_TYPE_MOUSE
         };
 
@@ -504,13 +506,13 @@ public abstract class SelectionTracker<K> {
          * Creates a new SelectionTracker.Builder useful for configuring and creating
          * a new SelectionTracker for use with your {@link RecyclerView}.
          *
-         * @param selectionId A unique string identifying this selection in the context
-         *        of the activity or fragment.
-         * @param recyclerView the owning RecyclerView
-         * @param keyProvider the source of selection keys
+         * @param selectionId   A unique string identifying this selection in the context
+         *                      of the activity or fragment.
+         * @param recyclerView  the owning RecyclerView
+         * @param keyProvider   the source of selection keys
          * @param detailsLookup the source of information about RecyclerView items.
-         * @param storage Strategy for type-safe storage of selection state in
-         *        {@link Bundle}.
+         * @param storage       Strategy for type-safe storage of selection state in
+         *                      {@link Bundle}.
          */
         public Builder(
                 @NonNull String selectionId,
@@ -641,7 +643,6 @@ public abstract class SelectionTracker<K> {
         /**
          * Replaces default band overlay.
          *
-         * @param bandOverlayId
          * @return this
          */
         public Builder<K> withBandOverlay(@DrawableRes int bandOverlayId) {
@@ -651,7 +652,7 @@ public abstract class SelectionTracker<K> {
 
         /**
          * Replaces default band predicate.
-         * @param bandPredicate
+         *
          * @return this
          */
         public Builder<K> withBandPredicate(@NonNull BandPredicate bandPredicate) {
@@ -697,10 +698,6 @@ public abstract class SelectionTracker<K> {
             // of events. If mouse handling is configured as well, the mouse input
             // related handlers will intercept mouse input events.
 
-            // EventRouter receives events for RecyclerView, dispatching to handlers
-            // registered by tool-type.
-            EventRouter eventRouter = new EventRouter();
-
             // GestureRouter is responsible for routing GestureDetector events
             // to tool-type specific handlers.
             GestureRouter<MotionInputHandler<K>> gestureRouter = new GestureRouter<>();
@@ -712,7 +709,11 @@ public abstract class SelectionTracker<K> {
             final GestureSelectionHelper gestureHelper = GestureSelectionHelper.create(
                     tracker, mSelectionPredicate, mRecyclerView, scroller, mMonitor);
 
-            // Finally hook the framework up to listening to recycle view events.
+            // EventRouter receives events for RecyclerView, dispatching to handlers
+            // registered by tool-type.
+            EventRouter eventRouter = new EventRouter();
+
+            // Finally hook the framework up to listening to RecycleView events.
             mRecyclerView.addOnItemTouchListener(eventRouter);
             mRecyclerView.addOnItemTouchListener(
                     new GestureDetectorOnItemTouchListenerAdapter(gestureDetector));

recyclerview/recyclerview-selection/src/androidTest/java/androidx/recyclerview/selection/GestureDetectorOnItemTouchListenerAdapterTest.java
@@ -1,0 +1,72 @@
+/*
+ * Copyright 2019 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package androidx.recyclerview.selection;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import android.os.Looper;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
+
+import androidx.recyclerview.selection.testing.TestEvents;
+import androidx.test.core.app.ApplicationProvider;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.filters.SmallTest;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+
+@RunWith(AndroidJUnit4.class)
+@SmallTest
+public final class GestureDetectorOnItemTouchListenerAdapterTest {
+
+    private GestureDetectorOnItemTouchListenerAdapter mAdapter;
+    private GestureDetector mDetector;
+    private TestOnGestureListener mListener;
+
+    @Before
+    public void setUp() {
+        if (Looper.myLooper() == null) {
+            Looper.prepare();
+        }
+        mListener = new TestOnGestureListener();
+        mDetector = new GestureDetector(ApplicationProvider.getApplicationContext(), mListener);
+        mAdapter = new GestureDetectorOnItemTouchListenerAdapter(mDetector);
+    }
+
+    @Test
+    public void testReflectsGestureDetectorReturnValue() {
+        assertFalse(mDetector.onTouchEvent(TestEvents.Mouse.SECONDARY_CLICK));
+        assertFalse(mAdapter.onInterceptTouchEvent(null, TestEvents.Mouse.SECONDARY_CLICK));
+
+        mListener.mReturnValue = true;
+        assertTrue(mDetector.onTouchEvent(TestEvents.Mouse.SECONDARY_CLICK));
+        assertTrue(mAdapter.onInterceptTouchEvent(null, TestEvents.Mouse.SECONDARY_CLICK));
+    }
+
+    private static class TestOnGestureListener extends GestureDetector.SimpleOnGestureListener {
+        boolean mReturnValue;
+
+        @Override
+        public boolean onDown(MotionEvent e) {
+            return mReturnValue;
+        }
+    }
+}

recyclerview/recyclerview-selection/src/androidTest/java/androidx/recyclerview/selection/testing/TestEvents.java
@@ -38,18 +38,42 @@ public final class TestEvents {
      * Common mouse event types...for your convenience.
      */
     public static final class Mouse {
-        public static final MotionEvent CLICK =
-                TestEvents.builder().mouse().primary().build();
-        public static final MotionEvent CTRL_CLICK =
-                TestEvents.builder().mouse().primary().ctrl().build();
-        public static final MotionEvent ALT_CLICK =
-                TestEvents.builder().mouse().primary().alt().build();
-        public static final MotionEvent SHIFT_CLICK =
-                TestEvents.builder().mouse().primary().shift().build();
-        public static final MotionEvent SECONDARY_CLICK =
-                TestEvents.builder().mouse().secondary().build();
-        public static final MotionEvent TERTIARY_CLICK =
-                TestEvents.builder().mouse().tertiary().build();
+        public static final MotionEvent CLICK = TestEvents.builder()
+                .mouse()
+                .primary()
+                // Note that when using a BT mouse
+                // right-click action is ACTION_DOWN, not
+                // ACTION_POINTER_DOWN as one might expect.
+                .action(MotionEvent.ACTION_DOWN)
+                .build();
+        public static final MotionEvent CTRL_CLICK = TestEvents.builder()
+                .mouse()
+                .primary()
+                .down()
+                .ctrl()
+                .build();
+        public static final MotionEvent ALT_CLICK = TestEvents.builder()
+                .mouse()
+                .primary()
+                .down()
+                .alt()
+                .build();
+        public static final MotionEvent SHIFT_CLICK = TestEvents.builder()
+                .mouse()
+                .primary()
+                .down()
+                .shift()
+                .build();
+        public static final MotionEvent SECONDARY_CLICK = TestEvents.builder()
+                .mouse()
+                .secondary()
+                .down()
+                .build();
+        public static final MotionEvent TERTIARY_CLICK = TestEvents.builder()
+                .mouse()
+                .tertiary()
+                .down()
+                .build();
     }
 
     /**
@@ -66,10 +90,11 @@ public final class TestEvents {
     @IntDef(flag = true, value = {
             MotionEvent.ACTION_DOWN,
             MotionEvent.ACTION_MOVE,
-            MotionEvent.ACTION_UP
+            MotionEvent.ACTION_UP,
     })
     @Retention(RetentionPolicy.SOURCE)
-    public @interface Action {}
+    public @interface Action {
+    }
 
     // Add other types from MotionEvent.TOOL_TYPE_ as needed.
     @IntDef(flag = true, value = {
@@ -79,21 +104,24 @@ public final class TestEvents {
             MotionEvent.TOOL_TYPE_UNKNOWN
     })
     @Retention(RetentionPolicy.SOURCE)
-    public @interface ToolType {}
+    public @interface ToolType {
+    }
 
     @IntDef(flag = true, value = {
             MotionEvent.BUTTON_PRIMARY,
             MotionEvent.BUTTON_SECONDARY
     })
     @Retention(RetentionPolicy.SOURCE)
-    public @interface Button {}
+    public @interface Button {
+    }
 
     @IntDef(flag = true, value = {
             KeyEvent.META_SHIFT_ON,
             KeyEvent.META_CTRL_ON
     })
     @Retention(RetentionPolicy.SOURCE)
-    public @interface Key {}
+    public @interface Key {
+    }
 
     private static final class State {
         private @Action int mAction = ACTION_UNSET;
@@ -118,7 +146,6 @@ public final class TestEvents {
 
         /**
          * @param action Any action specified in {@link MotionEvent}.
-         * @return
          */
         public Builder action(int action) {
             mState.mAction = action;
@@ -182,6 +209,17 @@ public final class TestEvents {
             for (int key : keys) {
                 mState.mKeys.remove(key);
             }
+            return this;
+        }
+
+        /**
+         * Sets action to MotionEvent#ACTION_DOWN which can be used
+         * with most tool types including mouse.
+         *
+         * <p>NOTE: ACTION_POINTER_DOWN is used for secondary pointers.
+         */
+        public Builder down() {
+            action(MotionEvent.ACTION_DOWN);
             return this;
         }
 
@@ -273,7 +311,7 @@ public final class TestEvents {
                     0,     // edge flags
                     0,     // int source,
                     0      // int flags
-                    );
+            );
         }
     }
 }