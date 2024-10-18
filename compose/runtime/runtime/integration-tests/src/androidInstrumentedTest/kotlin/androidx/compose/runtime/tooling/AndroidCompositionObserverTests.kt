21a8d9e093cf52b25b5606f954f69d2b29bd1cfd
Andrew Bailey <anbailey@google.com>
2024-10-18 16:47:18 -0400

    Add CompositionRegistrationObserver API
    This CL introduces a new tracing API requested by TikTok (but not
    /that/ TikTok).
    
    The introduced API allows for observing a Recomposer to receive a
    callback when a new composition is created within it. This makes it
    easier to automatically install tooling into subcompositions or
    in interwoven interop use cases, for example.
    
    Fixes: b/366038833
    Test: RecomposerObserverTest
    Relnote: """
        Adds tooling APIs to observe the creation of new compositions within
        a Recomposer
        """
    Change-Id: Iaeb7e218aa949ebf1c061b311a3c4383cf5c2728

--- compose/runtime/runtime/integration-tests/src/androidInstrumentedTest/kotlin/androidx/compose/runtime/tooling/AndroidCompositionObserverTests.kt
+++ compose/runtime/runtime/integration-tests/src/androidInstrumentedTest/kotlin/androidx/compose/runtime/tooling/AndroidCompositionObserverTests.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2023 The Android Open Source Project
+ * Copyright 2024 The Android Open Source Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,11 +14,13 @@
  * limitations under the License.
  */
 
-package androidx.compose.runtime
+package androidx.compose.runtime.tooling
 
-import androidx.compose.runtime.tooling.CompositionObserver
-import androidx.compose.runtime.tooling.CompositionObserverHandle
-import androidx.compose.runtime.tooling.observe
+import androidx.compose.runtime.BaseComposeTest
+import androidx.compose.runtime.Composition
+import androidx.compose.runtime.ExperimentalComposeRuntimeApi
+import androidx.compose.runtime.RecomposeScope
+import androidx.compose.runtime.makeTestActivityRule
 import androidx.compose.ui.R
 import androidx.compose.ui.platform.LocalView
 import androidx.test.ext.junit.runners.AndroidJUnit4
