a949b535ac54f50df66343efbc0b1bd44e72b604
Fred Sladkey <fsladkey@google.com>
2024-10-30 11:02:45 -0400

    Add script to wrap project creator in virtualenv
    Test: ./development/project_creator/create_project.sh androidx.foo foo-bar
    Change-Id: I0e290bd27e3b7d263db00c877d6a972f75ae7753

---
+++ development/project-creator/create_project.sh
@@ -0,0 +1,27 @@
+SCRIPT_DIR="$(cd $(dirname $0) && pwd)"
+
+if [ "$(uname)" = "Darwin" ]; then
+  VIRTUAL_ENV_INSTALL_COMMAND="pip3 install virtualenv"
+  else
+  VIRTUAL_ENV_INSTALL_COMMAND="sudo apt-get install virtualenv python3-venv"
+fi
+
+
+# check if virtualenv is installed
+if !(pyenv_version=$(virtualenv --version > /dev/null 2>&1)); then
+  echo "virtualenv is not installed. Please install with '$VIRTUAL_ENV_INSTALL_COMMAND'"
+  exit 1
+fi
+
+# create virtualenv
+virtualenv androidx_project_creator
+
+# install necessary tools
+androidx_project_creator/bin/pip3 install setuptools toml
+
+# run project creator
+androidx_project_creator/bin/python3 $SCRIPT_DIR/create_project.py "$@"
+
+
+# clean up virtualenv directory
+rm -rf ./androidx-project_creator
