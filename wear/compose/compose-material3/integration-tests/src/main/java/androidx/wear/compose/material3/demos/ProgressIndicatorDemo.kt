48dfb5ebfac8d6d7d6c927dab86da0d8e69fbeb4
Katrina Paikena <katrinapaikena@google.com>
2024-11-07 17:15:34 +0000

    LinearProgressIndicator motion.
    Screen recording of the animation: https://drive.google.com/file/d/1TfxVgYqtSzC-di01O2-pnVk4KiOmRcQi/view?usp=sharing&resourcekey=0-eK8HUAay-pL3Y5V1aqqnxg
    
    Bug: 376213138
    Test: Added new  screenshot tests and new animated LinearProgressIndicator demo.
    Relnote: "Added motion to material3 LinearProgressIndicator. Added
    LinearProgressIndicatorContent without animations."
    
    Change-Id: Idee9941c34444a2a695a0ab2171306085b7d5abd

--- wear/compose/compose-material3/integration-tests/src/main/java/androidx/wear/compose/material3/demos/ProgressIndicatorDemo.kt
+++ wear/compose/compose-material3/integration-tests/src/main/java/androidx/wear/compose/material3/demos/ProgressIndicatorDemo.kt
@@ -26,10 +26,12 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableFloatStateOf
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -45,12 +47,14 @@ import androidx.wear.compose.material3.Button
 import androidx.wear.compose.material3.CircularProgressIndicator
 import androidx.wear.compose.material3.CircularProgressIndicatorDefaults
 import androidx.wear.compose.material3.IconButtonDefaults
+import androidx.wear.compose.material3.LinearProgressIndicator
 import androidx.wear.compose.material3.ListHeader
 import androidx.wear.compose.material3.MaterialTheme
 import androidx.wear.compose.material3.ProgressIndicatorDefaults
 import androidx.wear.compose.material3.SegmentedCircularProgressIndicator
 import androidx.wear.compose.material3.Slider
 import androidx.wear.compose.material3.SliderDefaults
+import androidx.wear.compose.material3.Stepper
 import androidx.wear.compose.material3.SwitchButton
 import androidx.wear.compose.material3.Text
 import androidx.wear.compose.material3.samples.FullScreenProgressIndicatorSample
@@ -130,7 +134,17 @@ val ProgressIndicatorDemos =
                 },
             )
         ),
-        ComposableDemo("Linear progress") { Centralize { LinearProgressIndicatorSamples() } },
+        Material3DemoCategory(
+            title = "Linear progress",
+            listOf(
+                ComposableDemo("Linear Samples") {
+                    Centralize { LinearProgressIndicatorSamples() }
+                },
+                ComposableDemo("Animation") {
+                    Centralize { LinearProgressIndicatorAnimatedDemo() }
+                },
+            )
+        ),
         Material3DemoCategory(
             title = "Arc Progress Indicator",
             listOf(
@@ -547,6 +561,35 @@ fun ArcIndicatorCustomizer(
                 onCheckedChange = { hasCustomColors.value = it },
                 label = { Text("Custom colors") },
             )
+        }
+    }
+}
+
+@Composable
+fun LinearProgressIndicatorAnimatedDemo() {
+    var progress by remember { mutableFloatStateOf(0.25f) }
+    val valueRange = 0f..1f
+
+    Box(modifier = Modifier.fillMaxSize()) {
+        Stepper(
+            value = progress,
+            onValueChange = { progress = it },
+            valueRange = valueRange,
+            steps = 3,
+        ) {
+            ScalingLazyColumn(
+                modifier = Modifier.fillMaxSize().padding(horizontal = 12.dp),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                item { Text(String.format("Progress: %.0f%%", progress * 100)) }
+                item {
+                    LinearProgressIndicator(
+                        modifier = Modifier.padding(top = 8.dp),
+                        progress = { progress }
+                    )
+                }
+            }
         }
     }
 }
