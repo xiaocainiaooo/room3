d511bf15679abd14f288fcfa0ab603a294942858
Rahul Ravikumar <rahulrav@google.com>
2024-11-07 15:09:35 -0800

    Update `plot-benchmarks` to use Svelte 5.0.
    Test: Ran the app locally.
    Change-Id: I7f0ed08b67bce54af608919cd621d7eef00e76bd

--- development/plot-benchmarks/src/main.ts
+++ development/plot-benchmarks/src/main.ts
@@ -1,8 +1,7 @@
+import { mount } from 'svelte';
 import './assets/overrides.css';
 import App from './lib/App.svelte';
 
-const app = new App({
-  target: document.getElementById('app')!!,
-});
+const app = mount(App, { target: document.getElementById("app")!! });
 
 export default app;
