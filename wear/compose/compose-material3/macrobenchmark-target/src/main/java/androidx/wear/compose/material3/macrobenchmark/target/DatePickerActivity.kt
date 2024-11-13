a117d90bf41bc1a95a2a03c4d3e538f1f7c3d87c
Tianming Xu <tianmingxu@google.com>
2024-11-13 14:33:40 +0000

    Add macrobenchmark for TimePicker and DatePicker.
    Bug: 377458209
    Test: Test only
    Change-Id: Iddc00db87476d5423e7f68a8c6a25a45a613d791

--- wear/compose/compose-material3/macrobenchmark-target/src/main/java/androidx/wear/compose/material3/macrobenchmark/target/DatePickerActivity.kt
+++ wear/compose/compose-material3/macrobenchmark-target/src/main/java/androidx/wear/compose/material3/macrobenchmark/target/DatePickerActivity.kt
@@ -18,7 +18,7 @@ package androidx.wear.compose.material3.macrobenchmark.target
 
 import android.os.Build
 import androidx.annotation.RequiresApi
-import androidx.wear.compose.material3.macrobenchmark.common.AnimatedTextBenchmark
+import androidx.wear.compose.material3.macrobenchmark.common.DatePickerBenchmark
 
-@RequiresApi(Build.VERSION_CODES.S)
-class AnimatedTextActivity : BenchmarkBaseActivity(AnimatedTextBenchmark)
+@RequiresApi(Build.VERSION_CODES.O)
+class DatePickerActivity : BenchmarkBaseActivity(DatePickerBenchmark)
