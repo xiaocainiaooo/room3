713719a4b05750da6461796093e17e8109dae171
rahulkanojia <rahulkanojia@google.com>
2024-11-19 13:52:45 +0000

    Added initial PdfViewerFragment setup with all public API support
    - Added all public APIs as currently live.
    - Created fragment ui states and exposed it using state-flow to update ui in reactive manner.
    - Saved data using Saved State Handle in viewmodel, re-triggered flows upon process death.
    - Entry point to test PdfViewerFragmentV2.
    
    Test: Manually tested sample app
    Bug: 379053040
    Change-Id: I0bada243f4a81851acac53cc4cd3034dde6ba282

---
+++ pdf/pdf-viewer-fragment/src/androidTest/kotlin/androidx/pdf/viewer/fragment/PdfDocumentViewModelTest.kt
@@ -0,0 +1,115 @@
+/*
+ * Copyright 2024 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package androidx.pdf.viewer.fragment
+
+import androidx.lifecycle.SavedStateHandle
+import androidx.pdf.SandboxedPdfLoader
+import androidx.pdf.viewer.coroutines.toListDuring
+import androidx.pdf.viewer.fragment.TestUtils.openFileAsUri
+import androidx.pdf.viewer.fragment.model.PdfFragmentUiState
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.LargeTest
+import androidx.test.platform.app.InstrumentationRegistry
+import java.util.concurrent.Executors
+import kotlinx.coroutines.asCoroutineDispatcher
+import kotlinx.coroutines.flow.take
+import kotlinx.coroutines.flow.toList
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.test.runTest
+import org.junit.Assert.assertTrue
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+@LargeTest
+class PdfDocumentViewModelTest {
+
+    private val appContext =
+        InstrumentationRegistry.getInstrumentation().targetContext.applicationContext
+    private lateinit var pdfDocumentViewModel: PdfDocumentViewModel
+    val dispatcher = Executors.newSingleThreadExecutor().asCoroutineDispatcher()
+
+    @Before
+    fun setup() {
+        val savedStateHandle = SavedStateHandle()
+
+        pdfDocumentViewModel =
+            PdfDocumentViewModel(savedStateHandle, SandboxedPdfLoader(appContext, dispatcher))
+    }
+
+    @Test
+    fun test_pdfDocumentViewModel_loadDocumentSuccess() = runTest {
+        val documentUri = openFileAsUri(appContext, "sample.pdf")
+
+        pdfDocumentViewModel.loadDocument(uri = documentUri, password = null)
+
+        backgroundScope.launch {
+            // Collect Ui states
+            val uiStates = pdfDocumentViewModel.fragmentUiScreenState.toListDuring(200)
+            // Since we've selected a error-free unprotected pdf,
+            // ideally there should only 2 states transitions.
+            assertTrue(uiStates.size == 2)
+            // Assert the first state emitted was loading
+            assertTrue(uiStates.first() is PdfFragmentUiState.Loading)
+            // Assert the first state emitted was Document load
+            assertTrue(uiStates.last() is PdfFragmentUiState.DocumentLoaded)
+        }
+    }
+
+    @Test
+    fun test_pdfDocumentViewModel_loadDocumentSuccess_withStateSaved() = runTest {
+        val documentUri = openFileAsUri(appContext, "sample.pdf")
+        // Save document uri in savedStateHandle and inject in viewmodel
+        val savedState = SavedStateHandle().also { it["documentUri"] = documentUri }
+        // On init, pdfViewModel will try to load document again as documentUri != null
+        val pdfViewModel =
+            PdfDocumentViewModel(savedState, SandboxedPdfLoader(appContext, dispatcher))
+
+        backgroundScope.launch {
+            val uiStates = pdfViewModel.fragmentUiScreenState.toListDuring(200)
+
+            // Assert there are only 2 state transitions
+            assertTrue(uiStates.size == 2)
+
+            // Assert the first state emitted was loading
+            assertTrue(uiStates.first() is PdfFragmentUiState.Loading)
+            // Assert the first state emitted was Document load
+            assertTrue(uiStates.last() is PdfFragmentUiState.DocumentLoaded)
+        }
+    }
+
+    @Test
+    fun test_pdfDocumentViewModel_loadDocumentNotTriggerForSameUri() = runTest {
+        val documentUri = openFileAsUri(appContext, "sample.pdf")
+        // Save document uri in savedStateHandle and inject in viewmodel
+        val savedState = SavedStateHandle().also { it["documentUri"] = documentUri }
+        // On init, pdfViewModel will try to load document again as documentUri != null
+        val pdfViewModel =
+            PdfDocumentViewModel(savedState, SandboxedPdfLoader(appContext, dispatcher))
+        // Ignore the first 2 fragmentUiStates, as it's the first load
+        pdfViewModel.fragmentUiScreenState.take(2).toList()
+        // try loading the document again with same uri
+        pdfViewModel.loadDocument(uri = documentUri, password = null)
+
+        // Assert fragmentUiState never set to Loading
+        assertTrue(pdfViewModel.fragmentUiScreenState.value !is PdfFragmentUiState.Loading)
+    }
+
+    // TODO: Add tests for password-protected pdf and corrupted pdf after test-artifact b/379743760
+
+}
