d304a9d29625a76c8ed44c9259d934b1c44900f9
mkulaha <mkulaha@google.com>
2024-12-03 16:39:16 +0000

    Fix vertical jitter on AlertDialogs
    Bug: 380901500
    Test: Manually verified
    Change-Id: I529c476a38fa14b28077c32e8c761cb5416da692

--- wear/compose/compose-material3/src/main/java/androidx/wear/compose/material3/Dialog.kt
+++ wear/compose/compose-material3/src/main/java/androidx/wear/compose/material3/Dialog.kt
@@ -33,6 +33,7 @@ import androidx.compose.runtime.rememberUpdatedState
 import androidx.compose.runtime.setValue
 import androidx.compose.runtime.snapshotFlow
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.CompositingStrategy
 import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.platform.LocalView
 import androidx.compose.ui.window.DialogProperties
@@ -129,6 +130,9 @@ fun Dialog(
                         modifier =
                             Modifier.matchParentSize()
                                 .background(MaterialTheme.colorScheme.background)
+                                .graphicsLayer {
+                                    compositingStrategy = CompositingStrategy.Offscreen
+                                }
                     ) {
                         content()
                     }
