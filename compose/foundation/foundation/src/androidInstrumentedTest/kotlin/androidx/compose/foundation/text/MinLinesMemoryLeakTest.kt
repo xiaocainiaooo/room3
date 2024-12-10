0f9ff87b32510229fa1a1f26f4e2a6360a45839f
Louis Pullen-Freilich <lpf@google.com>
2024-12-10 18:37:09 +0000

    Ignores platform memory leak
    This was causing this test to flake, but the reported leak is out of control
    
    Fixes: b/362368117
    Test: MinLinesMemoryLeakTest
    Change-Id: I87f349d0bc5cbd39a48b04d4c2c45070b6675353

--- compose/foundation/foundation/src/androidInstrumentedTest/kotlin/androidx/compose/foundation/text/MinLinesMemoryLeakTest.kt
+++ compose/foundation/foundation/src/androidInstrumentedTest/kotlin/androidx/compose/foundation/text/MinLinesMemoryLeakTest.kt
@@ -19,11 +19,15 @@ package androidx.compose.foundation.text
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.test.filters.LargeTest
 import leakcanary.DetectLeaksAfterTestSuccess
+import leakcanary.LeakCanary
+import org.junit.AfterClass
+import org.junit.BeforeClass
 import org.junit.Rule
 import org.junit.Test
 import org.junit.rules.RuleChain
 import org.junit.runner.RunWith
 import org.junit.runners.Parameterized
+import shark.AndroidReferenceMatchers
 
 @RunWith(Parameterized::class)
 @LargeTest
@@ -32,6 +36,32 @@ class MinLinesMemoryLeakTest(private val numLines: Int) {
         @JvmStatic
         @Parameterized.Parameters
         fun data() = arrayOf(1, 2, 100) // potential when numLines greater than 1
+
+        // Config is a global static instance, so make sure to reset it after this test to prevent
+        // issues for other tests that use leak canary
+        private lateinit var savedLeakCanaryConfig: LeakCanary.Config
+
+        private val IgnoreFrameTrackerLeak =
+            AndroidReferenceMatchers.instanceFieldLeak(
+                className = "com.android.internal.jank.FrameTracker",
+                fieldName = "mConfig",
+                "Ignoring a leak due to misconfigured framework jank tracking b/349355283"
+            )
+
+        @JvmStatic
+        @BeforeClass
+        fun configureLeakCanaryReporting() {
+            val current = LeakCanary.config
+            savedLeakCanaryConfig = current
+            LeakCanary.config =
+                current.copy(referenceMatchers = current.referenceMatchers + IgnoreFrameTrackerLeak)
+        }
+
+        @JvmStatic
+        @AfterClass
+        fun cleanupLeakCanaryReporting() {
+            LeakCanary.config = savedLeakCanaryConfig
+        }
     }
 
     private val composeTestRule = createComposeRule()
