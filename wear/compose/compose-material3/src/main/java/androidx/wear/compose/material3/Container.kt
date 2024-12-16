e72bad21d6c68e9e1cc5f6c5cb74bfe455104668
Katrina Paikena <katrinapaikena@google.com>
2024-12-16 17:00:18 +0000

    Enable animations for Button text updates.
    Fixed issue with 'animateContentSize' not working with
    material3 Button.Previously the Button was not animating the height because we were
    using IntrinsicSize for height. Also switched to custom implementation
    of 'paint' modifier, to make 'paint' work correctly without intrinsic
    size..
    
    Bug: 345201131
    Test: Added unit test
    Relnote: "Fixed issue with 'animateContentSize' not working with
    material3 Button."
    
    Change-Id: Ib18a0fccadfec00160612b9491036eb153b151de

--- wear/compose/compose-material3/src/main/java/androidx/wear/compose/material3/Container.kt
+++ wear/compose/compose-material3/src/main/java/androidx/wear/compose/material3/Container.kt
@@ -20,15 +20,25 @@ import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.draw.paint
+import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.graphics.drawscope.ContentDrawScope
+import androidx.compose.ui.graphics.drawscope.translate
 import androidx.compose.ui.graphics.painter.ColorPainter
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.layout.times
+import androidx.compose.ui.node.DrawModifierNode
+import androidx.compose.ui.node.ModifierNodeElement
+import androidx.compose.ui.platform.InspectorInfo
+import androidx.compose.ui.unit.IntSize
+import androidx.compose.ui.util.fastIsFinite
+import androidx.compose.ui.util.fastRoundToInt
 import androidx.wear.compose.foundation.LocalReduceMotion
 import androidx.wear.compose.foundation.lazy.LocalTransformingLazyColumnItemScope
 import androidx.wear.compose.material3.lazy.scrollTransform
@@ -65,7 +75,7 @@ internal fun Modifier.container(
     return itemScope?.let { tlcScope -> scrollTransform(tlcScope, shape, painter, border) }
         ?: borderModifier
             .clip(shape = shape)
-            .paint(painter = painter, contentScale = ContentScale.Crop)
+            .paintBackground(painter = painter, contentScale = ContentScale.Crop)
 }
 
 /**
@@ -100,4 +110,98 @@ internal fun Modifier.container(
     return itemScope?.let { tlcScope ->
         scrollTransform(tlcScope, shape, ColorPainter(color), border)
     } ?: borderModifier.clip(shape = shape).background(color = color)
+}
+
+/**
+ * Paint the background using [Painter].
+ *
+ * This modifier simply paints the background, without modifying the size.
+ *
+ * @param painter [Painter] to be drawn by this [Modifier]
+ * @param alignment specifies alignment of the [painter] relative to content
+ * @param contentScale strategy for scaling [painter] if its size does not match the content size
+ */
+internal fun Modifier.paintBackground(
+    painter: Painter,
+    alignment: Alignment = Alignment.Center,
+    contentScale: ContentScale = ContentScale.Inside,
+) = this then PainterElement(painter = painter, alignment = alignment, contentScale = contentScale)
+
+private data class PainterElement(
+    val painter: Painter,
+    val contentScale: ContentScale,
+    var alignment: Alignment = Alignment.Center,
+) : ModifierNodeElement<PainterNode>() {
+    override fun create(): PainterNode {
+        return PainterNode(painter = painter, alignment = alignment, contentScale = contentScale)
+    }
+
+    override fun update(node: PainterNode) {
+        node.painter = painter
+    }
+
+    override fun InspectorInfo.inspectableProperties() {
+        name = "paint"
+        properties["painter"] = painter
+        properties["alignment"] = alignment
+        properties["contentScale"] = contentScale
+    }
+}
+
+private class PainterNode(
+    var painter: Painter,
+    var alignment: Alignment = Alignment.Center,
+    val contentScale: ContentScale,
+) : Modifier.Node(), DrawModifierNode {
+
+    override fun ContentDrawScope.draw() {
+        val intrinsicSize = painter.intrinsicSize
+        val srcWidth =
+            if (intrinsicSize.hasSpecifiedAndFiniteWidth()) {
+                intrinsicSize.width
+            } else {
+                size.width
+            }
+
+        val srcHeight =
+            if (intrinsicSize.hasSpecifiedAndFiniteHeight()) {
+                intrinsicSize.height
+            } else {
+                size.height
+            }
+
+        val srcSize = Size(srcWidth, srcHeight)
+        val scaledSize =
+            if (size.width != 0f && size.height != 0f) {
+                srcSize * contentScale.computeScaleFactor(srcSize, size)
+            } else {
+                Size.Zero
+            }
+
+        val alignedPosition =
+            alignment.align(
+                IntSize(scaledSize.width.fastRoundToInt(), scaledSize.height.fastRoundToInt()),
+                IntSize(size.width.fastRoundToInt(), size.height.fastRoundToInt()),
+                layoutDirection
+            )
+
+        val dx = alignedPosition.x.toFloat()
+        val dy = alignedPosition.y.toFloat()
+
+        translate(dx, dy) { with(painter) { draw(size = scaledSize) } }
+
+        // Maintain the same pattern as Modifier.drawBehind to allow chaining of DrawModifiers
+        drawContent()
+    }
+
+    private fun Size.hasSpecifiedAndFiniteWidth() = this != Size.Unspecified && width.fastIsFinite()
+
+    private fun Size.hasSpecifiedAndFiniteHeight() =
+        this != Size.Unspecified && height.fastIsFinite()
+
+    override fun toString(): String =
+        "PainterModifier(" +
+            "painter=$painter, " +
+            "alignment=$alignment, " +
+            "contentScale=$contentScale)"
 }
