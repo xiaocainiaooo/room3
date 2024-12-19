8d50e19de28f41a971761695b18bff7ed3d8801e
Jelle Fresen <jellefresen@google.com>
2024-12-19 18:00:21 +0000

    Avoid thread switches when getting SemanticsNodes
    TestOwner.getAllSemanticsNodes switches to the main thread in order to
    call SemanticsOwner.getAllSemanticsNodes. When it returns the result (on
    the test thread), every caller continues with switching back to the main
    thread to process the result. This is inefficient and could lead to race
    conditions. Solve this by adding a transformation callback to
    TestOwner.getAllSemanticsNodes so that processing can be done in the
    same main thread execution.
    
    Fixes: 376892469
    Test: presubmit
    Change-Id: Ie17774775b77a65e8195ef8bb50714fd6f2cf407

--- compose/ui/ui-test/src/commonMain/kotlin/androidx/compose/ui/test/SemanticsNodeInteraction.kt
+++ compose/ui/ui-test/src/commonMain/kotlin/androidx/compose/ui/test/SemanticsNodeInteraction.kt
@@ -60,17 +60,14 @@ constructor(
         atLeastOneRootRequired: Boolean,
         errorMessageOnFail: String? = null,
         skipDeactivatedNodes: Boolean = true
-    ): SelectionResult {
-        val nodes =
-            testContext.testOwner.getAllSemanticsNodes(
-                atLeastOneRootRequired = atLeastOneRootRequired,
-                useUnmergedTree = useUnmergedTree,
-                skipDeactivatedNodes = skipDeactivatedNodes
-            )
-        return testContext.testOwner.runOnUiThread {
-            selector.map(nodes, errorMessageOnFail.orEmpty())
+    ): SelectionResult =
+        testContext.testOwner.getAllSemanticsNodes(
+            atLeastOneRootRequired = atLeastOneRootRequired,
+            useUnmergedTree = useUnmergedTree,
+            skipDeactivatedNodes = skipDeactivatedNodes
+        ) {
+            selector.map(it, errorMessageOnFail.orEmpty())
         }
-    }
 
     /**
      * Returns the semantics node captured by this object.
@@ -201,13 +198,11 @@ constructor(
     /** If using the merged tree, performs the same search in the unmerged tree. */
     private fun getNodesInUnmergedTree(errorMessageOnFail: String?): List<SemanticsNode> {
         return if (!useUnmergedTree) {
-            val nodes =
-                testContext.testOwner.getAllSemanticsNodes(
-                    atLeastOneRootRequired = true,
-                    useUnmergedTree = true
-                )
-            testContext.testOwner.runOnUiThread {
-                selector.map(nodes, errorMessageOnFail.orEmpty()).selectedNodes
+            testContext.testOwner.getAllSemanticsNodes(
+                atLeastOneRootRequired = true,
+                useUnmergedTree = true
+            ) {
+                selector.map(it, errorMessageOnFail.orEmpty()).selectedNodes
             }
         } else {
             emptyList()
@@ -256,12 +251,9 @@ constructor(
         atLeastOneRootRequired: Boolean = true,
         errorMessageOnFail: String? = null
     ): List<SemanticsNode> {
-        val nodes =
-            testContext.testOwner.getAllSemanticsNodes(atLeastOneRootRequired, useUnmergedTree)
-
-        return testContext.testOwner
-            .runOnUiThread { selector.map(nodes, errorMessageOnFail.orEmpty()) }
-            .selectedNodes
+        return testContext.testOwner.getAllSemanticsNodes(atLeastOneRootRequired, useUnmergedTree) {
+            selector.map(it, errorMessageOnFail.orEmpty()).selectedNodes
+        }
     }
 
     /**
