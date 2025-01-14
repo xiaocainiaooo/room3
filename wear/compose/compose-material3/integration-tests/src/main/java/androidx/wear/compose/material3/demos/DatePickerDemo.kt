857deb9eb4bcc24b05994a1871e38f0d495f7587
Aman Khosa <akhosa@google.com>
2025-01-14 16:56:44 +0000

    Add Future only demo to DatePicker
    This simplifies the confusing MinDateMaxDate sample.
    
    Bug: 388770505
    Test: N/A - demo app
    
    Change-Id: I62aa27c42b36497d7acad723a10dda8692a44302

--- wear/compose/compose-material3/integration-tests/src/main/java/androidx/wear/compose/material3/demos/DatePickerDemo.kt
+++ wear/compose/compose-material3/integration-tests/src/main/java/androidx/wear/compose/material3/demos/DatePickerDemo.kt
@@ -34,7 +34,7 @@ import androidx.wear.compose.material3.DatePicker
 import androidx.wear.compose.material3.DatePickerType
 import androidx.wear.compose.material3.Icon
 import androidx.wear.compose.material3.Text
-import androidx.wear.compose.material3.samples.DatePickerMinDateMaxDateSample
+import androidx.wear.compose.material3.samples.DatePickerFutureOnlySample
 import androidx.wear.compose.material3.samples.DatePickerSample
 import androidx.wear.compose.material3.samples.DatePickerYearMonthDaySample
 import java.time.LocalDate
@@ -47,7 +47,7 @@ val DatePickerDemos =
         ComposableDemo("Date Month-Day-Year") { DatePickerDemo(DatePickerType.MonthDayYear) },
         ComposableDemo("Date Day-Month-Year") { DatePickerDemo(DatePickerType.DayMonthYear) },
         ComposableDemo("Date System date format") { DatePickerSample() },
-        ComposableDemo("Date Range") { DatePickerMinDateMaxDateSample() },
+        ComposableDemo("Future only") { DatePickerFutureOnlySample() },
         ComposableDemo("Past only") { DatePickerPastOnlyDemo() },
     )
 
