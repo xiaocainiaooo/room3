380254df97fe1599b3aa94a49d8d57451c335fba
bcorso <bcorso@google.com>
2025-01-18 21:25:04 -0800

    Add proper error type names in KSP.
    This feature was added recently to KSP.
    
    Bug: 168639183
    Test: XTypeTest.kt
    Change-Id: I51ffa528364ba2ec6c19b2e5ba0096fda6ac21c7

--- room/room-compiler-processing/src/test/java/androidx/room/compiler/processing/ksp/KspTypeTest.kt
+++ room/room-compiler-processing/src/test/java/androidx/room/compiler/processing/ksp/KspTypeTest.kt
@@ -92,17 +92,18 @@ class KspTypeTest {
             """
                     .trimIndent()
             )
-        runProcessorTest(listOf(src)) { invocation ->
+        runProcessorTest(
+            listOf(src),
+            kotlincArguments =
+                listOf("-P", "plugin:org.jetbrains.kotlin.kapt3:correctErrorTypes=true")
+        ) { invocation ->
             val subject = invocation.processingEnv.requireTypeElement("Subject")
+            val iDontExist = XClassName.get("", "IDontExist")
             subject.getField("errorType").type.let { type ->
                 assertThat(type.isError()).isTrue()
                 assertThat(type.typeArguments).isEmpty()
-                assertThat(type.asTypeName().java).isEqualTo(ERROR_JTYPE_NAME)
-                assertThat(type.typeElement!!.asClassName().java).isEqualTo(ERROR_JTYPE_NAME)
-                if (invocation.isKsp) {
-                    assertThat(type.asTypeName().kotlin).isEqualTo(ERROR_KTYPE_NAME)
-                    assertThat(type.typeElement!!.asClassName().kotlin).isEqualTo(ERROR_KTYPE_NAME)
-                }
+                assertThat(type.asTypeName()).isEqualTo(iDontExist)
+                assertThat(type.typeElement!!.asClassName()).isEqualTo(iDontExist)
             }
 
             subject.getField("listOfErrorType").type.let { type ->
@@ -110,10 +111,7 @@ class KspTypeTest {
                 assertThat(type.typeArguments).hasSize(1)
                 type.typeArguments.single().let { typeArg ->
                     assertThat(typeArg.isError()).isTrue()
-                    assertThat(typeArg.asTypeName().java).isEqualTo(ERROR_JTYPE_NAME)
-                    if (invocation.isKsp) {
-                        assertThat(typeArg.asTypeName().kotlin).isEqualTo(ERROR_KTYPE_NAME)
-                    }
+                    assertThat(typeArg.asTypeName()).isEqualTo(iDontExist)
                 }
             }
             invocation.assertCompilationResult { compilationDidFail() }
@@ -569,15 +567,18 @@ class KspTypeTest {
             """
                     .trimIndent()
             )
-        runKspTest(listOf(src)) { invocation ->
+        runKspTest(
+            listOf(src),
+            kotlincArguments =
+                listOf("-P", "plugin:org.jetbrains.kotlin.kapt3:correctErrorTypes=true")
+        ) { invocation ->
             val typeElement = invocation.processingEnv.requireTypeElement("foo.bar.Baz")
             val exception =
                 assertThrows(IllegalStateException::class) { typeElement.type.superTypes }
             exception
                 .hasMessageThat()
                 .isEqualTo(
-                    "Class foo.bar.Baz should have only one super class." +
-                        " Found 2 (foo.bar.A, error.NonExistentClass)."
+                    "Class foo.bar.Baz should have only one super class. Found 2 (foo.bar.A, C)."
                 )
             invocation.assertCompilationResult { compilationDidFail() }
         }
