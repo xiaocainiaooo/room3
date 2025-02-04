16ab3c4fc769a7da1db4654704500d8beb369829
MingWei <mingweiliao@google.com>
2025-02-04 16:57:32 +0000

    Add AppFunctionServiceDelegate
    Test: ./gradlew :appfunctions:appfunctions-runtime:connectedAndroidTest
    Change-Id: Ifbe064ff7f590a28748101fab90da4b1080d50ba

---
+++ appfunctions/appfunctions-runtime/src/androidTest/java/androidx/appfunctions/AppFunctionServiceDelegateTest.kt
@@ -0,0 +1,310 @@
+/*
+ * Copyright 2025 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package androidx.appfunctions
+
+import android.content.Context
+import android.os.Build
+import android.os.OutcomeReceiver
+import androidx.annotation.RequiresApi
+import androidx.appfunctions.internal.AggregateAppFunctionInventory
+import androidx.appfunctions.internal.AggregateAppFunctionInvoker
+import androidx.appfunctions.internal.AppFunctionInventory
+import androidx.appfunctions.internal.AppFunctionInvoker
+import androidx.appfunctions.metadata.AppFunctionDataTypeMetadata.Companion.TYPE_LONG
+import androidx.appfunctions.metadata.AppFunctionDataTypeMetadata.Companion.TYPE_STRING
+import androidx.appfunctions.metadata.AppFunctionDataTypeMetadata.Companion.TYPE_UNIT
+import androidx.appfunctions.metadata.AppFunctionMetadata
+import androidx.appfunctions.metadata.AppFunctionParameterMetadata
+import androidx.appfunctions.metadata.AppFunctionPrimitiveTypeMetadata
+import androidx.appfunctions.metadata.AppFunctionResponseMetadata
+import androidx.test.filters.SdkSuppress
+import androidx.test.platform.app.InstrumentationRegistry
+import com.google.common.truth.Truth.assertThat
+import kotlin.coroutines.resume
+import kotlin.coroutines.resumeWithException
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.suspendCancellableCoroutine
+import kotlinx.coroutines.test.UnconfinedTestDispatcher
+import org.junit.Assert.assertThrows
+import org.junit.Before
+import org.junit.Test
+
+@RequiresApi(Build.VERSION_CODES.VANILLA_ICE_CREAM)
+@SdkSuppress(minSdkVersion = Build.VERSION_CODES.VANILLA_ICE_CREAM)
+@OptIn(ExperimentalCoroutinesApi::class)
+class AppFunctionServiceDelegateTest {
+    private val testDispatcher = UnconfinedTestDispatcher()
+
+    private lateinit var context: Context
+
+    private lateinit var fakeAggregateInvoker: FakeAggregateInvoker
+
+    private lateinit var fakeAggregateInventory: FakeAggregateInventory
+
+    private lateinit var delegate: AppFunctionServiceDelegate
+
+    @Before
+    fun setup() {
+        context = InstrumentationRegistry.getInstrumentation().context
+        fakeAggregateInvoker = FakeAggregateInvoker()
+        fakeAggregateInventory = FakeAggregateInventory()
+        delegate =
+            AppFunctionServiceDelegate(
+                context,
+                testDispatcher,
+                testDispatcher,
+                fakeAggregateInventory,
+                fakeAggregateInvoker,
+            )
+    }
+
+    @Test
+    fun testOnExecuteFunction_functionNotExist() {
+        val request =
+            ExecuteAppFunctionRequest(
+                targetPackageName = context.packageName,
+                functionIdentifier = "fakeFunctionId",
+                functionParameters = AppFunctionData.EMPTY
+            )
+
+        assertThrows(AppFunctionFunctionNotFoundException::class.java) {
+            runBlocking { executeFunctionBlocking(request) }
+        }
+    }
+
+    @Test
+    fun testOnExecuteFunction_invalidParameter() {
+        fakeAggregateInventory.setAppFunctionMetadata(
+            AppFunctionMetadata(
+                id = "invaliadParameterFunction",
+                isEnabledByDefault = true,
+                schema = null,
+                parameters =
+                    listOf(
+                        AppFunctionParameterMetadata(
+                            name = "requiredLong",
+                            isRequired = true,
+                            dataType =
+                                AppFunctionPrimitiveTypeMetadata(
+                                    type = TYPE_LONG,
+                                    isNullable = false,
+                                )
+                        )
+                    ),
+                response =
+                    AppFunctionResponseMetadata(
+                        valueType =
+                            AppFunctionPrimitiveTypeMetadata(
+                                type = TYPE_UNIT,
+                                isNullable = false,
+                            )
+                    )
+            )
+        )
+        val request =
+            ExecuteAppFunctionRequest(
+                targetPackageName = context.packageName,
+                functionIdentifier = "invaliadParameterFunction",
+                // Missing requiredLong from the parameter
+                functionParameters = AppFunctionData.EMPTY
+            )
+
+        assertThrows(AppFunctionInvalidArgumentException::class.java) {
+            runBlocking { executeFunctionBlocking(request) }
+        }
+    }
+
+    @Test
+    fun testOnExecuteFunction_buildReturnValueFail() {
+        fakeAggregateInventory.setAppFunctionMetadata(
+            AppFunctionMetadata(
+                id = "returnIncorrectResultFunction",
+                isEnabledByDefault = true,
+                schema = null,
+                parameters = listOf(),
+                response =
+                    AppFunctionResponseMetadata(
+                        valueType =
+                            AppFunctionPrimitiveTypeMetadata(
+                                type = TYPE_LONG,
+                                isNullable = false,
+                            )
+                    )
+            )
+        )
+        // Returns String instead of Long
+        fakeAggregateInvoker.setAppFunctionResult("returnIncorrectResultFunction") { "TestString" }
+        val request =
+            ExecuteAppFunctionRequest(
+                targetPackageName = context.packageName,
+                functionIdentifier = "returnIncorrectResultFunction",
+                functionParameters = AppFunctionData.EMPTY
+            )
+
+        assertThrows(AppFunctionAppUnknownException::class.java) {
+            runBlocking { executeFunctionBlocking(request) }
+        }
+    }
+
+    @Test
+    fun testOnExecuteFunction_succeed_noArg() {
+        fakeAggregateInventory.setAppFunctionMetadata(
+            AppFunctionMetadata(
+                id = "succeedFunction",
+                isEnabledByDefault = true,
+                schema = null,
+                parameters = listOf(),
+                response =
+                    AppFunctionResponseMetadata(
+                        valueType =
+                            AppFunctionPrimitiveTypeMetadata(
+                                type = TYPE_STRING,
+                                isNullable = false,
+                            )
+                    )
+            )
+        )
+        fakeAggregateInvoker.setAppFunctionResult("succeedFunction") { "TestString" }
+        val request =
+            ExecuteAppFunctionRequest(
+                targetPackageName = context.packageName,
+                functionIdentifier = "succeedFunction",
+                functionParameters = AppFunctionData.EMPTY
+            )
+
+        val response = runBlocking { executeFunctionBlocking(request) }
+
+        assertThat(response.result.getString(ExecuteAppFunctionResponse.PROPERTY_RETURN_VALUE))
+            .isEqualTo("TestString")
+    }
+
+    @Test
+    fun testOnExecuteFunction_succeed_withArg() {
+        fakeAggregateInventory.setAppFunctionMetadata(
+            AppFunctionMetadata(
+                id = "succeedFunction",
+                isEnabledByDefault = true,
+                schema = null,
+                parameters =
+                    listOf(
+                        AppFunctionParameterMetadata(
+                            name = "testArg",
+                            isRequired = true,
+                            dataType =
+                                AppFunctionPrimitiveTypeMetadata(
+                                    type = TYPE_LONG,
+                                    isNullable = false,
+                                )
+                        )
+                    ),
+                response =
+                    AppFunctionResponseMetadata(
+                        valueType =
+                            AppFunctionPrimitiveTypeMetadata(
+                                type = TYPE_STRING,
+                                isNullable = false,
+                            )
+                    )
+            )
+        )
+        fakeAggregateInvoker.setAppFunctionResult("succeedFunction") { "TestString" }
+        val request =
+            ExecuteAppFunctionRequest(
+                targetPackageName = context.packageName,
+                functionIdentifier = "succeedFunction",
+                functionParameters = AppFunctionData.Builder("").setLong("testArg", 100L).build()
+            )
+
+        val response = runBlocking { executeFunctionBlocking(request) }
+
+        assertThat(response.result.getString(ExecuteAppFunctionResponse.PROPERTY_RETURN_VALUE))
+            .isEqualTo("TestString")
+    }
+
+    private suspend fun executeFunctionBlocking(
+        request: ExecuteAppFunctionRequest,
+    ): ExecuteAppFunctionResponse = suspendCancellableCoroutine { cont ->
+        delegate.onExecuteFunction(
+            request,
+            "test",
+            object : OutcomeReceiver<ExecuteAppFunctionResponse, AppFunctionException> {
+                override fun onResult(result: ExecuteAppFunctionResponse) {
+                    cont.resume(result)
+                }
+
+                override fun onError(e: AppFunctionException) {
+                    cont.resumeWithException(e)
+                }
+            },
+        )
+    }
+
+    private class FakeAggregateInvoker : AggregateAppFunctionInvoker() {
+
+        private val internalInvoker =
+            object : AppFunctionInvoker {
+
+                private val functionResultMap = mutableMapOf<String, () -> Any?>()
+
+                override val supportedFunctionIds: Set<String>
+                    get() = functionResultMap.keys
+
+                override suspend fun unsafeInvoke(
+                    appFunctionContext: AppFunctionContext,
+                    functionIdentifier: String,
+                    parameters: Map<String, Any?>
+                ): Any? {
+                    return functionResultMap[functionIdentifier]?.invoke()
+                }
+
+                fun setAppFunctionResult(functionId: String, result: () -> Any?) {
+                    functionResultMap[functionId] = result
+                }
+            }
+
+        override val invokers: List<AppFunctionInvoker>
+            get() = listOf(internalInvoker)
+
+        fun setAppFunctionResult(functionId: String, result: () -> Any?) {
+            internalInvoker.setAppFunctionResult(functionId, result)
+        }
+    }
+
+    private class FakeAggregateInventory : AggregateAppFunctionInventory() {
+
+        private val internalInventory =
+            object : AppFunctionInventory {
+
+                private val internalMap = mutableMapOf<String, AppFunctionMetadata>()
+
+                override val functionIdToMetadataMap: Map<String, AppFunctionMetadata>
+                    get() = internalMap
+
+                fun setAppFunctionMetadata(metadata: AppFunctionMetadata) {
+                    internalMap[metadata.id] = metadata
+                }
+            }
+
+        override val inventories: List<AppFunctionInventory>
+            get() = listOf(internalInventory)
+
+        fun setAppFunctionMetadata(metadata: AppFunctionMetadata) {
+            internalInventory.setAppFunctionMetadata(metadata)
+        }
+    }
+}
---
+++ appfunctions/appfunctions-runtime/src/main/java/androidx/appfunctions/AppFunctionServiceDelegate.kt
@@ -0,0 +1,139 @@
+/*
+ * Copyright 2025 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package androidx.appfunctions
+
+import android.content.Context
+import android.content.pm.SigningInfo
+import android.os.Build
+import android.os.OutcomeReceiver
+import android.util.Log
+import androidx.annotation.RequiresApi
+import androidx.appfunctions.internal.AggregateAppFunctionInventory
+import androidx.appfunctions.internal.AggregateAppFunctionInvoker
+import androidx.appfunctions.internal.Constants.APP_FUNCTIONS_TAG
+import androidx.appfunctions.internal.unsafeBuildReturnValue
+import androidx.appfunctions.internal.unsafeGetParameterValue
+import androidx.appfunctions.metadata.AppFunctionMetadata
+import kotlin.coroutines.CoroutineContext
+import kotlinx.coroutines.CancellationException
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+
+// TODO: Update the RequiresApi in AppFunctionData to be T.
+@RequiresApi(Build.VERSION_CODES.VANILLA_ICE_CREAM)
+internal class AppFunctionServiceDelegate(
+    context: Context,
+    workerCoroutineContext: CoroutineContext,
+    private val mainCoroutineContext: CoroutineContext,
+    private val aggregateInventory: AggregateAppFunctionInventory,
+    private val aggregateInvoker: AggregateAppFunctionInvoker,
+) {
+
+    private val job = Job()
+    private val workerCoroutineScope = CoroutineScope(workerCoroutineContext + job)
+    private val appContext = context.applicationContext
+
+    internal fun onExecuteFunction(
+        executeAppFunctionRequest: ExecuteAppFunctionRequest,
+        callingPackageName: String,
+        callback: OutcomeReceiver<ExecuteAppFunctionResponse, AppFunctionException>,
+    ): Job =
+        workerCoroutineScope.launch {
+            try {
+                val appFunctionMetadata =
+                    aggregateInventory.functionIdToMetadataMap[
+                            executeAppFunctionRequest.functionIdentifier]
+                if (appFunctionMetadata == null) {
+                    Log.d(
+                        APP_FUNCTIONS_TAG,
+                        "${executeAppFunctionRequest.functionIdentifier} is not available"
+                    )
+                    callback.onError(
+                        AppFunctionFunctionNotFoundException(
+                            "${executeAppFunctionRequest.functionIdentifier} is not available"
+                        )
+                    )
+                    return@launch
+                }
+
+                val parameters = extractParameters(executeAppFunctionRequest, appFunctionMetadata)
+                callback.onResult(
+                    unsafeInvokeFunction(
+                        executeAppFunctionRequest,
+                        callingPackageName,
+                        appFunctionMetadata,
+                        parameters,
+                    )
+                )
+            } catch (e: CancellationException) {
+                callback.onError(AppFunctionCancelledException(e.message))
+            } catch (e: AppFunctionException) {
+                callback.onError(e)
+            } catch (e: Exception) {
+                callback.onError(AppFunctionAppUnknownException(e.message))
+            }
+        }
+
+    internal fun onDestroy() {
+        job.cancel()
+    }
+
+    private fun extractParameters(
+        request: ExecuteAppFunctionRequest,
+        appFunctionMetadata: AppFunctionMetadata,
+    ): Map<String, Any?> {
+        return buildMap {
+            for (parameterMetadata in appFunctionMetadata.parameters) {
+                this[parameterMetadata.name] =
+                    request.functionParameters.unsafeGetParameterValue(parameterMetadata)
+            }
+        }
+    }
+
+    private suspend fun unsafeInvokeFunction(
+        request: ExecuteAppFunctionRequest,
+        callingPackageName: String,
+        appFunctionMetadata: AppFunctionMetadata,
+        parameters: Map<String, Any?>
+    ): ExecuteAppFunctionResponse {
+        val result =
+            withContext(mainCoroutineContext) {
+                aggregateInvoker.unsafeInvoke(
+                    buildAppFunctionContext(callingPackageName),
+                    request.functionIdentifier,
+                    parameters
+                )
+            }
+        val returnValue = appFunctionMetadata.response.unsafeBuildReturnValue(result)
+        return ExecuteAppFunctionResponse(returnValue)
+    }
+
+    private fun buildAppFunctionContext(callingPackageName: String): AppFunctionContext {
+        return object : AppFunctionContext {
+            override val context: Context
+                get() = appContext
+
+            override val callingPackageName: String
+                get() = callingPackageName
+
+            override val callingPackageSigningInfo: SigningInfo
+                get() = TODO("Not yet implemented")
+        }
+    }
+}
