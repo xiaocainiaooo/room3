ee9edcd0deaa7733683ff3d5ec6e9cb332309282
Hakan Mehmed <hmehmed@google.com>
2025-02-20 11:41:33 +0000

    Change to using LintLifecycleExtension
    For normal AGP plugins lint lifecycle extension is always configured and
    can be used to finalize the lint DSL without the need to access the
    androidComponents extension itself.
    
    Bug: 380218409
    Test: existing should work
    Change-Id: I8875b96ccbf735733c90ff4b8da8a50e63f54217

--- buildSrc/private/src/main/kotlin/androidx/build/AndroidXComposeImplPlugin.kt
+++ buildSrc/private/src/main/kotlin/androidx/build/AndroidXComposeImplPlugin.kt
@@ -16,7 +16,7 @@
 
 package androidx.build
 
-import com.android.build.api.variant.AndroidComponentsExtension
+import com.android.build.api.variant.LintLifecycleExtension
 import com.android.build.gradle.AppPlugin
 import com.android.build.gradle.LibraryPlugin
 import java.io.File
@@ -54,10 +54,10 @@ class AndroidXComposeImplPlugin : Plugin<Project> {
 
     companion object {
         private fun Project.configureAndroidCommonOptions() {
-            extensions.findByType(AndroidComponentsExtension::class.java)!!.finalizeDsl { android ->
+            extensions.findByType(LintLifecycleExtension::class.java)!!.finalizeDsl { lint ->
                 val isPublished = androidXExtension.shouldPublish()
 
-                android.lint {
+                lint.run {
                     // These lint checks are normally a warning (or lower), but we ignore (in
                     // AndroidX)
                     // warnings in Lint, so we make it an error here so it will fail the build.
