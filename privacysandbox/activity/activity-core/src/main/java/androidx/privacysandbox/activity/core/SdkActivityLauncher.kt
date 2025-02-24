a5055fa605efd7cf0c46af520365a35ab3cf6127
Hanna Nizhnikava <yulecat@google.com>
2025-02-24 13:23:01 +0000

    Add launcher to create non-LifecycleOwner sandboxed activities
    Adds LocalUnmanagedSdkActivityLauncher for activities that do not extend
    the LifecycleOwner interface. It extends the existing
    SdkActivityLauncher interface. To create a launcher, an extension method
    createUnmanagedSdkActivityLauncher is added for Activity.
    
    As opposed to LocalManagedSdkActivityLauncher, the new launcher must be
    manually disposed by the user.
    
    Relnote: "Added a launcher for non-LifecycleOwner activities. Renamed
    LocalSdkActivityLauncher to LocalManagedSdkActivityLauncher."
    Bug: 382012571
    Test: CreateSdkActivityLauncherTest
    
    Change-Id: Ib14089762f10b1ae42989b6ef66191bdae4beb06

--- privacysandbox/activity/activity-core/src/main/java/androidx/privacysandbox/activity/core/SdkActivityLauncher.kt
+++ privacysandbox/activity/activity-core/src/main/java/androidx/privacysandbox/activity/core/SdkActivityLauncher.kt
@@ -22,8 +22,10 @@ import android.os.IBinder
  * Interface that allows SDKs running in the Privacy Sandbox to launch activities.
  *
  * Apps can create launchers by calling
- * [createActivityLauncher][androidx.privacysandbox.activity.client.createSdkActivityLauncher] from
- * one of their activities.
+ * [createManagedSdkActivityLauncher][androidx.privacysandbox.activity.client.createManagedSdkActivityLauncher]
+ * or
+ * [createUnmanagedSdkActivityLauncher][androidx.privacysandbox.activity.client.createUnmanagedSdkActivityLauncher]
+ * from one of their activities.
  *
  * To send an [SdkActivityLauncher] to another process, they can call
  * [toLauncherInfo][androidx.privacysandbox.activity.client.toLauncherInfo] and send the resulting
