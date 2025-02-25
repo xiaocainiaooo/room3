53809608495372588053e12c1483527e6d94bb07
CharcoalChen <charcoalchen@google.com>
2025-02-25 19:19:31 +0800

    Fix getAvailableCameraInfos NPE failures in *LifecycleStatusChangeStressTest
    Bug: 390272054
    Test: ImageCaptureLifecycleStatusChangeStressTest on sm-s921u
    Change-Id: If51153447a8add9f0341a568c03801b0f38c2c23

--- camera/integration-tests/coretestapp/src/androidTest/java/androidx/camera/integration/core/stresstest/LifecycleStatusChangeStressTestBase.kt
+++ camera/integration-tests/coretestapp/src/androidTest/java/androidx/camera/integration/core/stresstest/LifecycleStatusChangeStressTestBase.kt
@@ -18,6 +18,7 @@ package androidx.camera.integration.core.stresstest
 
 import android.Manifest
 import android.content.Context
+import android.os.Build
 import androidx.camera.camera2.pipe.integration.CameraPipeConfig
 import androidx.camera.core.Camera
 import androidx.camera.core.CameraSelector
@@ -53,6 +54,7 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.runBlocking
 import kotlinx.coroutines.withContext
 import org.junit.After
+import org.junit.Assume.assumeFalse
 import org.junit.Assume.assumeTrue
 import org.junit.Before
 import org.junit.ClassRule
@@ -105,6 +107,7 @@ abstract class LifecycleStatusChangeStressTestBase(
 
     @Before
     fun setup(): Unit = runBlocking {
+        assumeLifecycleStatusChangeTestCompatibleDevice()
         assumeTrue(CameraUtil.deviceHasCamera())
         CoreAppTestUtil.assumeCompatibleDevice()
         CoreAppTestUtil.assumeNotUntestableFrontCamera(cameraId)
@@ -252,4 +255,17 @@ abstract class LifecycleStatusChangeStressTestBase(
             }
         }
     }
+
+    /**
+     * On some devices, after ProcessCameraProvider#shutdownAsync has been executed, the device
+     * WindowManager might still try to launch the CameraXActivity according to some unknown reason.
+     * This causes the CameraXActivity to invoke ProcessCameraProvider#getAvailableCameraInfos and
+     * then result in NPE problem. Therefore, the LifecycleStatusChange related tests will be
+     * skipped on those devices. See b/390272054 for the details.
+     */
+    private fun assumeLifecycleStatusChangeTestCompatibleDevice() =
+        assumeFalse(
+            "Samsung".equals(Build.BRAND, ignoreCase = true) &&
+                "SM-S921U".equals(Build.MODEL, ignoreCase = true)
+        )
 }
