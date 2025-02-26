c7bb13ec39112110e4a8027ffb1b86ade4861e57
Mariano Martin <ymarian@google.com>
2025-02-26 11:35:45 -0500

    [FabMenu] Fix crash when using keyboard navigation
    Test: Added key presses to test
    Bug: 395858222
    
    Change-Id: I3370194be94007929e05b324a06ca57b674259be

--- compose/material3/material3/src/androidInstrumentedTest/kotlin/androidx/compose/material3/FloatingActionButtonMenuTest.kt
+++ compose/material3/material3/src/androidInstrumentedTest/kotlin/androidx/compose/material3/FloatingActionButtonMenuTest.kt
@@ -22,6 +22,7 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Close
 import androidx.compose.material3.ToggleFloatingActionButtonDefaults.animateIcon
+import androidx.compose.runtime.Composable
 import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -30,8 +31,11 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.graphics.vector.rememberVectorPainter
+import androidx.compose.ui.input.key.Key
 import androidx.compose.ui.platform.testTag
+import androidx.compose.ui.test.ExperimentalTestApi
 import androidx.compose.ui.test.assertCountEquals
 import androidx.compose.ui.test.assertHasClickAction
 import androidx.compose.ui.test.assertHasNoClickAction
@@ -40,6 +44,9 @@ import androidx.compose.ui.test.assertIsNotDisplayed
 import androidx.compose.ui.test.isFocusable
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithTag
+import androidx.compose.ui.test.onRoot
+import androidx.compose.ui.test.performKeyInput
+import androidx.compose.ui.test.pressKey
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.MediumTest
 import org.junit.Rule
@@ -52,7 +59,7 @@ class FloatingActionButtonMenuTest {
 
     @get:Rule val rule = createComposeRule()
 
-    @OptIn(ExperimentalMaterial3ExpressiveApi::class)
+    @OptIn(ExperimentalMaterial3ExpressiveApi::class, ExperimentalTestApi::class)
     @Test
     fun fabMenu_childrenCount_collapsed() {
         val items =
@@ -63,51 +70,19 @@ class FloatingActionButtonMenuTest {
                 Icons.Filled.Add to "Snooze",
             )
 
-        rule.setContent {
-            var fabMenuExpanded by rememberSaveable { mutableStateOf(false) }
-            Box(modifier = Modifier.fillMaxSize()) {
-                FloatingActionButtonMenu(
-                    modifier = Modifier.align(Alignment.BottomEnd),
-                    expanded = fabMenuExpanded,
-                    button = {
-                        ToggleFloatingActionButton(
-                            checked = fabMenuExpanded,
-                            onCheckedChange = { fabMenuExpanded = !fabMenuExpanded }
-                        ) {
-                            val imageVector by remember {
-                                derivedStateOf {
-                                    if (checkedProgress > 0.5f) Icons.Filled.Close
-                                    else Icons.Filled.Add
-                                }
-                            }
-                            Icon(
-                                painter = rememberVectorPainter(imageVector),
-                                contentDescription = null,
-                                modifier = Modifier.animateIcon({ checkedProgress })
-                            )
-                        }
-                    }
-                ) {
-                    items.forEachIndexed { index, item ->
-                        FloatingActionButtonMenuItem(
-                            modifier = Modifier.testTag("button_$index"),
-                            onClick = { fabMenuExpanded = false },
-                            icon = { Icon(item.first, contentDescription = null) },
-                            text = { Text(text = item.second) },
-                        )
-                    }
-                }
-            }
-        }
+        rule.setContent { FabMenuTest(initialExpanded = false, items = items) }
 
+        rule.waitForIdle()
         rule.onAllNodes(isFocusable()).assertCountEquals(1)
 
         repeat(items.size) {
             rule.onNodeWithTag("button_$it").assertIsNotDisplayed().assertHasNoClickAction()
         }
+
+        rule.onRoot().performKeyInput { repeat(items.size) { pressKey(Key.Tab) } }
     }
 
-    @OptIn(ExperimentalMaterial3ExpressiveApi::class)
+    @OptIn(ExperimentalMaterial3ExpressiveApi::class, ExperimentalTestApi::class)
     @Test
     fun fabMenu_childrenCount_expanded() {
         val items =
@@ -118,47 +93,50 @@ class FloatingActionButtonMenuTest {
                 Icons.Filled.Add to "Snooze",
             )
 
-        rule.setContent {
-            var fabMenuExpanded by rememberSaveable { mutableStateOf(true) }
-            Box(Modifier.fillMaxSize()) {
-                FloatingActionButtonMenu(
-                    modifier = Modifier.align(Alignment.BottomEnd),
-                    expanded = fabMenuExpanded,
-                    button = {
-                        ToggleFloatingActionButton(
-                            checked = fabMenuExpanded,
-                            onCheckedChange = { fabMenuExpanded = !fabMenuExpanded }
-                        ) {
-                            val imageVector by remember {
-                                derivedStateOf {
-                                    if (checkedProgress > 0.5f) Icons.Filled.Close
-                                    else Icons.Filled.Add
-                                }
-                            }
-                            Icon(
-                                painter = rememberVectorPainter(imageVector),
-                                contentDescription = null,
-                                modifier = Modifier.animateIcon({ checkedProgress })
-                            )
-                        }
-                    }
-                ) {
-                    items.forEachIndexed { index, item ->
-                        FloatingActionButtonMenuItem(
-                            modifier = Modifier.testTag("button_$index"),
-                            onClick = { fabMenuExpanded = false },
-                            icon = { Icon(item.first, contentDescription = null) },
-                            text = { Text(text = item.second) },
-                        )
-                    }
-                }
-            }
-        }
+        rule.setContent { FabMenuTest(initialExpanded = true, items = items) }
 
         rule.onAllNodes(isFocusable()).assertCountEquals(5)
 
         repeat(items.size) {
             rule.onNodeWithTag("button_$it").assertIsDisplayed().assertHasClickAction()
+        }
+    }
+
+    @Composable
+    @OptIn(ExperimentalMaterial3ExpressiveApi::class)
+    private fun FabMenuTest(initialExpanded: Boolean, items: List<Pair<ImageVector, String>>) {
+        Box(Modifier.fillMaxSize()) {
+            var fabMenuExpanded by rememberSaveable { mutableStateOf(initialExpanded) }
+            FloatingActionButtonMenu(
+                modifier = Modifier.align(Alignment.BottomEnd),
+                expanded = fabMenuExpanded,
+                button = {
+                    ToggleFloatingActionButton(
+                        checked = fabMenuExpanded,
+                        onCheckedChange = { fabMenuExpanded = !fabMenuExpanded }
+                    ) {
+                        val imageVector by remember {
+                            derivedStateOf {
+                                if (checkedProgress > 0.5f) Icons.Filled.Close else Icons.Filled.Add
+                            }
+                        }
+                        Icon(
+                            painter = rememberVectorPainter(imageVector),
+                            contentDescription = null,
+                            modifier = Modifier.animateIcon({ checkedProgress })
+                        )
+                    }
+                }
+            ) {
+                items.forEachIndexed { index, item ->
+                    FloatingActionButtonMenuItem(
+                        modifier = Modifier.testTag("button_$index"),
+                        onClick = { fabMenuExpanded = false },
+                        icon = { Icon(item.first, contentDescription = null) },
+                        text = { Text(text = item.second) },
+                    )
+                }
+            }
         }
     }
 }
