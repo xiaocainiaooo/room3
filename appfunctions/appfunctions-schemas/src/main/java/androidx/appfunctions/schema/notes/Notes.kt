fa9ad856e16dc1b3c2c12484717624df3a681a70
Yara Hassan <yaraabdullatif@google.com>
2025-03-07 17:31:18 +0000

    Add NotesGroup APIs in Notes schema
    Relnote: Add NotesGroup APIs in Notes schema
    Bug: 401517540
    Test: N/A
    Change-Id: I6048177bbdd2fda3278f7aa082b1547e68323e52

--- appfunctions/appfunctions-schemas/src/main/java/androidx/appfunctions/schema/notes/Notes.kt
+++ appfunctions/appfunctions-schemas/src/main/java/androidx/appfunctions/schema/notes/Notes.kt
@@ -48,9 +48,13 @@ public interface FindNotesAppFunction<
     /**
      * Finds notes with the given search criteria.
      *
+     * The implementing app should throw an appropriate [androidx.appfunctions.AppFunctionException]
+     * in exceptional cases.
+     *
      * @param appFunctionContext The AppFunction execution context.
      * @param parameters The parameters for finding notes.
-     * @return The response including the list of notes that match the parameters.
+     * @return The response including the list of notes that match the parameters, or an empty list
+     *   if no match is found.
      */
     public suspend fun findNotes(
         appFunctionContext: AppFunctionContext,
@@ -74,7 +78,7 @@ public interface FindNotesAppFunction<
 
     /** The response including the list of notes that match the parameters. */
     public interface Response {
-        /** The list of notes that match the parameters. */
+        /** The list of notes that match the parameters, or an empty list if no match is found. */
         public val notes: List<AppFunctionNote>
     }
 
@@ -97,6 +101,9 @@ public interface CreateNoteAppFunction<
     /**
      * Creates an [AppFunctionNote] with the given parameters.
      *
+     * The implementing app should throw an appropriate subclass of
+     * [androidx.appfunctions.AppFunctionException] in exceptional cases.
+     *
      * @param appFunctionContext The AppFunction execution context.
      * @param parameters The parameters for creating a note.
      * @return The response including the created note.
@@ -117,6 +124,15 @@ public interface CreateNoteAppFunction<
         /** The attachments of the note. */
         public val attachments: List<AppFunctionNote.Attachment>
             get() = emptyList()
+
+        /**
+         * The ID of the group the note is in, if any else `null`.
+         *
+         * [androidx.appfunctions.AppFunctionElementNotFoundException] should be thrown when a group
+         * with the specified groupId doesn't exist.
+         */
+        public val groupId: String?
+            get() = null
 
         /**
          * An optional UUID for this note provided by the caller. If provided, the caller can use
@@ -223,7 +239,13 @@ public interface DeleteNotesAppFunction<
     Response : DeleteNotesAppFunction.Response
 > {
     /**
-     * Deletes the notes with the given search criteria.
+     * Deletes the notes with the given search criteria and returns the IDs of the notes that were
+     * successfully deleted.
+     *
+     * The implementing app should throw an appropriate subclass of
+     * [androidx.appfunctions.AppFunctionException] in exceptional cases, except for failures in
+     * deleting individual noteId(s), which can be ignored when returning the
+     * [DeleteNotesAppFunction.Response].
      *
      * @param appFunctionContext The AppFunction execution context.
      * @param parameters The parameters defining the criteria of notes to delete.
@@ -314,18 +336,26 @@ public interface ShowNoteAppFunction<
     /**
      * Shows the note with the given parameters.
      *
+     * The implementing app should throw an appropriate subclass of
+     * [androidx.appfunctions.AppFunctionException] in exceptional cases.
+     *
      * @param appFunctionContext The AppFunction execution context.
-     * @param showNoteParams The params of the note to show.
+     * @param parameters The params of the note to show.
      * @return The response including the intent to show the note.
      */
     public suspend fun showNote(
         appFunctionContext: AppFunctionContext,
-        showNoteParams: Parameters
+        parameters: Parameters
     ): Response
 
     /** The parameters for [showNote]. */
     public interface Parameters {
-        /** The [AppFunctionNote.id] of the note to show. */
+        /**
+         * The [AppFunctionNote.id] of the note to show.
+         *
+         * [androidx.appfunctions.AppFunctionElementNotFoundException] should be thrown when a note
+         * with the specified noteId doesn't exist.
+         */
         public val noteId: String
     }
 
@@ -333,6 +363,149 @@ public interface ShowNoteAppFunction<
     public interface Response : AppFunctionOpenable
 
     public companion object {
+        /** Current schema version. */
+        @RestrictTo(LIBRARY_GROUP) internal const val SCHEMA_VERSION: Int = 2
+    }
+}
+
+/** Finds notes groups with the given search criteria. */
+@AppFunctionSchemaDefinition(
+    name = "findNotesGroups",
+    version = FindNotesGroupsAppFunction.SCHEMA_VERSION,
+    category = APP_FUNCTION_SCHEMA_CATEGORY_NOTES
+)
+public interface FindNotesGroupsAppFunction<
+    Parameters : FindNotesGroupsAppFunction.Parameters,
+    Response : FindNotesGroupsAppFunction.Response
+> {
+    /**
+     * Finds notes groups with the given search criteria.
+     *
+     * The implementing app should throw an appropriate subclass of
+     * [androidx.appfunctions.AppFunctionException] in exceptional cases.
+     *
+     * @param appFunctionContext The AppFunction execution context.
+     * @param parameters The parameters for finding groups.
+     * @return The response including the list of groups that match the parameters, or an empty list
+     *   if no match is found.
+     */
+    public suspend fun findNotesGroups(
+        appFunctionContext: AppFunctionContext,
+        parameters: Parameters,
+    ): Response
+
+    /** The parameters for finding groups. */
+    public interface Parameters {
+        /**
+         * The query to search for groups. A null value means to query all groups.
+         *
+         * This parameter is analogous to the caller typing a query into a search box. The app
+         * providing the app function has full control over how the query is interpreted and
+         * processed; for example by name matching or semantic analysis.
+         */
+        public val query: String?
+            get() = null
+    }
+
+    /** The response including the list of groups that match the parameters. */
+    public interface Response {
+        /** The list of groups that match the parameters, or an empty list if no match is found. */
+        public val groups: List<AppFunctionNotesGroup>
+    }
+
+    public companion object {
+
+        /** Current schema version. */
+        @RestrictTo(LIBRARY_GROUP) internal const val SCHEMA_VERSION: Int = 2
+    }
+}
+
+/** Creates a group with the given parameters. */
+@AppFunctionSchemaDefinition(
+    name = "createNotesGroup",
+    version = CreateNotesGroupAppFunction.SCHEMA_VERSION,
+    category = APP_FUNCTION_SCHEMA_CATEGORY_NOTES
+)
+public interface CreateNotesGroupAppFunction<
+    Parameters : CreateNotesGroupAppFunction.Parameters,
+    Response : CreateNotesGroupAppFunction.Response
+> {
+    /**
+     * Creates a notes group with the given parameters.
+     *
+     * The implementing app should throw an appropriate subclass of
+     * [androidx.appfunctions.AppFunctionException] in exceptional cases.
+     *
+     * @param appFunctionContext The AppFunction execution context.
+     * @param parameters The parameters for creating a group.
+     * @return The created group.
+     */
+    public suspend fun createNotesGroup(
+        appFunctionContext: AppFunctionContext,
+        parameters: Parameters,
+    ): Response
+
+    /** The parameters for creating a group. */
+    public interface Parameters {
+        /** The name of the group. */
+        public val name: String
+    }
+
+    /** The response including the created group. */
+    public interface Response {
+        /** The created group. */
+        public val createdGroup: AppFunctionNotesGroup
+    }
+
+    public companion object {
+
+        /** Current schema version. */
+        @RestrictTo(LIBRARY_GROUP) internal const val SCHEMA_VERSION: Int = 2
+    }
+}
+
+/** Deletes groups with the given parameters. */
+@AppFunctionSchemaDefinition(
+    name = "deleteNotesGroups",
+    version = DeleteNotesGroupsAppFunction.SCHEMA_VERSION,
+    category = APP_FUNCTION_SCHEMA_CATEGORY_NOTES
+)
+public interface DeleteNotesGroupsAppFunction<
+    Parameters : DeleteNotesGroupsAppFunction.Parameters,
+    Response : DeleteNotesGroupsAppFunction.Response
+> {
+    /**
+     * Deletes notes groups with the given parameters and returns the IDs of the groups that were
+     * successfully deleted.
+     *
+     * The implementing app should throw an appropriate subclass of
+     * [androidx.appfunctions.AppFunctionException] in exceptional cases, except for failures in
+     * deleting individual groupId(s), which can be ignored when returning the
+     * [DeleteNotesGroupsAppFunction.Response].
+     *
+     * @param appFunctionContext The AppFunction execution context.
+     * @param parameters The parameters of the groups to delete.
+     * @return The response including the list of successfully deleted group IDs.
+     */
+    public suspend fun deleteNotesGroups(
+        appFunctionContext: AppFunctionContext,
+        parameters: Parameters,
+    ): Response
+
+    /** The parameters of the groups to delete. */
+    public interface Parameters {
+        /** The [AppFunctionNotesGroup.id] of the groups to delete. */
+        public val groupIds: List<String>
+    }
+
+    /** The response including the list of successfully deleted group IDs. */
+    public interface Response {
+        /** The IDs of the deleted groups. */
+        public val groupIds: List<String>
+    }
+
+    public companion object {
+
         /** Current schema version. */
         @RestrictTo(LIBRARY_GROUP) internal const val SCHEMA_VERSION: Int = 2
     }
@@ -375,4 +548,16 @@ public interface AppFunctionNote {
         public val mimeType: String?
             get() = null
     }
+
+    /** The ID of the group the note is in, if any else `null`. */
+    public val groupId: String?
+        get() = null
+}
+
+/** Represents a group used to organize notes. */
+public interface AppFunctionNotesGroup {
+    /** The ID of the group. */
+    public val id: String
+    /** The name of the group. */
+    public val name: String
 }
