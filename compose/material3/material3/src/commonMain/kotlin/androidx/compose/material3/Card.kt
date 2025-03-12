931e4f8b93c025a6e8a2efb4c83fff9782341942
Mariano Martin <ymarian@google.com>
2025-03-12 13:27:19 -0400

    [Elevation] Create an interface for component elevations
    This allows to do elevation targeted optimizations
    
    Test: Existing elevation tests
    Relnote: Elevation components now share a common interface and have public constructors and properties.
    
    Change-Id: Ibb172060c7f26c0bf4f19ef48d75ae56e4c0057c

--- compose/material3/material3/src/commonMain/kotlin/androidx/compose/material3/Card.kt
+++ compose/material3/material3/src/commonMain/kotlin/androidx/compose/material3/Card.kt
@@ -16,32 +16,19 @@
 
 package androidx.compose.material3
 
-import androidx.compose.animation.core.Animatable
-import androidx.compose.animation.core.VectorConverter
 import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.interaction.DragInteraction
-import androidx.compose.foundation.interaction.FocusInteraction
-import androidx.compose.foundation.interaction.HoverInteraction
 import androidx.compose.foundation.interaction.Interaction
-import androidx.compose.foundation.interaction.InteractionSource
 import androidx.compose.foundation.interaction.MutableInteractionSource
-import androidx.compose.foundation.interaction.PressInteraction
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.ColumnScope
-import androidx.compose.material3.internal.animateElevation
 import androidx.compose.material3.tokens.ElevatedCardTokens
 import androidx.compose.material3.tokens.FilledCardTokens
 import androidx.compose.material3.tokens.OutlinedCardTokens
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.Immutable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.Stable
-import androidx.compose.runtime.State
-import androidx.compose.runtime.mutableStateListOf
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.graphics.compositeOver
@@ -405,7 +392,7 @@ object CardDefaults {
         disabledElevation: Dp = FilledCardTokens.DisabledContainerElevation
     ): CardElevation =
         CardElevation(
-            defaultElevation = defaultElevation,
+            elevation = defaultElevation,
             pressedElevation = pressedElevation,
             focusedElevation = focusedElevation,
             hoveredElevation = hoveredElevation,
@@ -435,7 +422,7 @@ object CardDefaults {
         disabledElevation: Dp = ElevatedCardTokens.DisabledContainerElevation
     ): CardElevation =
         CardElevation(
-            defaultElevation = defaultElevation,
+            elevation = defaultElevation,
             pressedElevation = pressedElevation,
             focusedElevation = focusedElevation,
             hoveredElevation = hoveredElevation,
@@ -464,7 +451,7 @@ object CardDefaults {
         disabledElevation: Dp = OutlinedCardTokens.DisabledContainerElevation
     ): CardElevation =
         CardElevation(
-            defaultElevation = defaultElevation,
+            elevation = defaultElevation,
             pressedElevation = pressedElevation,
             focusedElevation = focusedElevation,
             hoveredElevation = hoveredElevation,
@@ -637,141 +624,21 @@ object CardDefaults {
  * - See [CardDefaults.outlinedCardElevation] for the default elevation used in an [OutlinedCard].
  */
 @Immutable
-class CardElevation
-internal constructor(
-    private val defaultElevation: Dp,
-    private val pressedElevation: Dp,
-    private val focusedElevation: Dp,
-    private val hoveredElevation: Dp,
-    private val draggedElevation: Dp,
-    private val disabledElevation: Dp
-) {
-    /**
-     * Represents the shadow elevation used in a card, depending on its [enabled] state and
-     * [interactionSource].
-     *
-     * Shadow elevation is used to apply a shadow around the card to give it higher emphasis.
-     *
-     * @param enabled whether the card is enabled
-     * @param interactionSource the [InteractionSource] for this card
-     */
-    @Composable
-    internal fun shadowElevation(
-        enabled: Boolean,
-        interactionSource: InteractionSource?
-    ): State<Dp> {
-        if (interactionSource == null) {
-            return remember { mutableStateOf(defaultElevation) }
-        }
-        return animateElevation(enabled = enabled, interactionSource = interactionSource)
-    }
-
-    @Composable
-    private fun animateElevation(
-        enabled: Boolean,
-        interactionSource: InteractionSource
-    ): State<Dp> {
-        val interactions = remember { mutableStateListOf<Interaction>() }
-        LaunchedEffect(interactionSource) {
-            interactionSource.interactions.collect { interaction ->
-                when (interaction) {
-                    is HoverInteraction.Enter -> {
-                        interactions.add(interaction)
-                    }
-                    is HoverInteraction.Exit -> {
-                        interactions.remove(interaction.enter)
-                    }
-                    is FocusInteraction.Focus -> {
-                        interactions.add(interaction)
-                    }
-                    is FocusInteraction.Unfocus -> {
-                        interactions.remove(interaction.focus)
-                    }
-                    is PressInteraction.Press -> {
-                        interactions.add(interaction)
-                    }
-                    is PressInteraction.Release -> {
-                        interactions.remove(interaction.press)
-                    }
-                    is PressInteraction.Cancel -> {
-                        interactions.remove(interaction.press)
-                    }
-                    is DragInteraction.Start -> {
-                        interactions.add(interaction)
-                    }
-                    is DragInteraction.Stop -> {
-                        interactions.remove(interaction.start)
-                    }
-                    is DragInteraction.Cancel -> {
-                        interactions.remove(interaction.start)
-                    }
-                }
-            }
-        }
-
-        val interaction = interactions.lastOrNull()
-
-        val target =
-            if (!enabled) {
-                disabledElevation
-            } else {
-                when (interaction) {
-                    is PressInteraction.Press -> pressedElevation
-                    is HoverInteraction.Enter -> hoveredElevation
-                    is FocusInteraction.Focus -> focusedElevation
-                    is DragInteraction.Start -> draggedElevation
-                    else -> defaultElevation
-                }
-            }
-
-        val animatable = remember { Animatable(target, Dp.VectorConverter) }
-
-        LaunchedEffect(target) {
-            if (animatable.targetValue != target) {
-                if (!enabled) {
-                    // No transition when moving to a disabled state.
-                    animatable.snapTo(target)
-                } else {
-                    val lastInteraction =
-                        when (animatable.targetValue) {
-                            pressedElevation -> PressInteraction.Press(Offset.Zero)
-                            hoveredElevation -> HoverInteraction.Enter()
-                            focusedElevation -> FocusInteraction.Focus()
-                            draggedElevation -> DragInteraction.Start()
-                            else -> null
-                        }
-                    animatable.animateElevation(
-                        from = lastInteraction,
-                        to = interaction,
-                        target = target
-                    )
-                }
-            }
-        }
-
-        return animatable.asState()
-    }
+class CardElevation(
+    override val elevation: Dp,
+    override val pressedElevation: Dp,
+    override val focusedElevation: Dp,
+    override val hoveredElevation: Dp,
+    override val draggedElevation: Dp,
+    override val disabledElevation: Dp
+) : ComponentElevation {
 
     override fun equals(other: Any?): Boolean {
-        if (this === other) return true
-        if (other == null || other !is CardElevation) return false
-
-        if (defaultElevation != other.defaultElevation) return false
-        if (pressedElevation != other.pressedElevation) return false
-        if (focusedElevation != other.focusedElevation) return false
-        if (hoveredElevation != other.hoveredElevation) return false
-        if (disabledElevation != other.disabledElevation) return false
-
-        return true
+        return ComponentElevation.equals(this, other)
     }
 
     override fun hashCode(): Int {
-        var result = defaultElevation.hashCode()
-        result = 31 * result + pressedElevation.hashCode()
-        result = 31 * result + focusedElevation.hashCode()
-        result = 31 * result + hoveredElevation.hashCode()
-        result = 31 * result + disabledElevation.hashCode()
-        return result
+        return ComponentElevation.hashCode(this)
     }
 }
 
