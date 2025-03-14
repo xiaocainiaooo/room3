43ab8a45fa21d0cd4d0e18867031b1ce4d7501ab
Pierluigi Fimiano <pierf@google.com>
2025-03-14 14:59:23 +0000

    Use static private values for platform event sources.
    Test: Unit tests
    Bug: 402993790
    Change-Id: Ie345ee0608ae85f11f7ee245baaaea9904544afb

--- wear/protolayout/protolayout-expression/src/main/java/androidx/wear/protolayout/expression/PlatformEventSources.kt
+++ wear/protolayout/protolayout-expression/src/main/java/androidx/wear/protolayout/expression/PlatformEventSources.kt
@@ -21,17 +21,29 @@ import androidx.wear.protolayout.expression.DynamicBuilders.DynamicBool
 /** Dynamic types for platform events */
 public object PlatformEventSources {
 
+    /** A [DynamicBool] which receives the current visibility status from platform. */
+    @RequiresSchemaVersion(major = 1, minor = 500)
+    private val isLayoutVisible: DynamicBool = DynamicBool.from(Keys.LAYOUT_VISIBILITY)
+
     /**
-     * Creates a [DynamicBool] which receives the current visibility status from platform.
+     * A [DynamicBool] which receives the current state of any pending updates for the current
+     * layout.
+     */
+    @ProtoLayoutExperimental
+    @RequiresSchemaVersion(major = 1, minor = 600)
+    private val isLayoutUpdatePending: DynamicBool = DynamicBool.from(Keys.LAYOUT_UPDATE_PENDING)
+
+    /**
+     * Returns a [DynamicBool] which receives the current visibility status from platform.
      *
      * The visibility status value is `true` when layout is visible, and `false` when invisible.
      */
     @JvmStatic
     @RequiresSchemaVersion(major = 1, minor = 500)
-    public fun isLayoutVisible(): DynamicBool = DynamicBool.from(Keys.LAYOUT_VISIBILITY)
+    public fun isLayoutVisible(): DynamicBool = isLayoutVisible
 
     /**
-     * Creates a [DynamicBool] which receives the current state of any pending updates for the
+     * Returns a [DynamicBool] which receives the current state of any pending updates for the
      * current layout.
      *
      * The layout update pending status is `true` from when a new layout is requested until it is
@@ -40,7 +52,7 @@ public object PlatformEventSources {
     @JvmStatic
     @ProtoLayoutExperimental
     @RequiresSchemaVersion(major = 1, minor = 600)
-    public fun isLayoutUpdatePending(): DynamicBool = DynamicBool.from(Keys.LAYOUT_UPDATE_PENDING)
+    public fun isLayoutUpdatePending(): DynamicBool = isLayoutUpdatePending
 
     /** Data sources keys for platform event. */
     public object Keys {
