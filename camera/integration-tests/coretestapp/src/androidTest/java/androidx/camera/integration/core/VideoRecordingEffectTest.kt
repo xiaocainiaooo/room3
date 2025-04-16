70f5c3e7586830eeeed7a7057eb5a9f685ee97e2
mingdatsai <mingdatsai@google.com>
2025-04-16 17:13:14 +0800

    Add SensorPatternUtil for camera integration tests
    Extract SensorPatternUtil from VideoRecordingEffectTest for reuse
    in other effect related tests.
    
    Also, add check in the verifyColor method to prevent denominator
    to be zero.
    
    Bug: 410479600
    Test: manual test and VideoRecordingEffectTest
    Change-Id: I53a5e493d9da10748bdd410c7256e5b21011b075

--- camera/integration-tests/coretestapp/src/androidTest/java/androidx/camera/integration/core/VideoRecordingEffectTest.kt
+++ camera/integration-tests/coretestapp/src/androidTest/java/androidx/camera/integration/core/VideoRecordingEffectTest.kt
@@ -18,23 +18,11 @@ package androidx.camera.integration.core
 
 import android.Manifest
 import android.content.Context
-import android.graphics.Color
 import android.hardware.camera2.CameraCaptureSession
-import android.hardware.camera2.CameraCharacteristics
-import android.hardware.camera2.CameraCharacteristics.COLOR_CORRECTION_ABERRATION_MODE_OFF
-import android.hardware.camera2.CameraCharacteristics.CONTROL_MODE_OFF
-import android.hardware.camera2.CameraCharacteristics.DISTORTION_CORRECTION_MODE_OFF
-import android.hardware.camera2.CameraCharacteristics.EDGE_MODE_OFF
-import android.hardware.camera2.CameraCharacteristics.SENSOR_AVAILABLE_TEST_PATTERN_MODES
-import android.hardware.camera2.CameraCharacteristics.SHADING_MODE_OFF
 import android.hardware.camera2.CameraMetadata.SENSOR_TEST_PATTERN_MODE_SOLID_COLOR
 import android.hardware.camera2.CaptureRequest
-import android.hardware.camera2.CaptureResult
 import android.hardware.camera2.CaptureResult.SENSOR_TEST_PATTERN_MODE
 import android.hardware.camera2.TotalCaptureResult
-import android.hardware.camera2.params.ColorSpaceTransform
-import android.hardware.camera2.params.RggbChannelVector
-import android.hardware.camera2.params.TonemapCurve
 import android.media.ThumbnailUtils
 import android.os.Build
 import android.util.Rational
@@ -43,21 +31,20 @@ import android.view.Surface
 import androidx.annotation.RequiresApi
 import androidx.camera.camera2.Camera2Config
 import androidx.camera.camera2.pipe.integration.CameraPipeConfig
-import androidx.camera.core.CameraInfo
 import androidx.camera.core.CameraSelector
 import androidx.camera.core.CameraXConfig
 import androidx.camera.core.DynamicRange
 import androidx.camera.core.Preview
 import androidx.camera.core.UseCaseGroup
 import androidx.camera.core.ViewPort
-import androidx.camera.integration.core.util.Camera2InteropUtil
-import androidx.camera.integration.core.util.Camera2InteropUtil.builder
-import androidx.camera.integration.core.util.Camera2InteropUtil.from
 import androidx.camera.integration.core.util.Camera2InteropUtil.setCameraCaptureSessionCallback
+import androidx.camera.integration.core.util.SensorPatternUtil.ColorChannel
+import androidx.camera.integration.core.util.SensorPatternUtil.assumeSolidColorPatternSupported
+import androidx.camera.integration.core.util.SensorPatternUtil.setSolidColorPatternToCamera
+import androidx.camera.integration.core.util.SensorPatternUtil.verifyColor
 import androidx.camera.integration.core.util.doTempRecording
 import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.camera.lifecycle.awaitInstance
-import androidx.camera.testing.impl.AndroidUtil
 import androidx.camera.testing.impl.CameraPipeConfigTestRule
 import androidx.camera.testing.impl.CameraUtil
 import androidx.camera.testing.impl.SurfaceTextureProvider
@@ -66,7 +53,6 @@ import androidx.camera.testing.impl.fakes.FakeLifecycleOwner
 import androidx.camera.video.Recorder
 import androidx.camera.video.VideoCapture
 import androidx.concurrent.futures.await
-import androidx.palette.graphics.Palette
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.filters.LargeTest
 import androidx.test.filters.SdkSuppress
@@ -81,7 +67,6 @@ import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.runBlocking
 import kotlinx.coroutines.withContext
 import org.junit.Assume
-import org.junit.AssumptionViolatedException
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
@@ -116,9 +101,6 @@ class VideoRecordingEffectTest(
     @get:Rule val wakelockEmptyActivityRule = WakelockEmptyActivityRule()
 
     companion object {
-        private val LIMITED = setOf(CameraCharacteristics.INFO_SUPPORTED_HARDWARE_LEVEL_LIMITED)
-        private val FULL = setOf(CameraCharacteristics.INFO_SUPPORTED_HARDWARE_LEVEL_FULL)
-        private val LEVEL_3 = setOf(CameraCharacteristics.INFO_SUPPORTED_HARDWARE_LEVEL_3)
 
         @JvmStatic
         @Parameterized.Parameters(name = "{1}+{0}")
@@ -207,12 +189,6 @@ class VideoRecordingEffectTest(
             dynamicRange = DynamicRange.HLG_10_BIT
         )
 
-    private enum class ColorChannel {
-        RED,
-        GREEN,
-        BLUE
-    }
-
     @RequiresApi(29)
     private fun runSolidColorRecordingTest(
         colorChannel: ColorChannel,
@@ -227,7 +203,7 @@ class VideoRecordingEffectTest(
             }
 
             // Assume the device supports solid color test patterns
-            assumeSolidColorPatternSupported(camInfo)
+            assumeSolidColorPatternSupported(camInfo, implName)
 
             val testPatternModeFlow = MutableStateFlow<Int?>(null)
             val captureCallback =
@@ -273,23 +249,12 @@ class VideoRecordingEffectTest(
                     SurfaceTextureProvider.createAutoDrainingSurfaceTextureProvider()
                 val camera =
                     camProvider.bindToLifecycle(lifecycleOwner, cameraSelector, useCaseGroup)
-
-                Camera2InteropUtil.Camera2CameraControlWrapper.from(implName, camera.cameraControl)
-                    .apply {
-                        setCaptureRequestOptions(
-                            createMinimalProcessedSolidColorCaptureRequestOptions(
-                                colorChannel,
-                                camera.cameraInfo
-                            )
-                        )
-                    }
+                setSolidColorPatternToCamera(camera, colorChannel, implName)
 
                 lifecycleOwner.startAndResume()
                 try {
                     // Wait for a capture result with the test image
-                    testPatternModeFlow.first {
-                        it == CaptureResult.SENSOR_TEST_PATTERN_MODE_SOLID_COLOR
-                    }
+                    testPatternModeFlow.first { it == SENSOR_TEST_PATTERN_MODE_SOLID_COLOR }
 
                     with(
                         doTempRecording(
@@ -303,26 +268,7 @@ class VideoRecordingEffectTest(
                         try {
                             ThumbnailUtils.createVideoThumbnail(this, Size(90, 160), null).also {
                                 bitmap ->
-                                val colorPalette = Palette.Builder(bitmap).generate()
-                                val primaryColor = colorPalette.getDominantColor(0x00000000)
-                                val normalizedDenom =
-                                    Color.red(primaryColor) +
-                                        Color.green(primaryColor) +
-                                        Color.blue(primaryColor)
-
-                                val expectedDominantColor =
-                                    when (colorChannel) {
-                                        ColorChannel.RED ->
-                                            Color.red(primaryColor).toFloat() / normalizedDenom
-                                        ColorChannel.GREEN ->
-                                            Color.green(primaryColor).toFloat() / normalizedDenom
-                                        ColorChannel.BLUE ->
-                                            Color.blue(primaryColor).toFloat() / normalizedDenom
-                                    }
-
-                                // Assert the selected channel is at least 2 / 3
-                                // of the signal on from the sensor
-                                assertThat(expectedDominantColor).isAtLeast(2f / 3)
+                                assertThat(verifyColor(bitmap, colorChannel)).isTrue()
                             }
                         } finally {
                             delete()
@@ -335,180 +281,6 @@ class VideoRecordingEffectTest(
                 }
             }
         }
-
-    private fun assumeSolidColorPatternSupported(cameraInfo: CameraInfo) {
-        // Skip for b/342016557
-        Assume.assumeFalse(
-            "Emulator API 30 reports incorrect supported available test pattern modes",
-            Build.VERSION.SDK_INT == 30 && AndroidUtil.isEmulator()
-        )
-
-        with(Camera2InteropUtil.Camera2CameraInfoWrapper.from(implName, cameraInfo)) {
-            val availableTestPatterns = getCameraCharacteristic(SENSOR_AVAILABLE_TEST_PATTERN_MODES)
-            if (availableTestPatterns?.contains(SENSOR_TEST_PATTERN_MODE_SOLID_COLOR) == false) {
-                throw AssumptionViolatedException(
-                    "Camera does not support solid color test pattern."
-                )
-            }
-        }
-    }
-
-    private fun createMinimalProcessedSolidColorCaptureRequestOptions(
-        colorChannel: ColorChannel,
-        cameraInfo: CameraInfo
-    ): Camera2InteropUtil.CaptureRequestOptionsWrapper {
-        val sensorData =
-            when (colorChannel) {
-                ColorChannel.RED -> {
-                    // Create sensor data of R: 100%, G: 0%, B: 0%
-                    intArrayOf(
-                        /*r=*/ 0xFFFFFFFF.toInt(),
-                        /*g_even=*/ 0,
-                        /*g_odd=*/ 0,
-                        /*b=*/ 0
-                    )
-                }
-                ColorChannel.GREEN -> {
-                    // Create sensor data of R: 0%, G: 100%, B: 0%
-                    intArrayOf(
-                        /*r=*/ 0,
-                        /*g_even=*/ 0xFFFFFFFF.toInt(),
-                        /*g_odd=*/ 0xFFFFFFFF.toInt(),
-                        /*b=*/ 0
-                    )
-                }
-                ColorChannel.BLUE -> {
-                    // Create sensor data of R: 0%, G: 0%, B: 100%
-                    intArrayOf(
-                        /*r=*/ 0,
-                        /*g_even=*/ 0,
-                        /*g_odd=*/ 0,
-                        /*b=*/ 0xFFFFFFFF.toInt()
-                    )
-                }
-            }
-
-        return Camera2InteropUtil.CaptureRequestOptionsWrapper.builder(implName)
-            .apply {
-                setCaptureRequestOption(CaptureRequest.SENSOR_TEST_PATTERN_DATA, sensorData)
-
-                setCaptureRequestOption(
-                    CaptureRequest.SENSOR_TEST_PATTERN_MODE,
-                    CaptureRequest.SENSOR_TEST_PATTERN_MODE_SOLID_COLOR
-                )
-
-                with(Camera2InteropUtil.Camera2CameraInfoWrapper.from(implName, cameraInfo)) {
-                    val availableAberrationModes =
-                        getCameraCharacteristic(
-                            CameraCharacteristics.COLOR_CORRECTION_AVAILABLE_ABERRATION_MODES
-                        )
-                    if (COLOR_CORRECTION_ABERRATION_MODE_OFF isOneOf availableAberrationModes) {
-                        setCaptureRequestOption(
-                            CaptureRequest.COLOR_CORRECTION_ABERRATION_MODE,
-                            CaptureRequest.COLOR_CORRECTION_ABERRATION_MODE_OFF
-                        )
-                    }
-
-                    val availableEdgeModes =
-                        getCameraCharacteristic(CameraCharacteristics.EDGE_AVAILABLE_EDGE_MODES)
-                    if (EDGE_MODE_OFF isOneOf availableEdgeModes) {
-                        setCaptureRequestOption(
-                            CaptureRequest.EDGE_MODE,
-                            CaptureRequest.EDGE_MODE_OFF
-                        )
-                    }
-
-                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
-                        val availableDistortionCorrectionModes =
-                            getCameraCharacteristic(
-                                CameraCharacteristics.DISTORTION_CORRECTION_AVAILABLE_MODES
-                            )
-                        if (
-                            DISTORTION_CORRECTION_MODE_OFF isOneOf
-                                availableDistortionCorrectionModes
-                        ) {
-                            setCaptureRequestOption(
-                                CaptureRequest.DISTORTION_CORRECTION_MODE,
-                                CaptureRequest.DISTORTION_CORRECTION_MODE_OFF
-                            )
-                        }
-                    }
-
-                    val hardwareLevel =
-                        getCameraCharacteristic(CameraCharacteristics.INFO_SUPPORTED_HARDWARE_LEVEL)
-                    if (
-                        hardwareLevel isOneOf (LIMITED + FULL + LEVEL_3) ||
-                            Build.VERSION.SDK_INT >= Build.VERSION_CODES.M &&
-                                CONTROL_MODE_OFF isOneOf
-                                    getCameraCharacteristic(
-                                        CameraCharacteristics.CONTROL_AVAILABLE_MODES
-                                    )
-                    ) {
-                        setCaptureRequestOption(
-                            CaptureRequest.CONTROL_MODE,
-                            CaptureRequest.CONTROL_MODE_OFF
-                        )
-                    }
-
-                    if (
-                        hardwareLevel isOneOf (FULL + LEVEL_3) ||
-                            Build.VERSION.SDK_INT >= Build.VERSION_CODES.M &&
-                                SHADING_MODE_OFF isOneOf
-                                    getCameraCharacteristic(
-                                        CameraCharacteristics.SHADING_AVAILABLE_MODES
-                                    )
-                    ) {
-                        setCaptureRequestOption(
-                            CaptureRequest.SHADING_MODE,
-                            CaptureRequest.SHADING_MODE_OFF
-                        )
-                    }
-
-                    if (hardwareLevel isOneOf (FULL + LEVEL_3)) {
-                        setCaptureRequestOption(
-                            CaptureRequest.COLOR_CORRECTION_MODE,
-                            CaptureRequest.COLOR_CORRECTION_MODE_TRANSFORM_MATRIX
-                        )
-
-                        setCaptureRequestOption(
-                            CaptureRequest.COLOR_CORRECTION_GAINS,
-                            RggbChannelVector(1f, 1f, 1f, 1f)
-                        )
-
-                        setCaptureRequestOption(
-                            CaptureRequest.COLOR_CORRECTION_TRANSFORM,
-                            ColorSpaceTransform(
-                                intArrayOf(1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1)
-                            )
-                        )
-                    }
-
-                    val availableTonemapModes =
-                        getCameraCharacteristic(
-                            CameraCharacteristics.TONEMAP_AVAILABLE_TONE_MAP_MODES
-                        )
-                    if (
-                        CameraCharacteristics.TONEMAP_MODE_CONTRAST_CURVE isOneOf
-                            availableTonemapModes
-                    ) {
-                        setCaptureRequestOption(
-                            CaptureRequest.TONEMAP_MODE,
-                            CaptureRequest.TONEMAP_MODE_CONTRAST_CURVE
-                        )
-
-                        setCaptureRequestOption(
-                            CaptureRequest.TONEMAP_CURVE,
-                            TonemapCurve(
-                                floatArrayOf(0f, 0f, 1f, 1f),
-                                floatArrayOf(0f, 0f, 1f, 1f),
-                                floatArrayOf(0f, 0f, 1f, 1f)
-                            )
-                        )
-                    }
-                }
-            }
-            .build()
-    }
 
     private inline fun runCameraTest(
         cameraConfig: CameraXConfig,
@@ -525,7 +297,3 @@ class VideoRecordingEffectTest(
         }
     }
 }
-
-private infix fun <T> T.isOneOf(set: Set<T>?) = set?.contains(this) ?: false
-
-private infix fun Int.isOneOf(array: IntArray?) = array?.contains(this) ?: false
