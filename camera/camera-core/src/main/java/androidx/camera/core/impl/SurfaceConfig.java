5e02a6cc6ff72cbae076b4c85cb3d135a806fb99
Tahsin Masrur <tahsinm@google.com>
2025-04-28 12:52:38 +0000

    Add feature combination support to SurfaceConfig
    - Adds new ConfigSize enum labels for feature combination while renaming some existing ones for better differentiation
    - Adds the logic to transform sizes to feature combination specific ConfigSize, ConfigSource is added to make this process easier and more scalable for future
    - Creates a new SurfaceConfigTest class to test all SurfaceConfig functionalities
    - Adds comments in SupportedSurfaceCombination classes to clarify some cases
    
    Bug: 413946820
    Test: SurfaceConfigTest
    Change-Id: Icdf4dc504a48a55612ee0da49daa6395c18caabd

--- camera/camera-core/src/main/java/androidx/camera/core/impl/SurfaceConfig.java
+++ camera/camera-core/src/main/java/androidx/camera/core/impl/SurfaceConfig.java
@@ -20,6 +20,9 @@ import static androidx.camera.core.impl.CameraMode.ULTRA_HIGH_RESOLUTION_CAMERA;
 
 import android.graphics.ImageFormat;
 import android.hardware.camera2.CameraCaptureSession.StateCallback;
+import android.hardware.camera2.CameraCharacteristics;
+import android.hardware.camera2.CameraDevice;
+import android.hardware.camera2.params.StreamConfigurationMap;
 import android.os.Handler;
 import android.util.Size;
 
@@ -34,7 +37,7 @@ import java.util.List;
 /**
  * Surface configuration type and size pair
  *
- * <p>{@link android.hardware.camera2.CameraDevice#createCaptureSession} defines the default
+ * <p>{@link CameraDevice#createCaptureSession} defines the default
  * guaranteed stream combinations for different hardware level devices. It defines what combination
  * of surface configuration type and size pairs can be supported for different hardware level camera
  * devices.
@@ -42,6 +45,15 @@ import java.util.List;
 @AutoValue
 public abstract class SurfaceConfig {
     public static final long DEFAULT_STREAM_USE_CASE_VALUE = 0;
+    public static final ConfigSize[] FEATURE_COMBO_QUERY_SUPPORTED_SIZES = {
+            ConfigSize.S720P_16_9,
+            ConfigSize.S1080P_4_3,
+            ConfigSize.S1080P_16_9,
+            ConfigSize.S1440P_16_9,
+            ConfigSize.UHD,
+            ConfigSize.X_VGA,
+    };
+
     /** Prevent subclassing */
     SurfaceConfig() {
     }
@@ -76,7 +88,7 @@ public abstract class SurfaceConfig {
      * <p> Stream use case is an int flag used to specify the purpose of the stream associated
      * with this surface. Use cases for the camera2 implementation that are available on devices can
      * be found in
-     * {@link android.hardware.camera2.CameraCharacteristics#SCALER_AVAILABLE_STREAM_USE_CASES}
+     * {@link CameraCharacteristics#SCALER_AVAILABLE_STREAM_USE_CASES}
      *
      * <p>See {@link android.hardware.camera2.params.OutputConfiguration#setStreamUseCase}
      * to see how Camera2 framework uses this.
@@ -138,7 +150,8 @@ public abstract class SurfaceConfig {
             @CameraMode.Mode int cameraMode,
             int imageFormat,
             @NonNull Size size,
-            @NonNull SurfaceSizeDefinition surfaceSizeDefinition) {
+            @NonNull SurfaceSizeDefinition surfaceSizeDefinition,
+            @NonNull ConfigSource configSource) {
         ConfigType configType =
                 SurfaceConfig.getConfigType(imageFormat);
         ConfigSize configSize = ConfigSize.NOT_SUPPORT;
@@ -148,10 +161,27 @@ public abstract class SurfaceConfig {
 
         if (cameraMode == CameraMode.CONCURRENT_CAMERA) {
             if (sizeArea <= SizeUtil.getArea(surfaceSizeDefinition.getS720pSize(imageFormat))) {
-                configSize = ConfigSize.s720p;
+                configSize = ConfigSize.S720P_16_9;
             } else if (sizeArea <= SizeUtil.getArea(surfaceSizeDefinition.getS1440pSize(
                     imageFormat))) {
-                configSize = ConfigSize.s1440p;
+                configSize = ConfigSize.S1440P_4_3;
+            }
+        } else if (configSource == ConfigSource.FEATURE_COMBINATION_TABLE) {
+            // Try all fixes sizes first for exact match
+            for (ConfigSize supportedSize : FEATURE_COMBO_QUERY_SUPPORTED_SIZES) {
+                if (size.equals(supportedSize.getRelatedFixedSize())) {
+                    configSize = supportedSize;
+                    break;
+                }
+            }
+
+            // There was no fixed size match, so try the max supported size next
+            if (configSize == ConfigSize.NOT_SUPPORT) {
+                Size maximumSize = surfaceSizeDefinition.getMaximumSize(imageFormat);
+
+                if (size == maximumSize) {
+                    configSize = ConfigSize.MAXIMUM;
+                }
             }
         } else {
             if (sizeArea <= SizeUtil.getArea(surfaceSizeDefinition.getAnalysisSize())) {
@@ -184,7 +214,8 @@ public abstract class SurfaceConfig {
      * The Camera2 configuration type for the surface.
      *
      * <p>These are the enumerations defined in {@link
-     * android.hardware.camera2.CameraDevice#createCaptureSession(List, StateCallback, Handler)}.
+     * CameraDevice#createCaptureSession(List, StateCallback, Handler)} or {@link
+     * CameraCharacteristics#INFO_SESSION_CONFIGURATION_QUERY_VERSION}.
      */
     public enum ConfigType {
         PRIV,
@@ -195,57 +226,178 @@ public abstract class SurfaceConfig {
     }
 
     /**
+     * Represents the source of config sizes, usually some stream config table defined in
+     * {@link CameraDevice#createCaptureSession(List, StateCallback, Handler)}
+     * or
+     * {@link CameraCharacteristics#INFO_SESSION_CONFIGURATION_QUERY_VERSION}.
+     */
+    public enum ConfigSource {
+        /**
+         * Represents the stream config table defined in
+         * {@link CameraCharacteristics#INFO_SESSION_CONFIGURATION_QUERY_VERSION}.
+         */
+        FEATURE_COMBINATION_TABLE,
+
+        /**
+         * Represents the guaranteed stream config tables defined in
+         * {@link CameraDevice#createCaptureSession(List, StateCallback, Handler)}.
+         */
+        CAPTURE_SESSION_TABLES,
+    }
+
+    /**
      * The Camera2 stream sizes for the surface.
      *
      * <p>These are the enumerations defined in {@link
-     * android.hardware.camera2.CameraDevice#createCaptureSession(List, StateCallback, Handler)}.
+     * CameraDevice#createCaptureSession(List, StateCallback, Handler)}
+     * for most cases, or
+     * {@link CameraCharacteristics#INFO_SESSION_CONFIGURATION_QUERY_VERSION}
+     * for feature combination cases.
      */
     public enum ConfigSize {
         /** Default VGA size is 640x480, which is the default size of Image Analysis. */
-        VGA(0),
+        VGA(0, new Size(640, 480)),
+        /** X_VGA size refers to 1024x768 which is of 4:3 aspect ratio. */
+        X_VGA(1, new Size(1024, 768)),
         /**
-         * s720p refers to the best size match to the device's screen resolution, or to 720p
-         * (1280x720), whichever is smaller.
+         * Represents 720P (1280x720) resolution of 16:9 resolution.
+         *
+         * <p> For cases like concurrent camera which supports lower resolutions as well for a
+         * specified stream size, it refers to the camera device's maximum resolution for that
+         * format from {@link StreamConfigurationMap#getOutputSizes(int)} or to 720p (1280x720),
+         * whichever is smaller.
+         *
+         * <p> For cases like feature combination which supports only the exact resolutions for a
+         * specified stream size, not lower resolutions, it refers to exactly 720P (1280x720).
          */
-        s720p(1),
+        S720P_16_9(2, new Size(1280, 720)),
         /**
          * PREVIEW refers to the best size match to the device's screen resolution, or to 1080p
          * (1920x1080), whichever is smaller.
          */
-        PREVIEW(2),
+        PREVIEW(3),
         /**
-         * s1440p refers to the best size match to the device's screen resolution, or to 1440p
-         * (1920x1440), whichever is smaller.
+         * Represents 1080P (1440x1080) resolution of 4:3 aspect ratio.
+         *
+         * <p> For cases like concurrent camera which supports lower resolutions as well for a
+         * specified stream size, it refers to the camera device's maximum resolution for that
+         * format from {@link StreamConfigurationMap#getOutputSizes(int)} or to 1080P (1440x1080),
+         * whichever is smaller.
+         *
+         * <p> For cases like feature combination which supports only the exact resolutions for a
+         * specified stream size, not lower resolutions, it refers to exactly 1080P (1440x1080).
          */
-        s1440p(3),
+        S1080P_4_3(4, new Size(1440, 1080)),
+        /**
+         * Represents 1080P (1920x1080) resolution of 16:9 aspect ratio.
+         *
+         * <p> For cases like concurrent camera which supports lower resolutions as well for a
+         * specified stream size, it refers to the camera device's maximum resolution for that
+         * format from {@link StreamConfigurationMap#getOutputSizes(int)} or to 1080P (1920x1080),
+         * whichever is smaller.
+         *
+         * <p> For cases like feature combination which supports only the exact resolutions for a
+         * specified stream size, not lower resolutions, it refers to exactly 1080P (1920x1080).
+         */
+        S1080P_16_9(5, new Size(1920, 1080)),
+        /**
+         * Represents 1440P (1920x1440) resolution of 4:3 aspect ratio.
+         *
+         * <p> For cases like concurrent camera which supports lower resolutions as well for a
+         * specified stream size, it refers to the camera device's maximum resolution for that
+         * format from {@link StreamConfigurationMap#getOutputSizes(int)} or to 1440P (1920x1440),
+         * whichever is smaller.
+         *
+         * <p> For cases like feature combination which supports only the exact resolutions for a
+         * specified stream size, not lower resolutions, it refers to exactly 1440P (1920x1440).
+         */
+        S1440P_4_3(6, new Size(1920, 1440)),
+        /**
+         * Represents 1440P (2560x1440) resolution of 16:9 aspect ratio.
+         *
+         * <p> For cases like concurrent camera which supports lower resolutions as well for a
+         * specified stream size, it refers to the camera device's maximum resolution for that
+         * format from {@link StreamConfigurationMap#getOutputSizes(int)} or to 1440P (2560x1440),
+         * whichever is smaller.
+         *
+         * <p> For cases like feature combination which supports only the exact resolutions for a
+         * specified stream size, not lower resolutions, it refers to exactly 1440P (2560x1440).
+         */
+        S1440P_16_9(7, new Size(2560, 1440)),
+        /**
+         * Represents UHD (3840x2160) resolution, which is of 16:9 aspect ratio.
+         *
+         * <p> For cases like concurrent camera which supports lower resolutions as well for a
+         * specified stream size, it refers to the camera device's maximum resolution for that
+         * format from {@link StreamConfigurationMap#getOutputSizes(int)} or to UHD (3840x2160),
+         * whichever is smaller.
+         *
+         * <p> For cases like feature combination which supports only the exact resolutions for a
+         * specified stream size, not lower resolutions, it refers to exactly UHD (3840x2160).
+         */
+        UHD(8, new Size(3840, 2160)),
         /**
          * RECORD refers to the camera device's maximum supported recording resolution, as
          * determined by CamcorderProfile.
          */
-        RECORD(4),
+        RECORD(9),
         /**
          * MAXIMUM refers to the camera device's maximum output resolution for that format or
          * target from StreamConfigurationMap.getOutputSizes() or getHighResolutionOutputSizes()
          * in the default sensor pixel mode.
          */
-        MAXIMUM(5),
+        MAXIMUM(10),
+        /**
+         * Refers to the camera device's maximum 4:3 output resolution for that format or target
+         * from {@link StreamConfigurationMap#getOutputSizes} or
+         * {@link StreamConfigurationMap#getHighResolutionOutputSizes} in the default sensor pixel
+         * mode.
+         */
+        MAXIMUM_4_3(11),
+        /**
+         * Refers to the camera device's maximum 16:9 output resolution for that format or target
+         * from {@link StreamConfigurationMap#getOutputSizes} or
+         * {@link StreamConfigurationMap#getHighResolutionOutputSizes} in the default sensor pixel
+         * mode.
+         */
+        MAXIMUM_16_9(12),
         /**
          * ULTRA_MAXIMUM refers to the camera device's maximum output resolution for that format or
          * target from StreamConfigurationMap.getOutputSizes() or getHighResolutionOutputSizes()
          * in the maximum resolution sensor pixel mode.
          */
-        ULTRA_MAXIMUM(6),
+        ULTRA_MAXIMUM(13),
         /** NOT_SUPPORT is for the size larger than MAXIMUM */
-        NOT_SUPPORT(7);
+        NOT_SUPPORT(14);
 
         final int mId;
+        final Size mRelatedFixedSize;
 
         ConfigSize(int id) {
+            this(id, null);
+        }
+
+        ConfigSize(int id, Size relatedFixedSize) {
             mId = id;
+            mRelatedFixedSize = relatedFixedSize;
         }
 
         int getId() {
             return mId;
+        }
+
+        /**
+         * Returns the fixed size that is related to the provided {@link ConfigSize}, null if none.
+         *
+         * <p> Depending on the source table, it may act as the upper bound for the config size
+         * (e.g. concurrent camera guaranteed stream combo table) or it may be the exact size (e.g.
+         * feature combination table).
+         *
+         * <p> For config sizes without any pre-defined fixed related to it (e.g.
+         * {@link ConfigSize#RECORD}), a null value will be returned.
+         */
+        Size getRelatedFixedSize() {
+            return mRelatedFixedSize;
         }
     }
 }
