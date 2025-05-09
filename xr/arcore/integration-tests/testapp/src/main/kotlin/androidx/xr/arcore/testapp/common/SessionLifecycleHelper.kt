2c7e6f527ba4c56ab3dbf44113a652f4db1bba7d
grozdanov <grozdanov@google.com>
2025-05-09 18:21:05 +0000

    Sync ARCoreTests consolidated app to androidx-main.
    Bug: 415768376
    Test: Tested in XR emulator
    Relnote: "Sync consolidated tests app for ARCore"
    Sync CL: N/A
    
    Change-Id: I6e0f8361cb1aab7dd81e014a29551545b255b3f5

---
+++ xr/arcore/integration-tests/testapp/src/main/kotlin/androidx/xr/arcore/testapp/common/SessionLifecycleHelper.kt
@@ -0,0 +1,151 @@
+/*
+ * Copyright 2024 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package androidx.xr.arcore.testapp.common
+
+import android.util.Log
+import android.widget.Toast
+import androidx.activity.ComponentActivity
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.lifecycle.DefaultLifecycleObserver
+import androidx.lifecycle.LifecycleOwner
+import androidx.xr.runtime.Config
+import androidx.xr.runtime.Session
+import androidx.xr.runtime.SessionConfigureConfigurationNotSupported
+import androidx.xr.runtime.SessionConfigureGooglePlayServicesLocationLibraryNotLinked
+import androidx.xr.runtime.SessionConfigurePermissionsNotGranted
+import androidx.xr.runtime.SessionConfigureSuccess
+import androidx.xr.runtime.SessionCreateApkRequired
+import androidx.xr.runtime.SessionCreatePermissionsNotGranted
+import androidx.xr.runtime.SessionCreateResult
+import androidx.xr.runtime.SessionCreateSuccess
+import androidx.xr.runtime.SessionCreateUnsupportedDevice
+import androidx.xr.runtime.SessionResumePermissionsNotGranted
+import androidx.xr.runtime.SessionResumeSuccess
+
+/**
+ * Observer class to manage the lifecycle of the JXR Runtime Session based on the lifecycle owner
+ * (activity).
+ */
+class SessionLifecycleHelper(
+    val activity: ComponentActivity,
+    val config: Config = Config(),
+    val onSessionAvailable: (Session) -> Unit = {},
+    val onSessionCreateActionRequired: (SessionCreateResult) -> Unit = {},
+) : DefaultLifecycleObserver {
+
+    /** Accessed through the [onSessionAvailable] callback. */
+    private lateinit var session: Session
+    private lateinit var requestPermissionLauncher: ActivityResultLauncher<Array<String>>
+
+    override fun onCreate(owner: LifecycleOwner) {
+        registerRequestPermissionLauncher(activity)
+
+        tryCreateSession()
+    }
+
+    override fun onResume(owner: LifecycleOwner) {
+        if (!this::session.isInitialized) {
+            return
+        }
+        when (val result = session.resume()) {
+            is SessionResumeSuccess -> {}
+            is SessionResumePermissionsNotGranted -> {
+                requestPermissionLauncher.launch(result.permissions.toTypedArray())
+            }
+        }
+    }
+
+    override fun onPause(owner: LifecycleOwner) {
+        if (!this::session.isInitialized) {
+            return
+        }
+        session.pause()
+    }
+
+    override fun onDestroy(owner: LifecycleOwner) {
+        if (!this::session.isInitialized) {
+            return
+        }
+        session.destroy()
+    }
+
+    private fun registerRequestPermissionLauncher(activity: ComponentActivity) {
+        requestPermissionLauncher =
+            activity.registerForActivityResult(
+                ActivityResultContracts.RequestMultiplePermissions()
+            ) { permissions ->
+                val allPermissionsGranted = permissions.all { it.value }
+                if (!allPermissionsGranted) {
+                    Toast.makeText(
+                            activity,
+                            "Required permissions were not granted, closing activity. ",
+                            Toast.LENGTH_LONG,
+                        )
+                        .show()
+                    activity.finish()
+                } else {
+                    activity.recreate()
+                }
+            }
+    }
+
+    public fun tryCreateSession() {
+        when (val result = Session.create(activity)) {
+            is SessionCreateSuccess -> {
+                session = result.session
+                when (val configResult = session.configure(config)) {
+                    is SessionConfigurePermissionsNotGranted -> {
+                        requestPermissionLauncher.launch(configResult.permissions.toTypedArray())
+                    }
+                    is SessionConfigureConfigurationNotSupported -> {
+                        showErrorMessage("Session configuration not supported.")
+                        activity.finish()
+                    }
+                    is SessionConfigureGooglePlayServicesLocationLibraryNotLinked -> {
+                        Log.e(
+                            TAG,
+                            "Google Play Services Location Library is not linked, this should not happen.",
+                        )
+                    }
+                    is SessionConfigureSuccess -> {
+                        onSessionAvailable(session)
+                    }
+                }
+            }
+            is SessionCreatePermissionsNotGranted -> {
+                requestPermissionLauncher.launch(result.permissions.toTypedArray())
+            }
+            is SessionCreateApkRequired -> {
+                onSessionCreateActionRequired(result)
+            }
+            is SessionCreateUnsupportedDevice -> {
+                showErrorMessage("Session could not be created, device is Unsupported.")
+                activity.finish()
+            }
+        }
+    }
+
+    companion object {
+        private val TAG = this::class.simpleName
+    }
+
+    private fun <F> showErrorMessage(error: F) {
+        Log.e(TAG, error.toString())
+        Toast.makeText(activity, error.toString(), Toast.LENGTH_LONG).show()
+    }
+}
