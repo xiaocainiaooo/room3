9fb0b379ea601a85186a7f7928946680f5fe9470
Kevin Truong <kevinctruong@google.com>
2025-05-19 15:46:16 -0700

    [ButtonGroup] Fix horizontal arrangement
    Do a max of the desired space and mainAxisMin to account for fillMaxWidth
    
    Test: Add testing for horizontal arrangement start, center, and end.
    Bug: 418848401
    Change-Id: If31b5a186f4f0d9560d418cb122f4e845dde8175

--- compose/material3/material3/src/androidInstrumentedTest/kotlin/androidx/compose/material3/ButtonGroupTest.kt
+++ compose/material3/material3/src/androidInstrumentedTest/kotlin/androidx/compose/material3/ButtonGroupTest.kt
@@ -18,12 +18,15 @@ package androidx.compose.material3
 
 import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.testutils.assertIsEqualTo
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.assertIsNotDisplayed
@@ -35,9 +38,11 @@ import androidx.compose.ui.test.onNodeWithText
 import androidx.compose.ui.test.performClick
 import androidx.compose.ui.test.performTouchInput
 import androidx.compose.ui.test.swipe
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.MediumTest
+import com.google.common.truth.Truth.assertThat
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -976,5 +981,83 @@ class ButtonGroupTest {
         }
 
         rule.onNodeWithText("9").assertIsDisplayed()
+    }
+
+    @Test
+    fun horizontalArrangement_startArrangement_buttonGroupPositioning() {
+        val interactionSources = List(3) { MutableInteractionSource() }
+        var screenWidth: Dp = 0.dp
+        rule.setMaterialContent(lightColorScheme()) {
+            val configuration = LocalConfiguration.current
+            screenWidth = configuration.screenWidthDp.dp
+            Box(Modifier.testTag(wrapperTestTag)) {
+                ButtonGroup(
+                    overflowIndicator = {},
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.Start,
+                ) {
+                    for (i in 0..interactionSources.lastIndex) {
+                        clickableItem(onClick = {}, label = "$i")
+                    }
+                }
+            }
+        }
+        val buttonOneBounds = rule.onNodeWithText("0").getUnclippedBoundsInRoot()
+        val buttonThreeBounds = rule.onNodeWithText("2").getUnclippedBoundsInRoot()
+        val buttonRange = buttonOneBounds.left..buttonThreeBounds.right
+        val expectedPositioning = screenWidth / 3
+        assertThat(expectedPositioning in buttonRange).isTrue()
+    }
+
+    @Test
+    fun horizontalArrangement_centerArrangement_buttonGroupPositioning() {
+        val interactionSources = List(3) { MutableInteractionSource() }
+        var screenWidth: Dp = 0.dp
+        rule.setMaterialContent(lightColorScheme()) {
+            val configuration = LocalConfiguration.current
+            screenWidth = configuration.screenWidthDp.dp
+            Box(Modifier.testTag(wrapperTestTag)) {
+                ButtonGroup(
+                    overflowIndicator = {},
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.Center,
+                ) {
+                    for (i in 0..interactionSources.lastIndex) {
+                        clickableItem(onClick = {}, label = "$i")
+                    }
+                }
+            }
+        }
+        val buttonOneBounds = rule.onNodeWithText("0").getUnclippedBoundsInRoot()
+        val buttonThreeBounds = rule.onNodeWithText("2").getUnclippedBoundsInRoot()
+        val buttonRange = buttonOneBounds.left..buttonThreeBounds.right
+        val expectedPositioning = screenWidth / 2
+        assertThat(expectedPositioning in buttonRange).isTrue()
+    }
+
+    @Test
+    fun horizontalArrangement_endArrangement_buttonGroupPositioning() {
+        val interactionSources = List(3) { MutableInteractionSource() }
+        var screenWidth: Dp = 0.dp
+        rule.setMaterialContent(lightColorScheme()) {
+            val configuration = LocalConfiguration.current
+            screenWidth = configuration.screenWidthDp.dp
+            Box(Modifier.testTag(wrapperTestTag)) {
+                ButtonGroup(
+                    overflowIndicator = {},
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.End,
+                ) {
+                    for (i in 0..interactionSources.lastIndex) {
+                        clickableItem(onClick = {}, label = "$i")
+                    }
+                }
+            }
+        }
+        val buttonOneBounds = rule.onNodeWithText("0").getUnclippedBoundsInRoot()
+        val buttonThreeBounds = rule.onNodeWithText("2").getUnclippedBoundsInRoot()
+        val buttonRange = buttonOneBounds.left..buttonThreeBounds.right
+        val expectedPositioning = screenWidth * 2 / 3
+        assertThat(expectedPositioning in buttonRange).isTrue()
     }
 }
