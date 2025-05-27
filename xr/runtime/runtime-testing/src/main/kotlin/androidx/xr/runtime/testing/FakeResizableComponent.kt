df3bbb586414848d83b012920331ef6ce218e2a9
Tim Wong <timtw@google.com>
2025-05-26 21:10:57 -0700

    Implement FakeResizableComponent with unit tests
    Bug: 416368622
    Test: Tested with FakeResizableComponentTest in the console
    
    Change-Id: I159f13516463bc64067d73521ca9c5c5ceae2f22

--- xr/runtime/runtime-testing/src/main/kotlin/androidx/xr/runtime/testing/FakeResizableComponent.kt
+++ xr/runtime/runtime-testing/src/main/kotlin/androidx/xr/runtime/testing/FakeResizableComponent.kt
@@ -22,30 +22,57 @@ import androidx.xr.runtime.internal.ResizableComponent
 import androidx.xr.runtime.internal.ResizeEventListener
 import java.util.concurrent.Executor
 
-// TODO: b/405218432 - Implement this correctly instead of stubbing it out.
 /** Fake implementation of [ResizableComponent] for testing. */
 @RestrictTo(RestrictTo.Scope.LIBRARY_GROUP_PREFIX)
-public class FakeResizableComponent : ResizableComponent, FakeComponent() {
+public class FakeResizableComponent(
+    override var size: Dimensions = Dimensions(2.0f, 2.0f, 2.0f),
+    override var minimumSize: Dimensions = Dimensions(1.0f, 1.0f, 1.0f),
+    override var maximumSize: Dimensions = Dimensions(2.0f, 2.0f, 2.0f),
+    override var fixedAspectRatio: Float = 20.0f,
+    @get:Suppress("GetterSetterNames") override var autoHideContent: Boolean = false,
+    @get:Suppress("GetterSetterNames") override var autoUpdateSize: Boolean = false,
+    @get:Suppress("GetterSetterNames") override var forceShowResizeOverlay: Boolean = false,
+) : FakeComponent(), ResizableComponent {
 
-    override var size: Dimensions = Dimensions(0.0f, 0.0f, 0.0f)
+    /**
+     * For test purposes only.
+     *
+     * Represents the set of listeners that are invoked through the resize operation. In tests, you
+     * can use this map to manually trigger the listener and verify that your code responds
+     * correctly to resize operation.
+     *
+     * <p>Map of resize event listeners to their executors.
+     */
+    public val resizeEventListenersMap: MutableMap<ResizeEventListener, Executor> = mutableMapOf()
 
-    override var minimumSize: Dimensions = Dimensions(0.0f, 0.0f, 0.0f)
-
-    override var maximumSize: Dimensions = Dimensions(0.0f, 0.0f, 0.0f)
-
-    override var fixedAspectRatio: Float = 0.0f
-
-    @get:Suppress("GetterSetterNames") override var autoHideContent: Boolean = false
-
-    @get:Suppress("GetterSetterNames") override var autoUpdateSize: Boolean = false
-
-    @get:Suppress("GetterSetterNames") override var forceShowResizeOverlay: Boolean = false
-
+    /**
+     * Adds the listener to the set of listeners that are invoked through the resize operation, such
+     * as start, ongoing and end.
+     *
+     * <p>The listener is invoked on the provided executor. If the app intends to modify the UI
+     * elements/views during the callback, the app should provide the thread executor that is
+     * appropriate for the UI operations. For example, if the app is using the main thread to render
+     * the UI, the app should provide the main thread (Looper.getMainLooper()) executor. If the app
+     * is using a separate thread to render the UI, the app should provide the executor for that
+     * thread.
+     *
+     * @param executor The executor to use for the listener callback.
+     * @param resizeEventListener The listener to be invoked when a resize event occurs.
+     */
     @Suppress("ExecutorRegistration")
     override fun addResizeEventListener(
         executor: Executor,
         resizeEventListener: ResizeEventListener,
-    ) {}
+    ) {
+        resizeEventListenersMap.put(resizeEventListener, executor)
+    }
 
-    override fun removeResizeEventListener(resizeEventListener: ResizeEventListener) {}
+    /**
+     * Removes the given listener from the set of listeners for the resize events.
+     *
+     * @param resizeEventListener The listener to be removed.
+     */
+    override fun removeResizeEventListener(resizeEventListener: ResizeEventListener) {
+        resizeEventListenersMap.remove(resizeEventListener)
+    }
 }
