c070393fcc91d4abd72de0bb7e59d827012d4ef6
Cheng-Nan Yang <ychengnan@google.com>
2025-05-27 13:49:39 +0800

    Implement a fake version of GltfEntity.
    Bug 411503838
    Test: Test FakeGltfEntity by FakeGltfEntityTest
    Relnote: Fake runtime implementation.
    
    Change-Id: Ie836a9519fed1f7e7a65826fa8ada2dc71aadeff

--- xr/scenecore/scenecore-testing/src/main/kotlin/androidx/xr/scenecore/testing/FakeGltfEntity.kt
+++ xr/scenecore/scenecore-testing/src/main/kotlin/androidx/xr/scenecore/testing/FakeGltfEntity.kt
@@ -20,23 +20,95 @@ import androidx.annotation.RestrictTo
 import androidx.xr.scenecore.internal.GltfEntity
 import androidx.xr.scenecore.internal.MaterialResource
 
-// TODO: b/405218432 - Implement this correctly instead of stubbing it out.
-/** Test-only implementation of [androidx.xr.scenecore.internal.GltfEntity] */
+/** Test-only implementation of [GltfEntity] */
 @RestrictTo(RestrictTo.Scope.LIBRARY_GROUP_PREFIX)
-public class FakeGltfEntity : GltfEntity, FakeEntity() {
+public open class FakeGltfEntity() : FakeEntity(), GltfEntity {
+    public class Node {
+        public val nodeName: String = "glTF node"
+        public val materialArray: Array<FakeResource> =
+            arrayOf(FakeResource(1), FakeResource(2), FakeResource(3))
+    }
+
+    public val node: Node = Node()
 
     @GltfEntity.AnimationStateValue
-    override val animationState: Int = GltfEntity.AnimationState.STOPPED
+    private var _animationState: Int = GltfEntity.AnimationState.STOPPED
 
-    override fun startAnimation(loop: Boolean, animationName: String?) {}
+    /** Returns the current animation state of the glTF entity. */
+    @GltfEntity.AnimationStateValue
+    override val animationState: Int
+        get() = _animationState
 
-    override fun stopAnimation() {}
+    /**
+     * Indicates whether the animation is currently looping. In tests, you can
+     * - call [startAnimation] with loop set to true to simulate looping the animation and verify
+     *   that your code responds correctly to the animation looping.
+     * - call [stopAnimation] to clear the looping state and verify that your code responds
+     *   correctly to the animation stopping.
+     */
+    public var isLooping: Boolean = false
 
     override fun setMaterialOverride(
         material: MaterialResource,
         nodeName: String,
         primitiveIndex: Int,
-    ) {}
+    ) {
+        if (nodeName == node.nodeName && primitiveIndex < node.materialArray.size) {
+            node.materialArray[primitiveIndex] = material as FakeResource
+        }
+    }
 
-    override fun clearMaterialOverride(nodeName: String, primitiveIndex: Int) {}
+    override fun clearMaterialOverride(nodeName: String, primitiveIndex: Int) {
+        if (nodeName == node.nodeName && primitiveIndex < node.materialArray.size) {
+            node.materialArray[primitiveIndex] = FakeResource(primitiveIndex.toLong())
+        }
+    }
+
+    /**
+     * The name of the animation that is currently playing. In tests, you can
+     * - call [startAnimation] with a supported animationName and verify that your code responds
+     *   correctly to the animation starting.
+     * - call [stopAnimation] to clear the value and verify that your code responds correctly to the
+     *   animation stopping.
+     */
+    public var currentAnimationName: String? = null
+        private set
+
+    /**
+     * A list of supported animation names with a default value of "animation_name" which is used in
+     * the scenecore/JxrPlatformAdapterAxrTest unit test. In tests, you can call [startAnimation]
+     * with a supported/unsupported animationName and verify that your code responds correctly to
+     * the [isLooping] and [currentAnimationName] values.
+     */
+    public var supportedAnimationNames: MutableList<String> = mutableListOf("animation_name")
+
+    /**
+     * Starts the animation with a supported given name when the animation state is STOPPED.
+     *
+     * @param currentAnimationName The name of the animation to start. If null is supplied, will
+     *   play the first animation found in the glTF.
+     * @param loop Whether the animation should loop.
+     */
+    override fun startAnimation(loop: Boolean, animationName: String?) {
+        if (
+            supportedAnimationNames.contains(animationName) &&
+                _animationState == GltfEntity.AnimationState.STOPPED
+        ) {
+
+            _animationState = GltfEntity.AnimationState.PLAYING
+
+            isLooping = loop
+            currentAnimationName = animationName
+        }
+    }
+
+    /** Stops the animation of the glTF entity. */
+    override fun stopAnimation() {
+        if (_animationState == GltfEntity.AnimationState.PLAYING) {
+            _animationState = GltfEntity.AnimationState.STOPPED
+
+            isLooping = false
+            currentAnimationName = null
+        }
+    }
 }
---
+++ xr/scenecore/scenecore-testing/src/test/kotlin/androidx/xr/scenecore/testing/FakeGltfEntityTest.kt
@@ -0,0 +1,114 @@
+/*
+ * Copyright 2025 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package androidx.xr.scenecore.testing
+
+import androidx.xr.scenecore.internal.GltfEntity
+import com.google.common.truth.Truth.assertThat
+import kotlinx.coroutines.runBlocking
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.JUnit4
+
+@RunWith(JUnit4::class)
+class FakeGltfEntityTest {
+
+    lateinit var underTest: FakeGltfEntity
+
+    @Before
+    fun setUp() {
+        underTest = FakeGltfEntity()
+    }
+
+    @Test
+    fun getInitialState_returnsDefaultValues() {
+        check(underTest.animationState == GltfEntity.AnimationState.STOPPED)
+        check(!underTest.isLooping)
+        check(underTest.currentAnimationName == null)
+    }
+
+    @Test
+    fun startAnimationWithSupportedAnimation_setsAnimationStateToPlaying() = runBlocking {
+        check(underTest.supportedAnimationNames.contains("animation_name"))
+
+        underTest.startAnimation(false, "test_animation")
+
+        // startAnimation doesn't work on an unsupported animation name.
+        assertThat(underTest.isLooping).isFalse()
+        assertThat(underTest.animationState).isEqualTo(GltfEntity.AnimationState.STOPPED)
+        assertThat(underTest.currentAnimationName).isNull()
+
+        underTest.startAnimation(loop = true, animationName = "animation_name")
+
+        assertThat(underTest.isLooping).isTrue()
+        assertThat(underTest.animationState).isEqualTo(GltfEntity.AnimationState.PLAYING)
+        assertThat(underTest.currentAnimationName).isEqualTo("animation_name")
+
+        underTest.supportedAnimationNames.add("test_animation")
+        underTest.startAnimation(false, "test_animation")
+
+        // startAnimation doesn't work when the animationState is PLAYING.
+        assertThat(underTest.isLooping).isTrue()
+        assertThat(underTest.animationState).isEqualTo(GltfEntity.AnimationState.PLAYING)
+        assertThat(underTest.currentAnimationName).isEqualTo("animation_name")
+
+        underTest.stopAnimation()
+
+        // stopAnimation resets all states.
+        assertThat(underTest.isLooping).isFalse()
+        assertThat(underTest.animationState).isEqualTo(GltfEntity.AnimationState.STOPPED)
+        assertThat(underTest.currentAnimationName).isNull()
+
+        underTest.startAnimation(false, "test_animation")
+
+        // Verifies that startAnimation works on the added supported animation name.
+        assertThat(underTest.isLooping).isFalse()
+        assertThat(underTest.animationState).isEqualTo(GltfEntity.AnimationState.PLAYING)
+        assertThat(underTest.currentAnimationName).isEqualTo("test_animation")
+    }
+
+    @Test
+    fun setMaterialOverride_setMaterialCorrectly() {
+        val material = FakeResource(123)
+        val nodeName = "glTF node"
+        val primitiveIndex = 0
+
+        check(underTest.node.materialArray[primitiveIndex] == FakeResource(1))
+
+        underTest.setMaterialOverride(material, nodeName, primitiveIndex)
+
+        assertThat(underTest.node.materialArray[primitiveIndex]).isEqualTo(material)
+    }
+
+    @Test
+    fun clearMaterialOverride_setAndClearOverrideMaterial_getMaterialAfterClearCorrectly() {
+        val material = FakeResource(123)
+        val nodeName = "glTF node"
+        val primitiveIndex = 0
+
+        check(underTest.node.materialArray[primitiveIndex] == FakeResource(1))
+
+        underTest.setMaterialOverride(material, nodeName, primitiveIndex)
+
+        assertThat(underTest.node.materialArray[primitiveIndex]).isEqualTo(material)
+
+        underTest.clearMaterialOverride(nodeName, primitiveIndex)
+
+        assertThat(underTest.node.materialArray[primitiveIndex])
+            .isEqualTo(FakeResource(primitiveIndex.toLong()))
+    }
+}
