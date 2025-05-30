2b59348ba244858dc4036809f8880e575590cf27
babelsw <babelsw@google.com>
2025-05-30 09:37:06 +0000

    Implement FakeActivitySpace.
    BUG: 412182320, 416596346
    Test: Tested by runtime-testing unit tests.
    
    Change-Id: I6f6903950a41fdc7ce645889c3d690a0ae69549e

--- xr/runtime/runtime-testing/src/main/kotlin/androidx/xr/runtime/testing/FakeActivitySpace.kt
+++ xr/runtime/runtime-testing/src/main/kotlin/androidx/xr/runtime/testing/FakeActivitySpace.kt
@@ -23,19 +23,83 @@ import androidx.xr.runtime.internal.Dimensions
 import androidx.xr.runtime.internal.HitTestResult
 import androidx.xr.runtime.math.BoundingBox
 import androidx.xr.runtime.math.Vector3
-import com.google.common.util.concurrent.Futures.immediateFailedFuture
+import com.google.common.util.concurrent.Futures.immediateFuture
 import com.google.common.util.concurrent.ListenableFuture
+import java.util.Collections
+import java.util.concurrent.atomic.AtomicReference
 
-// TODO: b/405218432 - Implement this correctly instead of stubbing it out.
-/** Test-only implementation of [ActivitySpace] */
+/** A test double for [ActivitySpace], designed for use in unit or integration tests. */
 @RestrictTo(RestrictTo.Scope.LIBRARY_GROUP_PREFIX)
-public class FakeActivitySpace : ActivitySpace, FakeSystemSpaceEntity() {
-    override val bounds: Dimensions = Dimensions(0.0f, 0.0f, 0.0f)
+public class FakeActivitySpace() : FakeSystemSpaceEntity(), ActivitySpace {
+    private val _bounds: AtomicReference<Dimensions> =
+        AtomicReference<Dimensions>(Dimensions(100.0f, 100.0f, 100.0f))
+    private val _onBoundsChangedListeners: MutableSet<ActivitySpace.OnBoundsChangedListener> =
+        Collections.synchronizedSet(
+            mutableSetOf(
+                ActivitySpace.OnBoundsChangedListener { newBounds -> _bounds.set(newBounds) }
+            )
+        )
 
+    /** Returns the bounds of this ActivitySpace. */
+    override val bounds: Dimensions
+        get() = _bounds.get()
+
+    /**
+     * For test purposes only.
+     *
+     * The set of listeners to be called when the bounds of the primary Activity change.
+     *
+     * @param bounds The new bounds of the primary Activity in Meters
+     */
+    public val onBoundsChangedListeners: Set<ActivitySpace.OnBoundsChangedListener>
+        get() = _onBoundsChangedListeners
+
+    /**
+     * For test purposes only.
+     *
+     * Simulates a bounds change event, invoking all registered listeners with the new bounds. This
+     * will also update the internal `bounds` property of this fake.
+     *
+     * @param bounds The new bounds to propagate to the listeners.
+     */
+    public fun onBoundsChanged(bounds: Dimensions) {
+        val listeners =
+            synchronized(_onBoundsChangedListeners) { _onBoundsChangedListeners.toSet() }
+        listeners.forEach { it.onBoundsChanged(bounds) }
+    }
+
+    /**
+     * Adds a listener to be called when the bounds of the primary Activity change. If the same
+     * listener is added multiple times, it will only fire each event on time.
+     *
+     * @param listener The listener to register.
+     */
     @Suppress("ExecutorRegistration")
-    override fun addOnBoundsChangedListener(listener: ActivitySpace.OnBoundsChangedListener) {}
+    override fun addOnBoundsChangedListener(listener: ActivitySpace.OnBoundsChangedListener) {
+        _onBoundsChangedListeners.add(listener)
+    }
 
-    override fun removeOnBoundsChangedListener(listener: ActivitySpace.OnBoundsChangedListener) {}
+    /**
+     * Removes a listener to be called when the bounds of the primary Activity change. If the given
+     * listener was not added, this call does nothing.
+     *
+     * @param listener The listener to unregister.
+     */
+    override fun removeOnBoundsChangedListener(listener: ActivitySpace.OnBoundsChangedListener) {
+        _onBoundsChangedListeners.remove(listener)
+    }
+
+    /**
+     * The [HitTestResult] that will be returned by [hitTestRelativeToActivityPose]. This can be
+     * modified in tests to simulate different hit test outcomes.
+     */
+    public var activitySpaceHitTestResult: HitTestResult =
+        HitTestResult(
+            hitPosition = null,
+            surfaceNormal = null,
+            surfaceType = HitTestResult.HitTestSurfaceType.HIT_TEST_RESULT_SURFACE_TYPE_UNKNOWN,
+            distance = 0f,
+        )
 
     @Suppress("AsyncSuffixFuture")
     override fun hitTestRelativeToActivityPose(
@@ -43,7 +107,7 @@ public class FakeActivitySpace : ActivitySpace, FakeSystemSpaceEntity() {
         direction: Vector3,
         @ActivityPose.HitTestFilterValue hitTestFilter: Int,
         activityPose: ActivityPose,
-    ): ListenableFuture<HitTestResult> = immediateFailedFuture(NotImplementedError())
+    ): ListenableFuture<HitTestResult> = immediateFuture(activitySpaceHitTestResult)
 
     override val recommendedContentBoxInFullSpace: BoundingBox
         get() =
