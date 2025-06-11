22730f2ca398c7df50d2fb143b006522660cfc5c
George Mount <mount@google.com>
2025-06-11 09:37:13 -0700

    Revert "Disable WindowInsetsRulers"
    This reverts commit d928668375fa66c6668aa3009dc8946f3ce1aaa1.
    
    Reason for revert: b/421431269
    
    I have fixed the tests that were breaking because of this change.
    
    Test: ran broken tests
    Bug: 421431269
    Change-Id: I52ecaacf5d957a9b18ae797575d2a4ccadef5ffa

--- compose/ui/ui/src/commonMain/kotlin/androidx/compose/ui/ComposeUiFlags.kt
+++ compose/ui/ui/src/commonMain/kotlin/androidx/compose/ui/ComposeUiFlags.kt
@@ -109,7 +109,7 @@ object ComposeUiFlags {
      * * `SafeContentRulers`
      */
     // off for b/410868572
-    @Suppress("MutableBareField") @JvmField var areWindowInsetsRulersEnabled = false
+    @Suppress("MutableBareField") @JvmField var areWindowInsetsRulersEnabled = true
 
     /**
      * With this flag on, when an AccessibilityService performs ACTION_FOCUS on a Composable node,
