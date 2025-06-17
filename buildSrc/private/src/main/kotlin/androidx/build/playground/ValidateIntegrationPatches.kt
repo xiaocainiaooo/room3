1a2f1b4c3562b21c7d31eebfa19252e733a71ed5
Aurimas Liutikas <aurimas@google.com>
2025-06-17 14:36:09 -0700

    Mark ValidateIntegrationPatches task as non-cacheable
    Before this change the assumption was that only the patch change could
    break the task, but it could also be changes to the androidx files.
    We had a build break ab/13660509 where this happened and because of
    caching it was not caught by test runners that run with remote cache
    enabled.
    
    Test: None
    Change-Id: Ic2d9dd97693c65b4b52435144eb7e6607792dea7

--- buildSrc/private/src/main/kotlin/androidx/build/playground/ValidateIntegrationPatches.kt
+++ buildSrc/private/src/main/kotlin/androidx/build/playground/ValidateIntegrationPatches.kt
@@ -25,15 +25,15 @@ import org.gradle.api.DefaultTask
 import org.gradle.api.GradleException
 import org.gradle.api.Project
 import org.gradle.api.file.DirectoryProperty
-import org.gradle.api.tasks.CacheableTask
 import org.gradle.api.tasks.InputDirectory
 import org.gradle.api.tasks.PathSensitive
 import org.gradle.api.tasks.PathSensitivity
 import org.gradle.api.tasks.TaskAction
 import org.gradle.process.ExecOperations
+import org.gradle.work.DisableCachingByDefault
 
 /** Validates that it is possible to apply the patch files in `.github/integration-patches`. */
-@CacheableTask
+@DisableCachingByDefault(because = "Patch applies to all files, any change could break it")
 abstract class ValidateIntegrationPatches : DefaultTask() {
     @get:Inject abstract val execOperations: ExecOperations
 
