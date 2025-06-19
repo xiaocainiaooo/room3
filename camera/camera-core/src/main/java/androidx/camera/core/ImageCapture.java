cb5eec13c7fd90cb0d696fb77b276f0d46424716
Tahsin Masrur <tahsinm@google.com>
2025-06-19 14:14:35 +0000

    Update ImageCapture dynamic range correctly for IMAGE_ULTRA_HDR feature
      Since feature group was being applied as last step in ImageCapture#onMergeConfig, the output format was not the updated one when updating the dynamic range.
    
    Bug: 426189330
    Test: ImageCaptureTest
    Change-Id: I8dd99c56e86868ffd6edba8b7251b86022e12e2b

--- camera/camera-core/src/main/java/androidx/camera/core/ImageCapture.java
+++ camera/camera-core/src/main/java/androidx/camera/core/ImageCapture.java
@@ -453,6 +453,10 @@ public final class ImageCapture extends UseCase {
     @Override
     protected @NonNull UseCaseConfig<?> onMergeConfig(@NonNull CameraInfoInternal cameraInfo,
             UseCaseConfig.@NonNull Builder<?, ?, ?> builder) {
+        // Apply config like JPEG_R output format first so that configs like input format, dynamic
+        // range etc. can be set correctly later
+        applyFeatureGroupToConfig(builder);
+
         if (cameraInfo.getCameraQuirks().contains(SoftwareJpegEncodingPreferredQuirk.class)) {
             // Request software JPEG encoder if quirk exists on this device, and the software JPEG
             // option has not already been explicitly set.
@@ -511,8 +515,6 @@ public final class ImageCapture extends UseCase {
             }
         }
 
-        applyFeatureGroupToConfig(builder);
-
         return builder.getUseCaseConfig();
     }
 
@@ -548,12 +550,6 @@ public final class ImageCapture extends UseCase {
                 }
             }
 
-            int inputFormat = ImageFormat.JPEG;
-            if (imageCaptureOutputFormat == ImageCapture.OUTPUT_FORMAT_JPEG_ULTRA_HDR) {
-                inputFormat = ImageFormat.JPEG_R;
-            }
-
-            builder.getMutableConfig().insertOption(OPTION_INPUT_FORMAT, inputFormat);
             builder.getMutableConfig().insertOption(OPTION_OUTPUT_FORMAT, imageCaptureOutputFormat);
         }
     }
