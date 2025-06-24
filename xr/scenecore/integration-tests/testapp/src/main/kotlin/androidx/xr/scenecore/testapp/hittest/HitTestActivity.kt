f8b00d2ac05f169d52f89ff15d8ad529fa1aab02
Ravin Anderson <ravinanderson@google.com>
2025-06-23 17:00:53 -0700

    Delete ListenableFuture functions from ScenePose in lieu of Java users depending on GuavaScenePose in :xr:scenecore:scenecore-guava.
    Bug: 427277524
    Test: presubmit tests + HitTestApp
    Relnote: Java users will depend on GuavaScenePose in :xr:scenecore:scenecore-guava for Guava equivalent Kotlin ScenePose API functions.
    
    Change-Id: Ia851535bdb3259a8866f974c1af3dfbbda1bdfc7

--- xr/scenecore/integration-tests/testapp/src/main/kotlin/androidx/xr/scenecore/testapp/hittest/HitTestActivity.kt
+++ xr/scenecore/integration-tests/testapp/src/main/kotlin/androidx/xr/scenecore/testapp/hittest/HitTestActivity.kt
@@ -93,40 +93,36 @@ class HitTestActivity : AppCompatActivity() {
         buttonHitTest.text = "Hit Test"
         buttonHitTest.setOnClickListener {
             if (session!!.scene.spatialUser.head != null) {
-
-                val hitTestFuture =
-                    session!!
-                        .scene
-                        .spatialUser
-                        .head!!
-                        .hitTestAsync(Vector3(), Vector3(0f, 0f, -1f))
-                        .get()
-                if (hitTestFuture.hitPosition != null && hitTestFuture.surfaceNormal != null) {
-                    val updatedRotation =
-                        Quaternion.fromLookTowards(
-                            hitTestFuture.surfaceNormal!!,
+                lifecycleScope.launch {
+                    val hitTest =
+                        session!!.scene.spatialUser.head!!.hitTest(Vector3(), Vector3(0f, 0f, -1f))
+                    if (hitTest.hitPosition != null && hitTest.surfaceNormal != null) {
+                        val updatedRotation =
+                            Quaternion.fromLookTowards(
+                                hitTest.surfaceNormal!!,
+                                session!!
+                                    .scene
+                                    .spatialUser
+                                    .head!!
+                                    .transformPoseTo(
+                                        Pose(Vector3(0f, 1f, 0f)),
+                                        session!!.scene.activitySpace,
+                                    )
+                                    .translation,
+                            )
+                        val hitTestPose =
                             session!!
                                 .scene
                                 .spatialUser
                                 .head!!
                                 .transformPoseTo(
-                                    Pose(Vector3(0f, 1f, 0f)),
+                                    Pose(hitTest.hitPosition!!, updatedRotation),
                                     session!!.scene.activitySpace,
                                 )
-                                .translation,
-                        )
-                    val hitTestPose =
-                        session!!
-                            .scene
-                            .spatialUser
-                            .head!!
-                            .transformPoseTo(
-                                Pose(hitTestFuture.hitPosition!!, updatedRotation),
-                                session!!.scene.activitySpace,
-                            )
-                    transformWidgetModel?.let {
-                        val gltfEntity = GltfModelEntity.create(session!!, it, hitTestPose)
-                        gltfEntity.parent = session!!.scene.activitySpace
+                        transformWidgetModel?.let {
+                            val gltfEntity = GltfModelEntity.create(session!!, it, hitTestPose)
+                            gltfEntity.parent = session!!.scene.activitySpace
+                        }
                     }
                 }
             }
