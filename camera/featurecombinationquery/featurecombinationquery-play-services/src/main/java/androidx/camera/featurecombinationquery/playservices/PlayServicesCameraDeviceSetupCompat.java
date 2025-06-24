9b247a011286f5241c052cce03a28de4818f7c09
Avichal Rakesh <arakesh@google.com>
2025-06-23 19:26:52 -0700

    Add featurecombinationquery-play-services implementation.
    This CL does the following to add initial play services implementation:
    
     - Adds dependency to play-services-cam-feature-combo-query.
       The actual prebuilts are dropped in Icd086104923fa
    
     - Updates the CameraDeviceSetupCompatFactory and CameraDeviceSetupCompat
       implementations to correctly interface with the play service library.
    
     - Updates the documentation to add missed restrictions and caveats that
       come from using the play services library.
    
    Bug: 356698584
    Test: ./gradlew camera:featurecombinationquery:featurecombinationquery:assemble
    Test: ./gradlew camera:featurecombinationquery:featurecombinationquery-play-services:assemble
    Test: camera-testapp-featurecombo works as expected
    Test: androidx.camera.integration.featurecombo.FeatureCombinationDeviceTest
          passes
    Change-Id: I214d200ae64284b4a7b96b022c542a1c4c3abdc5

--- camera/featurecombinationquery/featurecombinationquery-play-services/src/main/java/androidx/camera/featurecombinationquery/playservices/PlayServicesCameraDeviceSetupCompat.java
+++ camera/featurecombinationquery/featurecombinationquery-play-services/src/main/java/androidx/camera/featurecombinationquery/playservices/PlayServicesCameraDeviceSetupCompat.java
@@ -16,16 +16,25 @@
 
 package androidx.camera.featurecombinationquery.playservices;
 
+import static androidx.camera.featurecombinationquery.CameraDeviceSetupCompat.SupportQueryResult.RESULT_SUPPORTED;
 import static androidx.camera.featurecombinationquery.CameraDeviceSetupCompat.SupportQueryResult.RESULT_UNDEFINED;
+import static androidx.camera.featurecombinationquery.CameraDeviceSetupCompat.SupportQueryResult.RESULT_UNSUPPORTED;
 import static androidx.camera.featurecombinationquery.CameraDeviceSetupCompat.SupportQueryResult.SOURCE_PLAY_SERVICES;
 
 import android.hardware.camera2.params.SessionConfiguration;
+import android.os.Build;
+import android.os.RemoteException;
+import android.util.Log;
 
 import androidx.camera.featurecombinationquery.CameraDeviceSetupCompat;
 import androidx.camera.featurecombinationquery.CameraDeviceSetupCompatFactory;
 import androidx.camera.featurecombinationquery.SessionConfigurationLegacy;
 
+import com.google.android.gms.camera.feature.combination.query.CombinationQuery;
+import com.google.android.gms.camera.feature.combination.query.QueryResult;
+
 import org.jspecify.annotations.NonNull;
+import org.jspecify.annotations.Nullable;
 
 /**
  * A Google Play Services based {@link CameraDeviceSetupCompat} implementation.
@@ -35,22 +44,72 @@ import org.jspecify.annotations.NonNull;
  * this class via the {@link CameraDeviceSetupCompatFactory#getCameraDeviceSetupCompat} API.
  */
 public class PlayServicesCameraDeviceSetupCompat implements CameraDeviceSetupCompat {
+    private static final String TAG = PlayServicesCameraDeviceSetupCompat.class.getSimpleName();
 
-    public PlayServicesCameraDeviceSetupCompat(@NonNull String cameraId) {
-        // TODO: Implement this once Google Play Services CameraDeviceSetup is available.
+    @Nullable
+    private final CombinationQuery mQueryClient;
+    private final String mCameraId;
+
+    public PlayServicesCameraDeviceSetupCompat(@Nullable CombinationQuery queryClient,
+            @NonNull String cameraId) {
+        this.mQueryClient = queryClient;
+        this.mCameraId = cameraId;
     }
 
     @Override
     public @NonNull SupportQueryResult isSessionConfigurationSupported(
             @NonNull SessionConfiguration sessionConfig) {
-        // TODO: Implement this once Google Play Services CameraDeviceSetup is available.
-        return new SupportQueryResult(RESULT_UNDEFINED, SOURCE_PLAY_SERVICES, 0);
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.P) {
+            // SessionConfiguration was added in Android P. We have no way to query stuff before
+            // that. Return early.
+            return new SupportQueryResult(RESULT_UNDEFINED, SOURCE_PLAY_SERVICES, 0);
+        }
+
+        if (mQueryClient == null) {
+            return new SupportQueryResult(RESULT_UNDEFINED, SOURCE_PLAY_SERVICES, 0);
+        }
+
+        try {
+            QueryResult result = mQueryClient.isSessionConfigurationSupported(mCameraId,
+                    sessionConfig);
+            return fromPlayServicesQueryResult(result);
+        } catch (RemoteException e) {
+            Log.e(TAG, "Connection to play services broken. Failed to query feature combination",
+                    e);
+            return new SupportQueryResult(RESULT_UNDEFINED, SOURCE_PLAY_SERVICES, 0);
+        }
     }
 
     @Override
     public @NonNull SupportQueryResult isSessionConfigurationSupportedLegacy(
             @NonNull SessionConfigurationLegacy sessionConfig) {
-        // TODO: Implement this once Google Play Services CameraDeviceSetup is available.
-        return new SupportQueryResult(RESULT_UNDEFINED, SOURCE_PLAY_SERVICES, 0);
+        if (mQueryClient == null) {
+            return new SupportQueryResult(RESULT_UNDEFINED, SOURCE_PLAY_SERVICES, 0);
+        }
+
+        try {
+            QueryResult result = mQueryClient.isSessionConfigurationSupportedLegacy(mCameraId,
+                    sessionConfig.getOutputConfigurations(),
+                    sessionConfig.getSessionParameters().asMap());
+            return fromPlayServicesQueryResult(result);
+        } catch (RemoteException e) {
+            Log.e(TAG, "Could not query play services for querying feature combination", e);
+            return new SupportQueryResult(RESULT_UNDEFINED, SOURCE_PLAY_SERVICES, 0);
+        }
+    }
+
+    private static @NonNull SupportQueryResult fromPlayServicesQueryResult(
+            @NonNull QueryResult queryResult) {
+        switch (queryResult.getResult()) {
+            case QueryResult.Result.NOT_SUPPORTED:
+                return new SupportQueryResult(RESULT_UNSUPPORTED, SOURCE_PLAY_SERVICES,
+                        queryResult.getTimestamp());
+            case QueryResult.Result.SUPPORTED:
+                return new SupportQueryResult(RESULT_SUPPORTED, SOURCE_PLAY_SERVICES,
+                        queryResult.getTimestamp());
+            default:
+                return new SupportQueryResult(RESULT_UNDEFINED, SOURCE_PLAY_SERVICES,
+                        queryResult.getTimestamp());
+        }
     }
 }
