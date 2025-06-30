db7daa64910fbed9b5b435cce57da13a386d5882
Neda Topoljanac <tneda@google.com>
2025-06-30 20:45:32 +0000

    Add ProtoLayoutScope for resources and intent registration
    The implementaion  where this will be used and public API
    to get the object will be in a follow up.
    
    Bug: 428692423
    Test: Added
    Relnote: "Added ProtoLayoutScope concept in preparation for better resources handling in Tiles"
    Change-Id: I132ce6910147839f15c95c14ccc4f354b819f387

---
+++ wear/protolayout/protolayout/src/main/java/androidx/wear/protolayout/ProtoLayoutScope.kt
@@ -0,0 +1,118 @@
+/*
+ * Copyright 2025 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package androidx.wear.protolayout
+
+import android.app.PendingIntent
+import android.os.Bundle
+import androidx.annotation.MainThread
+import androidx.annotation.RestrictTo
+import androidx.annotation.RestrictTo.Scope
+import androidx.annotation.VisibleForTesting
+import androidx.wear.protolayout.ResourceBuilders.ImageResource
+import androidx.wear.protolayout.ResourceBuilders.Resources
+import java.util.Objects
+
+/**
+ * A scope object responsible for handling internal details of ProtoLayout layouts and Tiles.
+ *
+ * Object of this class, in Tiles cases, can be obtained via
+ * `androidx.wear.tiles.RequestBuilders#TileRequest.getScope`.
+ *
+ * Some example of usage:
+ * * Used for registering Android resources automatically instead of manually via
+ *   `androidx.wear.tiles.TileService.onTileResourcesRequest`.
+ *
+ * This class is not thread safe and should only be used from one thread, [MainThread].
+ */
+@MainThread
+public class ProtoLayoutScope
+@RestrictTo(RestrictTo.Scope.LIBRARY_GROUP_PREFIX)
+public constructor() {
+    /**
+     * Maps String key to the [ImageResource] type describing it in ProtoLayout terms.
+     *
+     * String key is generated based on the hash of the resource, to have its uniqueness and
+     * preserve the same value across diff updates.
+     *
+     * This can be:
+     * * Android Image [ResourceBuilders.AndroidImageResourceByResId]
+     * * Lottie animation [ResourceBuilders.AndroidLottieResourceByResId]
+     * * Inline Image [ResourceBuilders.InlineImageResource]
+     * * Android Animated Image [ResourceBuilders.AndroidAnimatedImageResourceByResId]
+     * * Android Seekable Animated Image
+     *   [ResourceBuilders.AndroidSeekableAnimatedImageResourceByResId]
+     */
+    @VisibleForTesting internal val resources: MutableMap<String, ImageResource> = mutableMapOf()
+
+    /** Maps Clickable ID to the [PendingIntent]. */
+    @VisibleForTesting internal val pendingIntents: Bundle = Bundle()
+
+    /**
+     * Registers the given Android resource that corresponds to the given String ProtoLayout
+     * resources ID and maps it to the given [ImageResource] type.
+     */
+    @RestrictTo(Scope.LIBRARY)
+    public fun registerResource(id: String, resource: ImageResource) {
+        resources[id] = resource
+    }
+
+    /** Registers the given [PendingIntent] with given clickable ID. */
+    @RestrictTo(Scope.LIBRARY)
+    public fun registerPendingIntent(id: String, intent: PendingIntent) {
+        pendingIntents.putParcelable(id, intent)
+    }
+
+    /**
+     * Returns [Resources] object containing all previously registered resources.
+     *
+     * Resource is registered with the String ID that is a hash value of all resources.
+     */
+    public fun collectResources(): Resources =
+        Resources.Builder()
+            .setVersion(hashedResources())
+            .apply {
+                for ((id, res) in resources) {
+                    addIdToImageMapping(/* id= */ id, /* image= */ res)
+                }
+            }
+            .build()
+
+    /**
+     * Returns a [Bundle] with all previously registered [PendingIntent].
+     *
+     * Bundle contains (key, value) pairs of [android.os.Parcelable], where `key` is String ID
+     * corresponding to the [ModifiersBuilders.Clickable] ID, and `value` is [PendingIntent].
+     */
+    public fun collectPendingIntents(): Bundle = pendingIntents.clone() as Bundle
+
+    /** Clears mappings for resources and pending intents. */
+    @RestrictTo(Scope.LIBRARY_GROUP_PREFIX)
+    public fun clear() {
+        resources.clear()
+        pendingIntents.clear()
+    }
+
+    /**
+     * Generates String version of hash codes for all [resources].
+     *
+     * Hash is calculated by first sorting the Map, so the order of added elements is not important.
+     * Then, we use hash for <id, resource> pair, and hash all those elements together.
+     */
+    private fun hashedResources(): String =
+        Objects.hash(resources.toSortedMap().map { (id, res) -> Pair(id, res).hashCode() }.toList())
+            .toString()
+}
