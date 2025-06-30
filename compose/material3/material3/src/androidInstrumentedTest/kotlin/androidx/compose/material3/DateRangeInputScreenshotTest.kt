0b87f21bf5771243df53728973e847fc85991e00
Shalom Gibly <sgibly@google.com>
2025-06-30 12:59:35 -0700

    Remove experimental annotations from date pickers
    - Remove @ExperimentalMaterial3Api annotations from the date picker and
      date range picker main functions and supporting states, classes,
      functions and types.
    - Removed the annotation from the DatePickerDialog.
    - Deleted a few date pickers deprecated hidden experimental functions
      that were deprecated a while ago.
    - Updated tests the samples to remove opt-ins.
    - Had to generate the API with ignore. See b/391848485 for a reference
      to a similar existing issue.
    
    
    
    Fixes: 407980840
    Fixed: 407982218
    Test: Updates tests
    Relnote: "Removed the experimental annotaion from DatePicker,
    DateRangePicker, and their supporting states, classes, and types."
    
    Change-Id: I0e4e0e1fb7ca05d21f9fc62e2411d533b4c5521b

--- compose/material3/material3/src/androidInstrumentedTest/kotlin/androidx/compose/material3/DateRangeInputScreenshotTest.kt
+++ compose/material3/material3/src/androidInstrumentedTest/kotlin/androidx/compose/material3/DateRangeInputScreenshotTest.kt
@@ -40,7 +40,6 @@ import org.junit.runners.Parameterized
 @RunWith(Parameterized::class)
 @LargeTest
 @SdkSuppress(minSdkVersion = Build.VERSION_CODES.O)
-@OptIn(ExperimentalMaterial3Api::class)
 class DateRangeInputScreenshotTest(private val scheme: ColorSchemeWrapper) {
 
     @get:Rule val rule = createComposeRule()
