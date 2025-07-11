67b99a744c7bac2d7b751bceec998e58e9c66ca4
Pradyuman Dixit <pdxt@google.com>
2025-07-11 12:31:02 +0000

    Update trace end-point for User interaction binder latency
    Earlier we were measuring only the binder latency till ProviderViewWrapper received the event. Now we are updating the end-point to be after the motion event has been scheduled.
    
    Test: UserInteractionBenchmark
    Bug: 415012285
    Change-Id: I9f4ac2700e9f617d0f68106ea60b6f69ef86c9a0

--- privacysandbox/ui/ui-provider/src/main/java/androidx/privacysandbox/ui/provider/ProviderViewWrapper.kt
+++ privacysandbox/ui/ui-provider/src/main/java/androidx/privacysandbox/ui/provider/ProviderViewWrapper.kt
@@ -24,7 +24,6 @@ import android.view.MotionEvent
 import android.widget.FrameLayout
 import androidx.annotation.RequiresApi
 import androidx.privacysandbox.ui.core.IMotionEventTransferCallback
-import androidx.tracing.trace
 
 /**
  * A container [FrameLayout] that wraps the provider content view.
@@ -83,7 +82,6 @@ internal class ProviderViewWrapper(context: Context) : FrameLayout(context) {
             }
         dispatchMessage.isAsynchronous = true
 
-        trace("ProviderViewWrapper#scheduleMotionEventProcessing", {})
         eventDispatchHandler?.sendMessageAtTime(
             dispatchMessage,
             eventTargetFrameTime + TRANSFERRED_EVENT_DISPATCH_DELAY_MS,
