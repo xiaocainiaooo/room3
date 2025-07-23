438080ccc35aa4fc56d086d5a17dde7cc0a75ce7
Sergio Sancho <ssancho@google.com>
2025-07-23 11:33:27 +0100

    Improve curved text documentation
    Test: N/A
    Relnote: Improve curved text documentation
    Bug: 366132767
    Change-Id: I9c29e496abd5ee8c9cee3d6d431f838b8de9b174

--- wear/compose/compose-material3/src/main/java/androidx/wear/compose/material3/CurvedText.kt
+++ wear/compose/compose-material3/src/main/java/androidx/wear/compose/material3/CurvedText.kt
@@ -93,7 +93,9 @@ import androidx.wear.compose.foundation.sizeIn
  * @param angularDirection Specify if the text is laid out clockwise or anti-clockwise, and if those
  *   needs to be reversed in a Rtl layout. If not specified, it will be inherited from the enclosing
  *   [curvedRow] or [CurvedLayout] See [CurvedDirection.Angular].
- * @param overflow How visual overflow should be handled.
+ * @param overflow How visual overflow should be handled. Note that this takes into account only
+ *   explicit size curved modifiers in this element, to size this element matching the parent's, add
+ *   a CurvedModifier.weight here.
  */
 public fun CurvedScope.curvedText(
     text: String,
