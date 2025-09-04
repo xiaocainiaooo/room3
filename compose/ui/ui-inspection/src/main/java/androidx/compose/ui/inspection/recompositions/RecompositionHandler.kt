b4b3948f09c64c5a09e3c3ee650e5cb2a2b42b5f
Jens Ole Lauridsen <jlauridsen@google.com>
2025-09-03 17:24:28 -0700

    Add ability to observe state reads
    Added:
    - field to UpdateSettingsCommand
    - new command: GetRecompositionStateReadCommand
    - new event: RecompositionStateReadEvent
    
    Bug: 434194620
    Test: RecompositionTest
    Change-Id: Id8b1ca2c05eb26158b4c07f402ab3eb2db13abe3

--- compose/ui/ui-inspection/src/main/java/androidx/compose/ui/inspection/recompositions/RecompositionHandler.kt
+++ compose/ui/ui-inspection/src/main/java/androidx/compose/ui/inspection/recompositions/RecompositionHandler.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2022 The Android Open Source Project
+ * Copyright 2025 The Android Open Source Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,11 +14,12 @@
  * limitations under the License.
  */
 
-package androidx.compose.ui.inspection
+package androidx.compose.ui.inspection.recompositions
 
 import android.util.Log
 import androidx.annotation.GuardedBy
 import androidx.compose.runtime.Composer
+import androidx.compose.ui.inspection.LOG_TAG
 import androidx.compose.ui.inspection.util.AnchorMap
 import androidx.inspection.ArtTooling
 
@@ -26,13 +27,18 @@ private const val START_RESTART_GROUP = "startRestartGroup(I)Landroidx/compose/r
 private const val SKIP_TO_GROUP_END = "skipToGroupEnd()V"
 
 /** Detection of recompose counts and skips from installing runtime hooks. */
-class RecompositionHandler(private val artTooling: ArtTooling, private val anchorMap: AnchorMap) {
-    /** For each composable store the recomposition [count] and [skips]. */
-    class Data(var count: Int, var skips: Int)
+open class RecompositionHandler<T : RecompositionData>(
+    protected val artTooling: ArtTooling,
+    protected val anchorMap: AnchorMap,
+    protected val createRecompositionData: () -> T,
+) {
+    protected val lock = Any()
 
-    private val lock = Any()
-    @GuardedBy("lock") private var currentlyCollecting = false
-    @GuardedBy("lock") private val counts = mutableMapOf<Any, Data>()
+    // True, if currently collecting recomposition counts.
+    @GuardedBy("lock") private var collectingRecompositionCounts = false
+
+    // The data collected for recomposition counts and state reads.
+    @GuardedBy("lock") protected val counts = mutableMapOf<Any, T>()
 
     init {
         currentHandler = this
@@ -47,18 +53,18 @@ class RecompositionHandler(private val artTooling: ArtTooling, private val ancho
         }
     }
 
-    fun dispose() {
+    open fun dispose() {
         counts.clear()
         currentHandler = null
     }
 
     fun changeCollectionMode(startCollecting: Boolean, keepCounts: Boolean) {
         synchronized(lock) {
-            if (startCollecting != currentlyCollecting) {
+            if (startCollecting != collectingRecompositionCounts) {
                 if (!hooksInstalled) {
                     installHooks(artTooling)
                 }
-                currentlyCollecting = startCollecting
+                collectingRecompositionCounts = startCollecting
             }
             if (!keepCounts) {
                 counts.clear()
@@ -66,35 +72,39 @@ class RecompositionHandler(private val artTooling: ArtTooling, private val ancho
         }
     }
 
-    fun getCounts(anchorId: Int): Data? {
+    // Return the recomposition counts and skips
+    fun getCounts(anchorHash: Int): RecompositionData? {
         synchronized(lock) {
-            return anchorMap[anchorId]?.let { counts[it] }
+            return anchorMap[anchorHash]?.let { counts[it] }
         }
     }
 
-    fun incrementCount(anchor: Any) {
+    // Increment the recomposition count.
+    // Adjust the state reads based on the max number of recompositions with state reads
+    // the agent is supposed to maintain.
+    open fun incrementRecompositionCount(anchor: Any): T? {
         synchronized(lock) {
-            if (currentlyCollecting) {
-                val data = counts.getOrPut(anchor) { Data(0, 0) }
-                data.count++
+            if (collectingRecompositionCounts) {
+                val data = counts.getOrPut(anchor, createRecompositionData)
+                data.incrementCount()
+                return data
             }
+            return null
         }
     }
 
-    fun incrementSkip(anchor: Any) {
+    // The last recomposition was skipped.
+    fun incrementRecompositionSkip(anchor: Any) {
         synchronized(lock) {
-            if (currentlyCollecting) {
-                counts[anchor]?.let {
-                    it.count--
-                    it.skips++
-                }
+            if (collectingRecompositionCounts) {
+                counts[anchor]?.incrementSkip()
             }
         }
     }
 }
 
 private var hooksInstalled = false
-private var currentHandler: RecompositionHandler? = null
+private var currentHandler: RecompositionHandler<*>? = null
 
 /**
  * We install 2 hooks:
@@ -110,7 +120,7 @@ private fun installHooks(artTooling: ArtTooling) {
     composerImplementationClasses().forEach { composer ->
         artTooling.registerExitHook(composer, START_RESTART_GROUP) { composer: Composer ->
             composer.recomposeScopeIdentity?.let { anchor ->
-                currentHandler?.incrementCount(anchor)
+                currentHandler?.incrementRecompositionCount(anchor)
             }
             composer
         }
@@ -118,7 +128,7 @@ private fun installHooks(artTooling: ArtTooling) {
         artTooling.registerEntryHook(composer, SKIP_TO_GROUP_END) { obj, _ ->
             val composer = obj as? Composer
             composer?.recomposeScopeIdentity?.let { anchor ->
-                currentHandler?.incrementSkip(anchor)
+                currentHandler?.incrementRecompositionSkip(anchor)
             }
         }
     }
