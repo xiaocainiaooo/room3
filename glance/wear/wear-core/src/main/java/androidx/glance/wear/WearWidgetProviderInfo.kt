e8f924d8206275f9628c4d13fb26d62a6f0ea35a
Lucas Muller Oliveira <lucasmo@google.com>
2025-09-23 22:59:01 +0000

    Add parsing for WearWidgetProviderInfoXml.
    Relnote: Add parsing for WearWidgetProviderInfoXml.
    Bug: 429979908
    Test: WearWidgetProviderInfoTest
    Change-Id: Ia950ad921bb9c93a40fabb927492e8892b6efe02

--- glance/wear/wear-core/src/main/java/androidx/glance/wear/WearWidgetProviderInfo.kt
+++ glance/wear/wear-core/src/main/java/androidx/glance/wear/WearWidgetProviderInfo.kt
@@ -17,9 +17,14 @@
 package androidx.glance.wear
 
 import android.content.ComponentName
+import android.content.Context
+import android.content.pm.PackageManager
 import androidx.annotation.DrawableRes
 import androidx.annotation.RestrictTo
 import androidx.annotation.RestrictTo.Scope.LIBRARY
+import androidx.glance.wear.WearWidgetProviderInfoXmlParser.parseWearWidgetProviderInfo
+import java.util.Objects
+import org.xmlpull.v1.XmlPullParserException
 
 /**
  * Describes the metadata for a Wear Widget provider.
@@ -43,14 +48,14 @@ import androidx.annotation.RestrictTo.Scope.LIBRARY
  *     label="@string/widget_label"
  *     description="@string/widget_description"
  *     icon="@drawable/widget_icon"
- *     preferredType="small" >
+ *     preferredType="@integer/glance_wear_container_type_small" >
  *
  *     <container
- *         type="small"
+ *         type="@integer/glance_wear_container_type_small"
  *         previewImage="@drawable/small_preview"/>
  *
  *     <container
- *         type="large"
+ *         type="@integer/glance_wear_container_type_large"
  *         previewImage="@drawable/large_preview"
  *         label="@string/large_container_label_override" />
  *
@@ -89,7 +94,44 @@ public constructor(
     public val configIntentAction: String? = null,
     public val minSchemaVersion: SchemaVersion? = null,
     public val maxSchemaVersion: SchemaVersion? = null,
-)
+) {
+    public companion object {
+        /** Name for the `meta-data` tag for the provider info. */
+        private const val META_DATA_WEAR_WIDGET_PROVIDER = "androidx.glance.wear.widget.provider"
+
+        /**
+         * Parses a [WearWidgetProviderInfo] from the metadata of a service.
+         *
+         * The metadata with name `androidx.glance.wear.widget.provider` should reference an XML
+         * resource with the provider info.
+         *
+         * @param context The [Context] to use for resolving resources and package manager.
+         * @param providerService The [ComponentName] of the widget provider service.
+         * @throws [PackageManager.NameNotFoundException] if the metadata is not found or the
+         *   resource is invalid.
+         * @throws [XmlPullParserException] if there is an error parsing the XML resource.
+         */
+        @Throws(XmlPullParserException::class)
+        public fun parseFromService(
+            context: Context,
+            providerService: ComponentName,
+        ): WearWidgetProviderInfo {
+            val serviceInfo =
+                context.packageManager.getServiceInfo(providerService, PackageManager.GET_META_DATA)
+            val xmlParser =
+                serviceInfo.loadXmlMetaData(context.packageManager, META_DATA_WEAR_WIDGET_PROVIDER)
+                    ?: throw PackageManager.NameNotFoundException(
+                        "Invalid meta-data name $META_DATA_WEAR_WIDGET_PROVIDER for service $providerService"
+                    )
+            return xmlParser.parseWearWidgetProviderInfo(
+                context.resources,
+                providerService,
+                defaultPreferredContainerType = ContainerType.Small,
+                defaultGroup = providerService.className,
+            )
+        }
+    }
+}
 
 /**
  * Describes a container supported by a widget provider.
@@ -101,9 +143,9 @@ public constructor(
  * For example:
  * ```xml
  * <container
- *     android:type="fullscreen"
- *     android:previewImage="@drawable/fullscreen_preview"
- *     android:label="@string/fullscreen_label" />
+ *     android:type="@integer/glance_wear_container_type_large"
+ *     android:previewImage="@drawable/large_preview"
+ *     android:label="@string/large_label" />
  * ```
  *
  * @property type The type of this widget container.
@@ -118,11 +160,24 @@ public constructor(
     @DrawableRes public val previewImage: Int,
     public val label: String? = null,
     public val description: String? = null,
-)
+) {
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (other !is ContainerInfo) return false
+        return type == other.type &&
+            previewImage == other.previewImage &&
+            label == other.label &&
+            description == other.description
+    }
+
+    override fun hashCode(): Int {
+        return Objects.hash(type, previewImage, label, description)
+    }
+}
 
 /** The container type of a widget. It defines the size and shape of the container. */
 @JvmInline
-public value class ContainerType private constructor(private val value: Int) {
+public value class ContainerType internal constructor(private val value: Int) {
 
     public companion object {
         /** Represents a fullscreen widget container, equivalent to a Wear Tile. */
