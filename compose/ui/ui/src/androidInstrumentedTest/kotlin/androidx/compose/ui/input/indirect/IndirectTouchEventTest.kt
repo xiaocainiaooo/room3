b6a2a261b0a0eb07c95cddc213bb3daaff990309
Himanshu Choudhary <hiichoudhary@google.com>
2025-09-25 08:07:46 +0000

    Migrated test cases in the :compose:ui:ui module.
    Migrated compose tests in the :compose:ui:ui module to use StandardTestDispatcher(). No other test changes are done in this commit.
    
    Bug: 254115946
    Test: Not Applicable
    Change-Id: I123fb91a9dcb0157665849d248dba7072fd1f49d

--- compose/ui/ui/src/androidInstrumentedTest/kotlin/androidx/compose/ui/input/indirect/IndirectTouchEventTest.kt
+++ compose/ui/ui/src/androidInstrumentedTest/kotlin/androidx/compose/ui/input/indirect/IndirectTouchEventTest.kt
@@ -45,6 +45,7 @@ import androidx.test.core.view.MotionEventBuilder
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.MediumTest
 import com.google.common.truth.Truth.assertThat
+import kotlinx.coroutines.test.StandardTestDispatcher
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
@@ -54,7 +55,7 @@ import org.junit.runner.RunWith
 @MediumTest
 @RunWith(AndroidJUnit4::class)
 class IndirectTouchEventTest {
-    @get:Rule val rule = createComposeRule()
+    @get:Rule val rule = createComposeRule(StandardTestDispatcher())
 
     private val initialFocus = FocusRequester()
     private lateinit var rootView: View
