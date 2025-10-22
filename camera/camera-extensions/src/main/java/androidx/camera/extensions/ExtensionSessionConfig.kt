94abd20655bba3abcae07ecb590960781a656197
CharcoalChen <charcoalchen@google.com>
2025-10-22 13:34:18 +0800

    Fix ExtensionSessionConfig API review comments
    1. For useCases in the builder ctor, move it to addUseCase setter function in the builder instead
    2. Constructed classes from a builder must have getters for all fields
    3. Change the Builder setter function from setEffects to addEffect to align with SessionConfig and UseCaseGroup Builders.
    
    Relnote: "Added addUseCase setter function for ExtensionSessionConfig.Builder and added the lacking getters for ExtensionSessionConfig."
    
    Bug: 453988621
    Test: ExtensionSessionConfigTest && ./gradlew :camera:camera-extensions:checkApi
    Change-Id: I71e077c85a32b7dd9a2279747fb509e8c8d0220b

--- camera/camera-extensions/src/main/java/androidx/camera/extensions/ExtensionSessionConfig.kt
+++ camera/camera-extensions/src/main/java/androidx/camera/extensions/ExtensionSessionConfig.kt
@@ -38,6 +38,12 @@ import androidx.camera.core.ViewPort
  * and common properties like the field-of-view defined by [ViewPort]. Note that [ImageAnalysis] is
  * not supported in extension sessions.
  *
+ * **Constraints:**
+ * - When used for binding to a camera via
+ *   `androidx.camera.lifecycle.ProcessCameraProvider.bindToLifecycle` or
+ *   `androidx.camera.lifecycle.LifecycleCameraProvider.bindToLifecycle`, the list of [UseCase]
+ *   provided to the constructor or added via [Builder.addUseCase] cannot be empty.
+ *
  * Apps can use [CameraProvider.getCameraInfo] with an [ExtensionSessionConfig] to obtain the
  * [CameraInfo] of the camera which can support the given [ExtensionSessionConfig].
  *
@@ -54,11 +60,13 @@ import androidx.camera.core.ViewPort
  *         val imageCapture = ImageCapture.Builder().build()
  *         val preview = Preview.Builder().build()
  *
- *         val config = ExtensionSessionConfig(
- *             mode = ExtensionMode.NIGHT,
- *             extensionsManager = extensionsManager,
- *             useCases = listOf(preview, imageCapture)
+ *         val config = ExtensionSessionConfig.Builder(
+ *             ExtensionMode.NIGHT,
+ *             extensionsManager
  *         )
+ *         .addUseCase(preview)
+ *         .addUseCase(imageCapture)
+ *         .build()
  *
  *         // Now it's safe to bind the configuration
  *         cameraProvider.bindToLifecycle(lifecycleOwner, cameraSelector, config)
@@ -70,23 +78,22 @@ import androidx.camera.core.ViewPort
  * }
  * ```
  *
- * @param mode The extension mode.
+ * @param mode The extension mode. See [ExtensionMode] for the list of available modes.
  * @param extensionsManager The [ExtensionsManager] instance.
- * @param useCases The list of [UseCase] to be attached to the camera and receive camera data. When
- *   used for binding to a camera, this list cannot be empty. When used for query purposes, this
- *   list can be empty.
+ * @param useCases The list of [UseCase] to be attached to the camera and receive camera data.
  * @param viewPort The [ViewPort] to be applied on the camera session. If not set, the default is no
  *   viewport.
  * @param effects The list of [CameraEffect] to be applied on the camera session. If not set, the
  *   default is no effects.
+ * @throws IllegalArgumentException if the given mode is not a valid extension mode.
  * @see androidx.camera.lifecycle.ProcessCameraProvider.bindToLifecycle
  * @see ExtensionsManager.getInstanceAsync
  */
 public class ExtensionSessionConfig
 @JvmOverloads
 constructor(
-    @ExtensionMode.Mode mode: Int,
-    extensionsManager: ExtensionsManager,
+    @param:ExtensionMode.Mode @get:ExtensionMode.Mode public val mode: Int,
+    public val extensionsManager: ExtensionsManager,
     useCases: List<UseCase> = emptyList(),
     viewPort: ViewPort? = null,
     effects: List<CameraEffect> = emptyList(),
@@ -102,6 +109,14 @@ constructor(
     override val cameraFilter: CameraFilter?
         get() = _cameraFilter
 
+    /**
+     * Creates an [ExtensionSessionConfig] with a variable number of [UseCase] instances.
+     *
+     * @param mode The extension mode. See [ExtensionMode] for the list of available modes.
+     * @param extensionsManager The [ExtensionsManager] instance.
+     * @param useCases The [UseCase] instances to be attached to the camera and receive camera data.
+     * @throws IllegalArgumentException if the given mode is not a valid extension mode.
+     */
     public constructor(
         @ExtensionMode.Mode mode: Int,
         extensionsManager: ExtensionsManager,
@@ -111,26 +126,23 @@ constructor(
     /**
      * Builder for [ExtensionSessionConfig].
      *
-     * @param mode The extension mode.
+     * @param mode The extension mode. See [ExtensionMode] for the list of available modes.
      * @param extensionsManager The [ExtensionsManager] instance.
-     * @param useCases The list of [UseCase] to be attached to the camera and receive camera data.
-     *   When used for binding to a camera, this list cannot be empty. When used for query purposes,
-     *   this list can be empty.
      * @see ExtensionsManager.getInstanceAsync
      */
     public class Builder(
         @param:ExtensionMode.Mode private val mode: Int,
         private val extensionsManager: ExtensionsManager,
-        private val useCases: List<UseCase>,
     ) {
+        private val useCases: MutableList<UseCase> = mutableListOf()
         private var viewPort: ViewPort? = null
-        private var effects: List<CameraEffect> = emptyList()
+        private var effects: MutableList<CameraEffect> = mutableListOf()
 
-        public constructor(
-            @ExtensionMode.Mode mode: Int,
-            extensionsManager: ExtensionsManager,
-            vararg useCases: UseCase,
-        ) : this(mode, extensionsManager, useCases.toList())
+        /** Adds a [UseCase] to the session. */
+        public fun addUseCase(useCase: UseCase): Builder {
+            useCases.add(useCase)
+            return this
+        }
 
         /** Sets the [ViewPort] for the session. */
         public fun setViewPort(viewPort: ViewPort): Builder {
@@ -138,20 +150,24 @@ constructor(
             return this
         }
 
-        /** Sets a list of [CameraEffect]s for the session. */
-        public fun setEffects(effects: List<CameraEffect>): Builder {
-            this.effects = effects
+        /** Adds a [CameraEffect] for the session. */
+        public fun addEffect(effect: CameraEffect): Builder {
+            effects.add(effect)
             return this
         }
 
-        /** Builds an [ExtensionSessionConfig] from the current configuration. */
+        /**
+         * Builds an [ExtensionSessionConfig] from the current configuration.
+         *
+         * @throws IllegalArgumentException if the given mode is not a valid extension mode.
+         */
         public fun build(): ExtensionSessionConfig {
             return ExtensionSessionConfig(
                 mode = mode,
                 extensionsManager = extensionsManager,
-                useCases = useCases,
+                useCases = useCases.toList(),
                 viewPort = viewPort,
-                effects = effects,
+                effects = effects.toList(),
             )
         }
     }
