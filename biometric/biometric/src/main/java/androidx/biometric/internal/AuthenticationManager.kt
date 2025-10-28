b33dac19cca6b622bf97a4642948cab6e0838893
Hao Dong <spdonghao@google.com>
2025-10-28 11:13:17 -0700

    Revert "[6/n] Support FingerprintManager API"
    This reverts commit 780b170d16b4aff5a7f58f002818c17d10b14566.
    
    Reason for revert: Build Breakage
    
    Change-Id: I0de3a51f7d621eadec531a1302cf6d12cf68b1bd

--- biometric/biometric/src/main/java/androidx/biometric/internal/AuthenticationManager.kt
+++ biometric/biometric/src/main/java/androidx/biometric/internal/AuthenticationManager.kt
@@ -209,6 +209,7 @@ internal class AuthenticationManager(
         }
         viewModel.canceledFrom = canceledFrom
         viewModel.cancellationSignalProvider.cancel()
+        destroy()
     }
 
     /** Removes any associated UI from the client activity/fragment. */
