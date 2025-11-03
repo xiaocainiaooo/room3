13d5676608ea76eb2c8c5cbb2475ed1b44bccae6
Marcello Galhardo <mgalhardo@google.com>
2025-11-03 14:53:14 +0000

    Update NavDisplay benchmarks
    Bug: N/A
    Test: NavDisplayBenchmark
    Change-Id: I64467ced4ecae157170041ae5f55b12fdb524bd0

--- navigation3/navigation3-benchmark/src/androidTest/java/androidx/navigation3/benchmark/NavDisplayBenchmark.kt
+++ navigation3/navigation3-benchmark/src/androidTest/java/androidx/navigation3/benchmark/NavDisplayBenchmark.kt
@@ -5,7 +5,7 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -16,35 +16,55 @@
 
 package androidx.navigation3.benchmark
 
-import androidx.compose.foundation.text.BasicText
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateListOf
-import androidx.compose.runtime.remember
 import androidx.compose.testutils.LayeredComposeTestCase
+import androidx.compose.testutils.ToggleableTestCase
 import androidx.compose.testutils.benchmark.ComposeBenchmarkRule
-import androidx.compose.testutils.benchmark.benchmarkToFirstPixel
-import androidx.navigation3.runtime.entryProvider
+import androidx.compose.testutils.benchmark.benchmarkFirstCompose
+import androidx.compose.testutils.benchmark.toggleStateBenchmarkRecompose
+import androidx.compose.ui.Modifier
+import androidx.navigation3.runtime.NavEntry
 import androidx.navigation3.ui.NavDisplay
 import kotlin.test.Test
 import org.junit.Rule
 
 class NavDisplayBenchmark {
+
     @get:Rule val benchmarkRule = ComposeBenchmarkRule()
 
-    @Test
-    fun simpleNavDisplay() {
-        benchmarkRule.benchmarkToFirstPixel { NavigationTestCase() }
+    private val caseFactory = {
+        object : LayeredComposeTestCase(), ToggleableTestCase {
+            var keyCount = 0
+            val backStack = mutableStateListOf(keyCount++)
+
+            @Composable
+            override fun MeasuredContent() {
+                NavDisplay(
+                    backStack,
+                    entryProvider = { key -> NavEntry(key) { Box(Modifier.fillMaxSize()) } },
+                )
+            }
+
+            override fun toggleState() {
+                backStack.add(keyCount++)
+            }
+        }
     }
-}
 
-private class NavigationTestCase : LayeredComposeTestCase() {
-    @Composable
-    override fun MeasuredContent() {
-        val backStack = remember { mutableStateListOf<Any>("Test1") }
+    /** Measure the time taken to compose the NavDisplay from scratch. */
+    @Test
+    fun first_compose() {
+        benchmarkRule.benchmarkFirstCompose(caseFactory)
+    }
 
-        NavDisplay(
-            backStack,
-            entryProvider = entryProvider { entry(key = "Test1") { BasicText("Text") } },
-        )
+    /**
+     * Measure the time taken to recompose the NavDisplay when the back stack state gets toggled.
+     */
+    @Test
+    fun toggleState_recompose() {
+        benchmarkRule.toggleStateBenchmarkRecompose(caseFactory, assertOneRecomposition = false)
     }
 }
