af86686c6deaf304baff5fe8216713585c3e104c
corycook <corycook@google.com>
2025-11-04 15:35:11 -0800

    Check if entity is disposed.
    Add the ability to check if an entity is disposed before executing actions on it. This modifies the ActionQueue implementation to allow an "isValid" method that will be checked before performing actions on the value to ensure that the value is in a valid state prior to executing actions in the queue.
    
    This also changes the signature of the "executeWhenAvailable" method to take the value as a parameter instead of as the scope for the function. This should make it more clear which actions in the block are applied to the value object versus a local function.
    
    Bug: 440426914
    Test: Added unit tests and unit tests pass.
    Change-Id: I0c1f3b4cc05b884197f0dac07af4390cb2429224

--- xr/compose/compose/src/main/kotlin/androidx/xr/compose/subspace/ActionQueue.kt
+++ xr/compose/compose/src/main/kotlin/androidx/xr/compose/subspace/ActionQueue.kt
@@ -16,25 +16,35 @@
 
 package androidx.xr.compose.subspace
 
-import androidx.compose.ui.util.fastForEach
-
 /**
  * A helper utility that can queue actions to be executed when the target value is available.
  *
  * If the target is available the actions will be executed immediately in the same thread; however,
- * if the target is null the actions will be queued to be executed later once [value] is set.
+ * if the target is null the actions will be queued to be executed later once [value] is set. If
+ * [value] becomes invalidated, execution will stop and actions will be queued until a new, valid
+ * [value] is set.
  *
  * This class is not thread-safe and should only be used from a single thread.
+ *
+ * @param initialValue the initial value of the queue. If a valid value is initially provided then
+ *   calls to [executeWhenAvailable] will trigger immediately in the same thread.
+ * @property isValid a predicate function that indicates whether the current value is valid. This
+ *   will be checked before and after executing any action on the value. If the value is invalid,
+ *   execution of the queue will be paused and the value will be set to null. Execution will not
+ *   proceed until a new, valid value is set.
  */
-internal class ActionQueue<T>(initialValue: T? = null) {
-    private val queue = mutableListOf<T.() -> Any?>()
+internal class ActionQueue<T>(
+    initialValue: T? = null,
+    private val isValid: (T) -> Boolean = { true },
+) {
+    private val queue = ArrayDeque<(T) -> Any?>()
 
     var value: T? = initialValue
-        set(value) {
-            field = value
-            if (value != null) {
-                queue.fastForEach { action -> value.action() }
-                clear()
+        get() = field?.takeIf(isValid)
+        set(nextValue) {
+            field = nextValue
+            while (value != null && !queue.isEmpty()) {
+                field?.let(queue.removeFirst())
             }
         }
 
@@ -43,12 +53,12 @@ internal class ActionQueue<T>(initialValue: T? = null) {
      * null); otherwise, enqueue the action to be run on the next target when the target is set with
      * [value].
      */
-    fun <U> executeWhenAvailable(action: T.() -> U): U? {
-        return value?.run(action)
-            ?: run {
-                queue.add(action)
-                null
-            }
+    fun <U> executeWhenAvailable(action: (T) -> U): U? {
+        if (value == null) {
+            queue.add(action)
+            return null
+        }
+        return value?.let(action)
     }
 
     fun clear() {
--- xr/compose/compose/src/test/kotlin/androidx/xr/compose/subspace/ActionQueueTest.kt
+++ xr/compose/compose/src/test/kotlin/androidx/xr/compose/subspace/ActionQueueTest.kt
@@ -37,13 +37,30 @@ class ActionQueueTest {
     }
 
     @Test
+    fun constructor_withInvalidInitialValue_valueIsNull() {
+        val queue = ActionQueue(initialValue = "invalid", isValid = { it != "invalid" })
+        assertNull(queue.value)
+    }
+
+    @Test
+    fun clear_withQueuedActions_removesAllActions() {
+        val queue = ActionQueue<StringBuilder>()
+        queue.executeWhenAvailable { it.append("test") }
+
+        queue.clear()
+        queue.value = StringBuilder()
+
+        assertThat(queue.value.toString()).isEmpty()
+    }
+
+    @Test
     fun executeWhenAvailable_valueIsNull_actionIsQueuedAndReturnsNull() {
         val queue = ActionQueue<StringBuilder>()
         var executed = false
         val result =
-            queue.executeWhenAvailable {
+            queue.executeWhenAvailable { builder ->
                 executed = true
-                append("test")
+                builder.append("test")
             }
 
         assertThat(executed).isFalse()
@@ -51,14 +68,54 @@ class ActionQueueTest {
     }
 
     @Test
+    fun executeWhenAvailable_valueIsInvalidatedByAction_queueIsStopped() {
+        val queue =
+            ActionQueue(
+                initialValue = StringBuilder("in"),
+                isValid = { it.toString() != "invalid" },
+            )
+        val executions = mutableListOf<Int>()
+
+        queue.executeWhenAvailable { executions += 1 }
+        queue.executeWhenAvailable { builder ->
+            executions += 2
+            builder.append("valid")
+        }
+        queue.executeWhenAvailable { executions += 3 }
+
+        assertNull(queue.value)
+        assertThat(executions).containsExactly(1, 2).inOrder()
+    }
+
+    @Test
+    fun executeWhenAvailable_valueIsInvalidatedBetweenActions_queueIsStopped() {
+        val queue =
+            ActionQueue(
+                initialValue = StringBuilder("in"),
+                isValid = { it.toString() != "invalid" },
+            )
+        val executions = mutableListOf<Int>()
+
+        queue.executeWhenAvailable { executions += 1 }
+        queue.executeWhenAvailable { executions += 2 }
+
+        queue.value?.append("valid")
+
+        queue.executeWhenAvailable { executions += 3 }
+
+        assertNull(queue.value)
+        assertThat(executions).containsExactly(1, 2).inOrder()
+    }
+
+    @Test
     fun executeWhenAvailable_valueIsAvailable_actionIsExecutedImmediately() {
         val queue = ActionQueue(initialValue = StringBuilder("initial"))
         var executed = false
         val result =
-            queue.executeWhenAvailable {
+            queue.executeWhenAvailable { builder ->
                 executed = true
-                append("-test")
-                toString()
+                builder.append("-test")
+                builder.toString()
             }
 
         assertThat(executed).isTrue()
@@ -67,17 +124,61 @@ class ActionQueueTest {
     }
 
     @Test
+    fun executeWhenAvailable_valueIsNull_actionIsExecutedImmediately() {
+        val queue = ActionQueue(initialValue = StringBuilder("initial"))
+        var executed = false
+        val result =
+            queue.executeWhenAvailable { builder ->
+                executed = true
+                builder.append("-test")
+                builder.toString()
+            }
+
+        assertThat(executed).isTrue()
+        assertThat(result).isEqualTo("initial-test")
+        assertThat(queue.value.toString()).isEqualTo("initial-test")
+    }
+
+    @Test
+    fun executeWhenAvailable_valueIsAvailable_actionIsNotQueued() {
+        val queue = ActionQueue(initialValue = StringBuilder("initial"))
+        var executions = 0
+
+        // This action should execute immediately and not be queued.
+        queue.executeWhenAvailable {
+            executions++
+            it.append("-executed")
+            // return null to ensure that the return value is not used to determine eligibility for
+            // queueing this action
+            null
+        }
+
+        // Verify immediate execution.
+        assertThat(executions).isEqualTo(1)
+        assertThat(queue.value.toString()).isEqualTo("initial-executed")
+
+        // Set the value to null and then provide a new value. If the original action was
+        // queued, it would execute again here.
+        queue.value = null
+        queue.value = StringBuilder("new")
+
+        // Verify the action was not executed again.
+        assertThat(executions).isEqualTo(1)
+        assertThat(queue.value.toString()).isEqualTo("new")
+    }
+
+    @Test
     fun setValue_fromNullToNonNull_executesQueuedActions() {
         val queue = ActionQueue<StringBuilder>()
         val executionOrder = mutableListOf<Int>()
 
-        queue.executeWhenAvailable {
+        queue.executeWhenAvailable { builder ->
             executionOrder.add(1)
-            append("first-")
+            builder.append("first-")
         }
-        queue.executeWhenAvailable {
+        queue.executeWhenAvailable { builder ->
             executionOrder.add(2)
-            append("second")
+            builder.append("second")
         }
 
         assertThat(executionOrder).isEmpty()
@@ -86,17 +187,6 @@ class ActionQueueTest {
 
         assertThat(queue.value.toString()).isEqualTo("first-second")
         assertThat(executionOrder).containsExactly(1, 2).inOrder()
-    }
-
-    @Test
-    fun clear_withQueuedActions_removesAllActions() {
-        val queue = ActionQueue<StringBuilder>()
-        queue.executeWhenAvailable { append("test") }
-
-        queue.clear()
-        queue.value = StringBuilder()
-
-        assertThat(queue.value.toString()).isEmpty()
     }
 
     @Test
@@ -117,5 +207,71 @@ class ActionQueueTest {
         queue.value = null
 
         assertThat(executed).isFalse()
+    }
+
+    @Test
+    fun setValue_withInvalidValue_doesNotExecuteActions() {
+        val queue = ActionQueue<StringBuilder>(isValid = { it.toString() != "invalid" })
+        var executed = 0
+
+        queue.executeWhenAvailable { executed++ }
+
+        queue.value = StringBuilder("invalid")
+
+        assertNull(queue.value)
+        assertThat(executed).isEqualTo(0)
+    }
+
+    @Test
+    fun setValue_withInvalidatedValue_doesNotProceedExecutions() {
+        val queue = ActionQueue<StringBuilder>(isValid = { it.toString() != "invalid" })
+        val executions = mutableListOf<Int>()
+
+        queue.executeWhenAvailable { executions += 1 }
+        queue.executeWhenAvailable { builder ->
+            executions += 2
+            builder.append("valid")
+        }
+        queue.executeWhenAvailable { executions += 3 }
+
+        queue.value = StringBuilder("in")
+        assertNull(queue.value)
+        assertThat(executions).containsExactly(1, 2).inOrder()
+    }
+
+    @Test
+    fun setValue_inQueuedAction_doesNotRecurse() {
+        val queue = ActionQueue<StringBuilder>()
+        val executions = mutableListOf<Int>()
+
+        queue.executeWhenAvailable {
+            executions.add(1)
+            // This setter should not trigger re-execution of the action queue.
+            queue.value = StringBuilder("new value")
+            executions.add(2)
+        }
+
+        // This triggers the queued action.
+        queue.value = StringBuilder("initial")
+
+        assertThat(queue.value.toString()).isEqualTo("new value")
+        assertThat(executions).containsExactly(1, 2).inOrder()
+    }
+
+    @Test
+    fun setValue_inImmediatelyExecutedAction_doesNotRecurse() {
+        val queue = ActionQueue(initialValue = StringBuilder("initial"))
+        val executions = mutableListOf<Int>()
+
+        // This action will be executed immediately because the queue already has a value.
+        queue.executeWhenAvailable {
+            executions.add(1)
+            // This setter should not trigger re-execution of the action queue.
+            queue.value = StringBuilder("new value")
+            executions.add(2)
+        }
+
+        assertThat(queue.value.toString()).isEqualTo("new value")
+        assertThat(executions).containsExactly(1, 2).inOrder()
     }
 }
