ab15a953f871a8d0d0f53a6631ba90e06a2c98ba
Andrey Kulikov <andreykulikov@google.com>
2025-11-05 16:56:17 +0000

    Recalculate the rects for the subtree when the layer transformation is removed
    When there was a transformation like scale, and it was removed, the whole subtree should be recalculated as all the bounds were affected by the now removed scale. Previous logic was just resetting hasPositionalLayerTransformationsInOffsetFromRoot flag without recalculating the bounds.
    It is the bug which made the tests in g3 fail, with it fixed we can re-enable isRectManagerOffsetUsageFromLayoutCoordinatesEnabled.
    
    Bug: 456606764
    Bug: 456607982
    Test: new tests in OnGlobalRectChangedTest
    Change-Id: I8963897a8258f715b3607c7cabf8f53b381b20de

--- compose/ui/ui/src/commonMain/kotlin/androidx/compose/ui/ComposeUiFlags.kt
+++ compose/ui/ui/src/commonMain/kotlin/androidx/compose/ui/ComposeUiFlags.kt
@@ -204,7 +204,7 @@ object ComposeUiFlags {
      */
     @Suppress("MutableBareField")
     @JvmField
-    var isRectManagerOffsetUsageFromLayoutCoordinatesEnabled: Boolean = false
+    var isRectManagerOffsetUsageFromLayoutCoordinatesEnabled: Boolean = true
 
     /**
      * Enables automatic pinning of interop AndroidViews when they are focused inside lazy
