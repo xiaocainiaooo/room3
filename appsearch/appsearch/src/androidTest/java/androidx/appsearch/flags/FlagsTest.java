ac28e293a96b76f6f6f0dae4ca67589aaca35495
Yen-Chieh Sung <sungyc@google.com>
2025-11-11 12:07:48 -0800

    Switch to new delete propagation flag
    Bug: 384947619
    Test: presubmit
    Relnote: Use new delete propagation flag
    Change-Id: If21ae4f3bbe0b2eb2376d2e81a102575c2d9d196

--- appsearch/appsearch/src/androidTest/java/androidx/appsearch/flags/FlagsTest.java
+++ appsearch/appsearch/src/androidTest/java/androidx/appsearch/flags/FlagsTest.java
@@ -188,12 +188,6 @@ public class FlagsTest {
     }
 
     @Test
-    public void testFlagValue_enableDeletePropagationType() {
-        assertThat(Flags.FLAG_ENABLE_DELETE_PROPAGATION_TYPE)
-                .isEqualTo("com.android.appsearch.flags.enable_delete_propagation_type");
-    }
-
-    @Test
     public void testFlagValue_enableAppSearchManageBlobFiles() {
         assertThat(Flags.FLAG_ENABLE_APP_SEARCH_MANAGE_BLOB_FILES)
                 .isEqualTo("com.android.appsearch.flags.enable_app_search_manage_blob_files");
@@ -358,5 +352,11 @@ public class FlagsTest {
     public void testFlagValue_enableRepeatedFieldJoins() {
         assertThat(Flags.FLAG_ENABLE_REPEATED_FIELD_JOINS).isEqualTo(
                 "com.android.appsearch.flags.enable_repeated_field_joins");
+    }
+
+    @Test
+    public void testFlagValue_enableDeletePropagationRw() {
+        assertThat(Flags.FLAG_ENABLE_DELETE_PROPAGATION_RW)
+                .isEqualTo("com.android.appsearch.flags.enable_delete_propagation_rw");
     }
 }
