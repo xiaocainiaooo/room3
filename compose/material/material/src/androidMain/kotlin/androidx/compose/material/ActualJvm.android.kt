69b013df9e819e4e7e69981e6eac5fbb8176960a
Omar Ismail <omarismail@google.com>
2025-12-03 09:21:21 +0000

    Add applyAndroidXDefaultHierarchyTemplate to compose material
    BUG: 428757597
    Change-Id: I010a00c8cdb1e3ddccc7e10e75474f06c2449250

--- compose/material/material/src/androidMain/kotlin/androidx/compose/material/ActualJvm.android.kt
+++ compose/material/material/src/androidMain/kotlin/androidx/compose/material/ActualJvm.android.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2023 The Android Open Source Project
+ * Copyright 2025 The Android Open Source Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,5 +16,6 @@
 
 package androidx.compose.material
 
-internal actual typealias InternalAtomicReference<V> =
-    java.util.concurrent.atomic.AtomicReference<V>
+import java.util.concurrent.atomic.AtomicReference
+
+internal actual typealias InternalAtomicReference<V> = AtomicReference<V>
