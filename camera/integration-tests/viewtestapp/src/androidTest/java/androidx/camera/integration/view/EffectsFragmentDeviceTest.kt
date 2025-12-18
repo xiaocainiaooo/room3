9191e5d32e7148033fae5401036b086f9db0b3be
Tahsin Masrur <tahsinm@google.com>
2025-12-18 09:03:07 +0000

    SurfaceProcessor: Ignore frames arriving after surface release
    On some devices, a few frames can still be sent for processing even
    after the camera session has been closed and the output Surface has been
    released. This can lead to `onFrameAvailable` being called on a released
    `SurfaceTexture`, causing errors.
    
    This CL addresses this race condition by:
    1.  Updating `ToneMappingSurfaceProcessor` to track released
        `SurfaceTexture` instances and ignore any subsequent frames.
    2.  Adding documentation and code sample to the `SurfaceProcessor`
        interface to guide developers in handling this scenario correctly.
    
    This fix allows for the re-enabling of the `EffectsFragmentDeviceTest`,
    which was previously disabled due to this issue on certain devices.
    
    Bug: 465855844
    Test: EffectsFragmentDeviceTest
    Change-Id: I964e8403cfdb6019f09316f515f966126e27f2d6

--- camera/integration-tests/viewtestapp/src/androidTest/java/androidx/camera/integration/view/EffectsFragmentDeviceTest.kt
+++ camera/integration-tests/viewtestapp/src/androidTest/java/androidx/camera/integration/view/EffectsFragmentDeviceTest.kt
@@ -17,7 +17,6 @@
 package androidx.camera.integration.view
 
 import android.net.Uri
-import android.os.Build
 import android.util.Log
 import androidx.camera.camera2.Camera2Config
 import androidx.camera.camera2.pipe.integration.CameraPipeConfig
@@ -41,7 +40,6 @@ import com.google.common.truth.Truth.assertThat
 import java.util.concurrent.Semaphore
 import java.util.concurrent.TimeUnit
 import org.junit.After
-import org.junit.Assume.assumeFalse
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
@@ -128,10 +126,6 @@ class EffectsFragmentDeviceTest(
 
     @Test
     fun shareToImageCapture_canTakePicture() {
-        assumeFalse(
-            "Test fails on cuttlefish b/465855844",
-            Build.MODEL.contains("Cuttlefish", ignoreCase = true),
-        )
         // Act.
         instrumentation.runOnMainSync { fragment.surfaceEffectForImageCapture.isChecked = true }
         // Assert.
