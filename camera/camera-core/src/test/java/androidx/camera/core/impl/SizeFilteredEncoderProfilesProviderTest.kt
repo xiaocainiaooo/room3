0bb5416cdda91f2c52a4397a58718a792a088379
leo huang <leohuang@google.com>
2025-12-30 17:33:28 +0800

    Refactor: Move SizeFilteredEncoderProfilesProvider to core artifact
     This class can be reused by the upcoming VideoCapabilities implementation in the video artifact.
    
    Bug: 471353211
    Test: ./gradlew camera:camera-video:testRelease
    Change-Id: Ie6fb999aa9855741dffe1a99d004a0ad77a9d562

--- camera/camera-core/src/test/java/androidx/camera/core/impl/SizeFilteredEncoderProfilesProviderTest.kt
+++ camera/camera-core/src/test/java/androidx/camera/core/impl/SizeFilteredEncoderProfilesProviderTest.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2024 The Android Open Source Project
+ * Copyright 2025 The Android Open Source Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package androidx.camera.camera2.pipe.integration.compat.workaround
+package androidx.camera.core.impl
 
 import android.media.CamcorderProfile.QUALITY_1080P
 import android.media.CamcorderProfile.QUALITY_2160P
