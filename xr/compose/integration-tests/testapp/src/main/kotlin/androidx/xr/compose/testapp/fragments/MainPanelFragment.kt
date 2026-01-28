07b37b01c46c2b3b8e6eb87db97cefc70972026a
Yashwanth Gajji <yashwanthgajji@google.com>
2026-01-28 14:32:05 -0800

    MainPanel compatibility with Fragments
    Added a testcase to use MainPanel inside a fragment to try it's compatibility.
    
    Bug: 467764398
    Test: Tested in headset and emulator
    Change-Id: Ice9911a729728b607d21a38d0ff0ac44a6512fed

--- xr/compose/integration-tests/testapp/src/main/kotlin/androidx/xr/compose/testapp/fragments/MainPanelFragment.kt
+++ xr/compose/integration-tests/testapp/src/main/kotlin/androidx/xr/compose/testapp/fragments/MainPanelFragment.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2025 The Android Open Source Project
+ * Copyright 2026 The Android Open Source Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -20,46 +20,60 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Surface
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.platform.ViewCompositionStrategy
 import androidx.compose.ui.unit.dp
 import androidx.fragment.app.Fragment
+import androidx.xr.compose.spatial.ContentEdge
+import androidx.xr.compose.spatial.Orbiter
+import androidx.xr.compose.spatial.OrbiterOffsetType
 import androidx.xr.compose.spatial.Subspace
-import androidx.xr.compose.subspace.SpatialPanel
+import androidx.xr.compose.subspace.SpatialMainPanel
 import androidx.xr.compose.subspace.layout.SubspaceModifier
 import androidx.xr.compose.subspace.layout.height
 import androidx.xr.compose.subspace.layout.offset
 import androidx.xr.compose.subspace.layout.width
 
-class SimpleTextFragment : Fragment() {
+class MainPanelFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?,
     ): View {
+        val xOffset = arguments?.getFloat("x_offset") ?: 0f
+        val displayText = arguments?.getString("text") ?: "MainPanelFragment"
         return ComposeView(requireContext()).apply {
             setViewCompositionStrategy(ViewCompositionStrategy.DisposeOnDetachedFromWindow)
             setContent {
+                MainPanelContent(displayText)
                 Subspace {
-                    val xOffset = arguments?.getFloat("x_offset") ?: 0f
-                    val displayText = arguments?.getString("text") ?: "Simple Text Fragment"
-                    SpatialPanel(
+                    SpatialMainPanel(
                         modifier =
-                            SubspaceModifier.width(400.dp).height(200.dp).offset(x = xOffset.dp)
+                            SubspaceModifier.width(300.dp).height(200.dp).offset(x = xOffset.dp)
+                    )
+                    Orbiter(
+                        position = ContentEdge.Top,
+                        offsetType = OrbiterOffsetType.InnerEdge,
+                        offset = 10.dp,
                     ) {
-                        Surface(color = Color.LightGray, modifier = Modifier.fillMaxSize()) {
-                            Column(modifier = Modifier.padding(16.dp)) { Text(displayText) }
-                        }
+                        Button(onClick = { parentFragmentManager.popBackStack() }) { Text("Back") }
                     }
                 }
             }
+        }
+    }
+
+    @Composable
+    private fun MainPanelContent(displayText: String) {
+        Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+            Text(text = displayText)
         }
     }
 }
