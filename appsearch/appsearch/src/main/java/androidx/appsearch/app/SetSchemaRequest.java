61504b1f7e244be1631bf8e53f6637a6ae188de8
Terry Wang <tytytyww@google.com>
2026-02-11 15:14:55 -0800

    Change setSchemaTypeWipeoutAccountPropertyPaths to take set.
    Bug: 470398298
    Test: presubmit
    Relnote: Change setSchemaTypeWipeoutAccountPropertyPaths to take set.
    Change-Id: If442cb14a8acbfc181172680d2ba3924da292dd1

--- appsearch/appsearch/src/main/java/androidx/appsearch/app/SetSchemaRequest.java
+++ appsearch/appsearch/src/main/java/androidx/appsearch/app/SetSchemaRequest.java
@@ -946,7 +946,7 @@ public final class SetSchemaRequest {
          * recognized.
          *
          * @param schemaType The name of the schema type being configured (e.g., "Email").
-         * @param accountPropertyPaths A collection of property paths (e.g., "sender.account") point
+         * @param accountPropertyPaths A Set of property paths (e.g., "sender.account") point
          *                             to the field containing the account identifier.
          * @param autoWipeout If {@code true}, enables automatic account wipeout for the given
          *                    paths. If {@code false}, disables it.
@@ -965,7 +965,7 @@ public final class SetSchemaRequest {
         @FlaggedApi(Flags.FLAG_ENABLE_SCHEMAS_WIPEOUT_ACCOUNT_PROPERTY_PATHS)
         @ExperimentalAppSearchApi
         public @NonNull Builder setSchemaTypeWipeoutAccountPropertyPaths(
-                @NonNull String schemaType, @NonNull Collection<PropertyPath> accountPropertyPaths,
+                @NonNull String schemaType, @NonNull Set<PropertyPath> accountPropertyPaths,
                 boolean autoWipeout) {
             Preconditions.checkNotNull(schemaType);
             Preconditions.checkNotNull(accountPropertyPaths);
@@ -1012,7 +1012,7 @@ public final class SetSchemaRequest {
          *
          * @param documentClass the {@link androidx.appsearch.annotation.Document} annotated class
          *                      of the schema type being configured (e.g., "Email").
-         * @param accountPropertyPaths A collection of property paths (e.g., "sender.account") point
+         * @param accountPropertyPaths A Set of property paths (e.g., "sender.account") point
          *                             to the field containing the account identifier.
          * @param autoWipeOut If {@code true}, enables automatic account wipeout for the given
          *                    paths. If {@code false}, disables it.
@@ -1028,7 +1028,7 @@ public final class SetSchemaRequest {
         @ExperimentalAppSearchApi
         public @NonNull Builder setDocumentClassWipeoutAccountPropertyPaths(
                 @NonNull Class<?> documentClass,
-                @NonNull Collection<PropertyPath> accountPropertyPaths,
+                @NonNull Set<PropertyPath> accountPropertyPaths,
                 boolean autoWipeOut) throws AppSearchException {
             Preconditions.checkNotNull(documentClass);
             resetIfBuilt();
