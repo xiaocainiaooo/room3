3b8d4cface4f6d5f63c50d7246d0ddd382670477
yingmak <yingmak@google.com>
2026-02-13 14:01:39 +0000

    Adds RemoteCircularProgressIndicator
    Test: Screenshot Tests
    Bug: 477323007
    Change-Id: I7634bf4bc645cfff833b1e4eac825dbcef49ff76

---
+++ wear/compose/remote/remote-material3/samples/src/main/java/androidx/wear/compose/remote/material3/samples/RemoteCircularProgressIndicatorSample.kt
@@ -0,0 +1,57 @@
+/*
+ * Copyright 2025 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package androidx.wear.compose.remote.material3.samples
+
+import androidx.annotation.Sampled
+import androidx.compose.remote.creation.compose.layout.RemoteAlignment
+import androidx.compose.remote.creation.compose.layout.RemoteArrangement
+import androidx.compose.remote.creation.compose.layout.RemoteBox
+import androidx.compose.remote.creation.compose.layout.RemoteComposable
+import androidx.compose.remote.creation.compose.modifier.RemoteModifier
+import androidx.compose.remote.creation.compose.modifier.fillMaxSize
+import androidx.compose.remote.creation.compose.state.rf
+import androidx.compose.remote.tooling.preview.RemotePreview
+import androidx.compose.runtime.Composable
+import androidx.wear.compose.remote.material3.RemoteCircularProgressIndicator
+import androidx.wear.compose.ui.tooling.preview.WearPreviewDevices
+
+@Sampled
+@RemoteComposable
+@Composable
+public fun RemoteCircularProgressIndicatorSample(modifier: RemoteModifier = RemoteModifier) {
+    RemoteCircularProgressIndicator(modifier = modifier, progress = 0.75f.rf)
+}
+
+@WearPreviewDevices
+@Composable
+fun RemoteCircularProgressIndicatorSamplePreview() = RemotePreview {
+    Container { RemoteCircularProgressIndicatorSample() }
+}
+
+@Composable
+@RemoteComposable
+private fun Container(
+    modifier: RemoteModifier = RemoteModifier.fillMaxSize(),
+    content: @Composable @RemoteComposable () -> Unit,
+) {
+    RemoteBox(
+        modifier,
+        horizontalAlignment = RemoteAlignment.CenterHorizontally,
+        verticalArrangement = RemoteArrangement.Center,
+        content = content,
+    )
+}
