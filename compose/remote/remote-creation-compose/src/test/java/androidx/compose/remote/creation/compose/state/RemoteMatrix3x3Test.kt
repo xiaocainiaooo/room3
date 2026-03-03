fe49f672c146d6c10956c09da1379d1939e5cbad
Alex Clarke <alexclarke@google.com>
2026-03-03 13:26:24 +0000

    Fix RemoteMatrix3x3 times operator
    The underlying MatrixOperation turns out not to support matrix
    indirection, so we need to concetenate the matricies. Note
    when doing so we have to be careful to insert
    MatrixOperations.IDENTITY for the right hand side of any times
    operation (it's implicit for the left hand side).
    
    Test: Unit tests
    Bug: 489352584
    Change-Id: Iceb89406afe8fec7222ee000502a6d3f3b0a4794

--- compose/remote/remote-creation-compose/src/test/java/androidx/compose/remote/creation/compose/state/RemoteMatrix3x3Test.kt
+++ compose/remote/remote-creation-compose/src/test/java/androidx/compose/remote/creation/compose/state/RemoteMatrix3x3Test.kt
@@ -16,6 +16,14 @@
 
 package androidx.compose.remote.creation.compose.state
 
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import androidx.compose.remote.core.CoreDocument
+import androidx.compose.remote.core.operations.matrix.MatrixExpression
+import androidx.compose.remote.creation.compose.capture.RemoteComposeCreationState
+import androidx.compose.remote.creation.platform.AndroidxRcPlatformServices
+import androidx.compose.remote.player.core.platform.AndroidRemoteContext
+import androidx.compose.ui.geometry.Size
 import com.google.common.truth.Truth.assertThat
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -23,6 +31,11 @@ import org.junit.runners.JUnit4
 
 @RunWith(JUnit4::class)
 class RemoteMatrix3x3Test {
+    val context =
+        AndroidRemoteContext().apply {
+            useCanvas(Canvas(Bitmap.createBitmap(1, 1, Bitmap.Config.ARGB_8888)))
+        }
+    val creationState = RemoteComposeCreationState(AndroidxRcPlatformServices(), Size(1f, 1f))
 
     @Test
     fun remoteMatrix_cacheKey() {
@@ -45,4 +58,146 @@ class RemoteMatrix3x3Test {
         assertThat(combined.cacheKey).isNotNull()
         assertThat(combined.cacheKey).isNotEqualTo(rotate1.cacheKey)
     }
+
+    @Test
+    fun matrix_concatenation() {
+        val m =
+            RemoteMatrix3x3.createTranslateXY(RemoteFloat(10f), RemoteFloat(20f)) *
+                RemoteMatrix3x3.createScaleX(RemoteFloat(1.5f)) *
+                RemoteMatrix3x3.createScaleY(RemoteFloat(2f))
+        val mId = m.getIdForCreationState(creationState)
+
+        makeAndPaintCoreDocument()
+        val evaluatedMatrix = (context.getObject(mId) as MatrixExpression).get()
+        assertThat(evaluatedMatrix)
+            .usingExactEquality()
+            .containsExactly(
+                floatArrayOf(
+                    1.5f,
+                    0.0f,
+                    0.0f,
+                    10.0f,
+                    0.0f,
+                    2.0f,
+                    0.0f,
+                    20.0f,
+                    0.0f,
+                    0.0f,
+                    1.0f,
+                    0.0f,
+                    0.0f,
+                    0.0f,
+                    0.0f,
+                    1.0f,
+                )
+            )
+    }
+
+    @Test
+    fun matrix_concatenation2() {
+        val m =
+            RemoteMatrix3x3.createTranslateXY(RemoteFloat(10f), RemoteFloat(20f)) *
+                RemoteMatrix3x3.createTranslateXY(RemoteFloat(10f), RemoteFloat(20f)) *
+                RemoteMatrix3x3.createTranslateXY(RemoteFloat(10f), RemoteFloat(20f))
+        val mId = m.getIdForCreationState(creationState)
+
+        makeAndPaintCoreDocument()
+        val evaluatedMatrix = (context.getObject(mId) as MatrixExpression).get()
+        assertThat(evaluatedMatrix)
+            .usingExactEquality()
+            .containsExactly(
+                floatArrayOf(
+                    1.0f,
+                    0.0f,
+                    0.0f,
+                    30.0f,
+                    0.0f,
+                    1.0f,
+                    0.0f,
+                    60.0f,
+                    0.0f,
+                    0.0f,
+                    1.0f,
+                    0.0f,
+                    0.0f,
+                    0.0f,
+                    0.0f,
+                    1.0f,
+                )
+            )
+    }
+
+    @Test
+    fun matrix_concatenation_with_identity() {
+        val m =
+            RemoteMatrix3x3.createTranslateXY(RemoteFloat(10f), RemoteFloat(20f)) *
+                RemoteMatrix3x3.createIdentity()
+        val mId = m.getIdForCreationState(creationState)
+
+        makeAndPaintCoreDocument()
+        val evaluatedMatrix = (context.getObject(mId) as MatrixExpression).get()
+        assertThat(evaluatedMatrix)
+            .usingExactEquality()
+            .containsExactly(
+                floatArrayOf(
+                    1.0f,
+                    0.0f,
+                    0.0f,
+                    10.0f,
+                    0.0f,
+                    1.0f,
+                    0.0f,
+                    20.0f,
+                    0.0f,
+                    0.0f,
+                    1.0f,
+                    0.0f,
+                    0.0f,
+                    0.0f,
+                    0.0f,
+                    1.0f,
+                )
+            )
+    }
+
+    @Test
+    fun matrix_concatenation_with_identity2() {
+        val m =
+            RemoteMatrix3x3.createIdentity() *
+                RemoteMatrix3x3.createTranslateXY(RemoteFloat(10f), RemoteFloat(20f))
+        val mId = m.getIdForCreationState(creationState)
+
+        makeAndPaintCoreDocument()
+        val evaluatedMatrix = (context.getObject(mId) as MatrixExpression).get()
+        assertThat(evaluatedMatrix)
+            .usingExactEquality()
+            .containsExactly(
+                floatArrayOf(
+                    1.0f,
+                    0.0f,
+                    0.0f,
+                    10.0f,
+                    0.0f,
+                    1.0f,
+                    0.0f,
+                    20.0f,
+                    0.0f,
+                    0.0f,
+                    1.0f,
+                    0.0f,
+                    0.0f,
+                    0.0f,
+                    0.0f,
+                    1.0f,
+                )
+            )
+    }
+
+    private fun makeAndPaintCoreDocument() =
+        CoreDocument().apply {
+            val buffer = creationState.document.buffer
+            buffer.buffer.index = 0
+            initFromBuffer(buffer)
+            paint(context, 0)
+        }
 }
